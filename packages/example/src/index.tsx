b551b0a285fae031383f7017de7c066061c91918
Samuel Alev <samuel.alev@protonmail.com>
2021-10-20 13:50:18 +0200

    refactor: remove css module example

--- packages/example/src/index.tsx
+++ packages/example/src/index.tsx
@@ -4,7 +4,7 @@ import { ReactElement } from 'react';
 export default function AnExampleBlock(): ReactElement {
     return (
         <div>
-            <span className="text-red-500 underline">A custom block in blue and underlined</span>
+            <span className="text-violet-60 underline">A custom block in blue and underlined</span>
         </div>
     );
 }
