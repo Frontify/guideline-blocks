47b40ec07d80db79aa18b2695f94190d3c8d82c2
Kevin Walker <mail@kevinwalker.ch>
2021-11-05 15:29:02 +0100

    Add basic block

--- packages/personal-note/src/index.tsx
+++ packages/personal-note/src/index.tsx
@@ -1,10 +1,49 @@
-import 'tailwindcss/tailwind.css';
-import { ReactElement } from 'react';
+/* (c) Copyright Frontify Ltd., all rights reserved. */
 
-export default function AnExampleBlock(): ReactElement {
+import 'tailwindcss/tailwind.css';
+import '@frontify/arcade/style';
+import { FC } from 'react';
+import { AppBridgeNative, useBlockSettings, useEditorState } from '@frontify/app-bridge';
+import { NoteStyle, NoteBorderRadius, NoteBorderStyle, NotePadding, NoteVisibility } from './types';
+
+type PersonalNoteBlockProps = {
+    appBridge: AppBridgeNative;
+};
+
+type Settings = {
+    note: string;
+};
+
+const PersonalNoteBlock: FC<PersonalNoteBlockProps> = ({ appBridge }) => {
+    const [blockSettings, setBlockSettings] = useBlockSettings<Settings>(appBridge);
+    const isEditing = useEditorState();
+
+    const { note = '' } = blockSettings;
+
+    const saveNote = (value: string) => {
+        console.log('save my note, dude: ', value);
+
+        setBlockSettings({
+            ...blockSettings,
+            note: value,
+        });
+    };
+
     return (
         <div>
-            <span className="tw-text-violet-60 tw-underline">A custom block in violet and underlined</span>
+            {isEditing ? (
+                <textarea
+                    className="tw-w-full tw-outline-none tw-resize-y"
+                    onChange={(event) => saveNote(event.target.value)}
+                    placeholder="Write personal note here..."
+                >
+                    {note}
+                </textarea>
+            ) : (
+                <p>{note}</p>
+            )}
         </div>
     );
-}
+};
+
+export default DosDontsBlock;
