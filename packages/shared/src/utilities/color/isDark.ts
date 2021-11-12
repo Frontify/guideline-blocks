5b4c5dc3573f04205ec3653a181650e444c570b1
Kevin Walker <mail@kevinwalker.ch>
2021-11-12 11:23:36 +0100

    Move type to separate file

--- packages/shared/src/utilities/color/isDark.ts
+++ packages/shared/src/utilities/color/isDark.ts
@@ -1,11 +1,6 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-type RgbaObject = {
-    r: number;
-    g: number;
-    b: number;
-    a?: number;
-};
+import { RgbaObject } from './types';
 
 /**
  * Calculates if text should be in a light color depending on color (e.g. background-color)
