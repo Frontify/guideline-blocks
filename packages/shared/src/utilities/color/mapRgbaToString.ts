904fdb5b2f2629d1101261b9249318708e928449
Kevin Walker <mail@kevinwalker.ch>
2021-11-12 12:01:50 +0100

    feat: add isDark color utility (shared package) (#44)

--- packages/shared/src/utilities/color/mapRgbaToString.ts
+++ packages/shared/src/utilities/color/mapRgbaToString.ts
@@ -1,16 +1,11 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-type RgbaObject = {
-    r: number;
-    g: number;
-    b: number;
-    a: number;
-};
+import { RgbaObject } from './types';
 
 /**
  * Maps object of rgba values to string.
  *
- * @param rgbaObject Object of rgba values
- * @returns String to be used in as css value.
+ * @param {Object} Object of RGBA values
+ * @returns {String} To be used as css value.
  */
 export const mapRgbaToString = (rgbaObject: RgbaObject): string => `rgba(${Object.values(rgbaObject).join(', ')})`;
