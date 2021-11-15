e9f4ce8ddef51103c5b3f33e9090f128f78fac3b
Kevin Walker <mail@kevinwalker.ch>
2021-11-15 14:12:21 +0100

    Fix shared utilities

--- packages/personal-note-block/src/index.tsx
+++ packages/personal-note-block/src/index.tsx
@@ -5,7 +5,7 @@ import '@frontify/arcade/style';
 import { FC } from 'react';
 import { AppBridgeNative, useBlockSettings, useEditorState } from '@frontify/app-bridge';
 import { RichTextEditor, Color } from '@frontify/arcade';
-import { mapRgbaToString, isDark } from '../../shared';
+import { mapRgbaToString, isDark } from '@frontify/guideline-blocks-shared';
 import { NoteHeader } from './components/NoteHeader';
 import { NoteStyle, NoteBorderRadius, NoteBorderStyle, NotePadding, NoteVisibility } from './types';
 import { BACKGROUND_COLOR_DEFAULT_VALUE, BORDER_COLOR_DEFAULT_VALUE } from './settings';
