bde182ff6a863aac2b3bf5d2aea02a5b56c97d01
Sam Creasey <samcreasey@sam-creasey.localdomain>
2021-11-16 12:49:02 +0100

    created basic settings structure

--- packages/checklist-block/src/index.tsx
+++ packages/checklist-block/src/index.tsx
@@ -1,10 +1,38 @@
 import 'tailwindcss/tailwind.css';
 import { ReactElement } from 'react';
+import { AppBridgeNative, useBlockSettings } from '@frontify/app-bridge';
 
-export default function AnExampleBlock(): ReactElement {
+type ChecklistProps = {
+    appBridge: AppBridgeNative;
+};
+
+type ChecklistItem = {
+    text: string;
+};
+
+type Settings = {
+    content: ChecklistItem[];
+    padding: string;
+    incompleteTextColor: string;
+    incompleteCheckboxColor: string;
+    completeTextColor: string;
+    completeCheckboxColor: string;
+    completedDecoration: string;
+    highlightColor: string;
+    dateVisible: boolean;
+    progressBarVisible: boolean;
+    progressBarType: string;
+    progressBarFillColor: string;
+    progressBarTrackColor: string;
+};
+
+export default function AnExampleBlock({ appBridge }: ChecklistProps): ReactElement {
+    const [blockSettings, setBlockSettings] = useBlockSettings<Settings>(appBridge);
     return (
         <div>
-            <span className="tw-text-violet-60 tw-underline">A custom block in violet and underlined</span>
+            {Object.keys(blockSettings).map((val) => (
+                <p>{val}</p>
+            ))}
         </div>
     );
 }
--- packages/checklist-block/src/settings.ts
+++ packages/checklist-block/src/settings.ts
@@ -1,20 +1,180 @@
-import { IconEnum } from '@frontify/arcade';
+import { IconEnum, MultiInputLayout } from '@frontify/arcade';
+
+const showProgressStyles = (bundle: any): boolean => {
+    return bundle.getBlock('progressBarVisible').value === true && bundle.getBlock('progressBarType').value === 'bar';
+};
 
 export default {
     main: [
         {
-            id: 'main-dropdown',
+            id: 'completedDecoration',
             type: 'dropdown',
-            defaultValue: 'custom_block',
+            defaultValue: 'checkbox',
             size: 'large',
-            disabled: true,
+            disabled: false,
             choices: [
                 {
-                    value: 'custom_block',
-                    icon: IconEnum.Snippet,
-                    label: 'Custom Block',
+                    value: 'checkbox',
+                    icon: IconEnum.Symbols,
+                    label: 'Checkbox',
+                },
+                {
+                    value: 'strikethrough',
+                    icon: IconEnum.Symbols,
+                    label: 'Strikethrough',
+                },
+                {
+                    value: 'highlight',
+                    icon: IconEnum.Symbols,
+                    label: 'Highlight',
                 },
             ],
+        },
+    ],
+    layout: [
+        {
+            id: 'paddingSwitch',
+            label: 'Padding',
+            type: 'switch',
+            switchLabel: 'Custom',
+            on: [
+                {
+                    id: 'paddingCustom',
+                    type: 'input',
+                    placeholder: '0px',
+                },
+            ],
+            off: [
+                {
+                    id: 'paddingBasic',
+                    type: 'slider',
+                    defaultValue: 'padding-s',
+                    choices: [
+                        {
+                            value: 'padding-none',
+                            label: 'None',
+                        },
+                        {
+                            value: 'padding-s',
+                            label: 'S',
+                        },
+                        {
+                            value: 'padding-m',
+                            label: 'M',
+                        },
+                        {
+                            value: 'padding-l',
+                            label: 'L',
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            id: 'progressBarVisible',
+            label: 'Progress Indicator',
+            type: 'switch',
+            on: [
+                {
+                    id: 'progressBarType',
+                    type: 'slider',
+                    defaultValue: 'percentage',
+                    choices: [
+                        {
+                            value: 'percentage',
+                            label: 'Percentage',
+                        },
+                        {
+                            value: 'fraction',
+                            label: 'Fraction',
+                        },
+                        {
+                            value: 'bar',
+                            label: 'Bar',
+                        },
+                    ],
+                },
+            ],
+            off: [],
+        },
+        {
+            id: 'dateVisible',
+            label: 'Date completed',
+            type: 'switch',
+        },
+    ],
+    style: [
+        {
+            id: 'incompleteTextColor',
+            type: 'colorInput',
+            label: 'Incomplete Text',
+        },
+        {
+            id: 'incompleteCheckboxColor',
+            type: 'colorInput',
+            label: 'Incomplete Checkbox',
+        },
+        {
+            id: 'completeTextColor',
+            type: 'colorInput',
+            label: 'Complete Text',
+        },
+        {
+            id: 'completeTextColor',
+            type: 'colorInput',
+            label: 'Complete Text',
+        },
+        {
+            id: 'highlightColor',
+            type: 'colorInput',
+            label: 'Highlight',
+            show: (bundle: any) => bundle.getBlock('completedDecoration').value === 'highlight',
+        },
+        {
+            id: 'strikethroughMultiInput',
+            type: 'multiInput',
+            label: 'Line',
+            layout: MultiInputLayout.Columns,
+            lastItemFullWidth: true,
+            show: (bundle: any) => bundle.getBlock('completedDecoration').value === 'strikethrough',
+            blocks: [
+                {
+                    id: 'strikethroughStyle',
+                    type: 'dropdown',
+                    defaultValue: 'solid',
+                    choices: [
+                        {
+                            label: 'Solid',
+                            value: 'solid',
+                        },
+                        {
+                            label: 'Dashed',
+                            value: 'dashed',
+                        },
+                    ],
+                },
+                {
+                    id: 'strikethroughWidth',
+                    type: 'input',
+                    defaultValue: '1px',
+                },
+                {
+                    id: 'strikethroughColor',
+                    type: 'colorInput',
+                },
+            ],
+        },
+        {
+            id: 'progressBarFillColor',
+            type: 'colorInput',
+            label: 'Progress Bar Fill',
+            show: showProgressStyles,
+        },
+        {
+            id: 'progressBarTrackColor',
+            type: 'colorInput',
+            label: 'Progress Bar Track',
+            show: showProgressStyles,
         },
     ],
 };
