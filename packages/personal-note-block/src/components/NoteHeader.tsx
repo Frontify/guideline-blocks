a4f68a85b527db86943dcc6e63bce83dab9c3cbc
Kevin Walker <mail@kevinwalker.ch>
2021-11-16 10:55:44 +0100

    Add user data

--- packages/personal-note-block/src/components/NoteHeader.tsx
+++ packages/personal-note-block/src/components/NoteHeader.tsx
@@ -2,22 +2,30 @@
 
 import 'tailwindcss/tailwind.css';
 import '@frontify/arcade/style';
-import { FC, useRef, useState } from 'react';
+import { FC } from 'react';
 import dayjs from 'dayjs';
 
 type NoteHeaderProps = {
+    name: string;
+    avatar: string;
     hasAvatarName: boolean;
     hasDateEdited: boolean;
     dateEdited: Date;
     useLightText: boolean;
 };
 
-export const NoteHeader: FC<NoteHeaderProps> = ({ hasAvatarName, hasDateEdited, dateEdited, useLightText }) => (
+export const NoteHeader: FC<NoteHeaderProps> = ({
+    name,
+    avatar,
+    hasAvatarName,
+    hasDateEdited,
+    dateEdited,
+    useLightText,
+}) => (
     <div className="tw-flex tw-items-center tw-space-x-4">
-        {/* TODO: Replace with acutal user data */}
-        {hasAvatarName && <img src="https://picsum.photos/200" width="32" height="32" className="tw-rounded-full" />}
+        {hasAvatarName && avatar && <img src={avatar} width="32" height="32" className="tw-rounded-full" />}
         <div className="tw-flex tw-flex-col tw-text-s">
-            {hasAvatarName && <span className={useLightText ? 'tw-text-white' : 'tw-text-black'}>Leanne Simpson</span>}
+            {hasAvatarName && <span className={useLightText ? 'tw-text-white' : 'tw-text-black'}>{name}</span>}
             {hasDateEdited && dateEdited && (
                 <span className={useLightText ? 'tw-text-white tw-opacity-60' : 'tw-text-black-60'}>
                     Last edited on {dayjs(dateEdited).format('DD/MM/YYYY, HH:mm:ss')}
