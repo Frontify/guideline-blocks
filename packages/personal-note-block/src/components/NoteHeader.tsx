7d280e7e1d99e7a450df9ed84cc5cad62d0cc084
Kevin Walker <mail@kevinwalker.ch>
2021-11-17 14:26:36 +0100

    Use browser locale for date

--- packages/personal-note-block/src/components/NoteHeader.tsx
+++ packages/personal-note-block/src/components/NoteHeader.tsx
@@ -2,7 +2,10 @@
 
 import { FC } from 'react';
 import dayjs from 'dayjs';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
 import { NoteHeaderProps } from '../types';
+
+dayjs.extend(localizedFormat).locale(navigator.language || navigator.userLanguage);
 
 export const NoteHeader: FC<NoteHeaderProps> = ({
     name,
@@ -18,7 +21,7 @@ export const NoteHeader: FC<NoteHeaderProps> = ({
             {hasAvatarName && <span className={useLightText ? 'tw-text-white' : 'tw-text-black'}>{name}</span>}
             {hasDateEdited && dateEdited && (
                 <span className={useLightText ? 'tw-text-white tw-opacity-60' : 'tw-text-black-60'}>
-                    Last edited on {dayjs(dateEdited).format('DD/MM/YYYY, HH:mm:ss')}
+                    Last edited on {dayjs(dateEdited).format('L, LT')}
                 </span>
             )}
         </div>
