1bafb3dccacfce342b93ef47c6c075e1d933e631
Kevin Walker <mail@kevinwalker.ch>
2021-11-17 09:06:26 +0100

    Refactor types

--- packages/personal-note-block/src/types.ts
+++ packages/personal-note-block/src/types.ts
@@ -1,27 +1,49 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
+export type BlockProps = {
+    appBridge: AppBridgeNative;
+};
+
+export type borderSelectionType = [NoteBorderStyle, string, Color];
+
+export type Settings = {
+    backgroundColor: Color;
+    borderRadiusChoice: NoteBorderRadius;
+    borderSelection: borderSelectionType;
+    dateEdited: Date;
+    hasAvatarName: boolean;
+    hasBackground: boolean;
+    hasCustomPadding: boolean;
+    hasDateEdited: boolean;
+    note: string;
+    paddingChoice: NotePadding;
+    paddingValue: string;
+    userId: string;
+    visibility: NoteVisibility;
+};
+
 export enum NoteStyle {
     Card = 'Card',
 }
 
 export enum NotePadding {
-    None = 'None',
-    Small = 'Small',
-    Medium = 'Medium',
-    Large = 'Large',
+    None = 'none',
+    Small = 'small',
+    Medium = 'medium',
+    Large = 'large',
 }
 
 export enum NoteBorderRadius {
-    None = 'None',
-    Small = 'Small',
-    Medium = 'Medium',
-    Large = 'Large',
+    None = 'none',
+    Small = 'small',
+    Medium = 'medium',
+    Large = 'large',
 }
 
 export enum NoteBorderStyle {
-    Solid = 'Solid',
-    Dotted = 'Dotted',
-    Dashed = 'Dashed',
+    Solid = 'solid',
+    Dotted = 'dotted',
+    Dashed = 'dashed',
 }
 
 export enum NoteVisibility {
@@ -29,3 +51,23 @@ export enum NoteVisibility {
     Editors = 'Editors',
     Everyone = 'Everyone',
 }
+
+export const borderStyles: Record<NoteBorderStyle, string> = {
+    [NoteBorderStyle.Solid]: 'solid',
+    [NoteBorderStyle.Dotted]: 'dotted',
+    [NoteBorderStyle.Dashed]: 'dashed',
+};
+
+export const borderRadiusClasses: Record<NoteBorderRadius, string> = {
+    [NoteBorderRadius.None]: 'tw-rounded-none',
+    [NoteBorderRadius.Small]: 'tw-rounded',
+    [NoteBorderRadius.Medium]: 'tw-rounded-md',
+    [NoteBorderRadius.Large]: 'tw-rounded-lg',
+};
+
+export const paddingClasses: Record<NotePadding, string> = {
+    [NotePadding.None]: 'tw-p-0',
+    [NotePadding.Small]: 'tw-p-6',
+    [NotePadding.Medium]: 'tw-p-9',
+    [NotePadding.Large]: 'tw-p-12',
+};
