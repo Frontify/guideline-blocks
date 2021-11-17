105f53137c39575a6611c0b58d44b15c99e43288
Samuel Alev <samuel.alev@protonmail.com>
2021-11-17 12:50:19 +0100

    refactor: remove old text block

--- packages/text/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-const arcadeTailwindConfig = require('@frontify/arcade/config').tailwindConfig;
-const tailwindConfig = arcadeTailwindConfig({
-    corePlugins: {
-        preflight: false,
-    },
-});
-delete tailwindConfig.plugins;
-module.exports = tailwindConfig;
