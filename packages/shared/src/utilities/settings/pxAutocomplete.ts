26e52ba7ec17361d1d546de94fe19c364e5ede81
Kevin Walker <mail@kevinwalker.ch>
2021-11-17 10:26:23 +0100

    Update JSDoc
    Co-authored-by: Samuel Alev <samuel.alev@frontify.com>

--- packages/shared/src/utilities/settings/pxAutocomplete.ts
+++ packages/shared/src/utilities/settings/pxAutocomplete.ts
@@ -3,9 +3,9 @@
 /**
  * Checks if value is a numeric value without "px" and creates string from value with "px"
  *
- * @param Block bundle
- * @param {string} Block Id
- * @returns Set block value to string with "px"
+ * @param {Object} Sidebar bundle object
+ * @param {number} Block Id
+ * @returns {string} Set block value to string with "px"
  */
 
 export const pxAutocomplete = (bundle: any, blockId: string) => {
