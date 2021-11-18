e93fe5d3ef88915465441e7b65e8de3ae3186221
Kevin Walker <mail@kevinwalker.ch>
2021-11-18 09:17:58 +0100

    Add settings types to shared

--- packages/shared/src/helpers/settings/pxAutocomplete.spec.ts
+++ packages/shared/src/helpers/settings/pxAutocomplete.spec.ts
@@ -1,17 +1,16 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-empty-function */
+import { ApiBundle } from '@frontify/guideline-blocks-settings';
 
 import { pxAutocomplete } from './pxAutocomplete';
 
 describe('pxAutocomplete', () => {
-    test('it should call setBlockValue with correct string with 20px', () => {
-        const bundle = {
-            getBlock(settingId: string) {
+    test('it should set correct value with "px" when entering a number', () => {
+        const bundle: ApiBundle = {
+            getBlock() {
                 return { value: 20 };
             },
-            setBlockValue(settingId: string, value: string) {},
+            setBlockValue(): void {},
         };
 
         const setBlockValueSpy = jest.spyOn(bundle, 'setBlockValue');
@@ -20,12 +19,12 @@ describe('pxAutocomplete', () => {
         expect(setBlockValueSpy).toHaveBeenCalledWith('my_setting_id', '20px');
     });
 
-    test('it should not call setBlockValue', () => {
-        const bundle = {
-            getBlock(settingId: string) {
+    test('it should not call setBlockValue when entering a px-value', () => {
+        const bundle: ApiBundle = {
+            getBlock() {
                 return { value: '20px' };
             },
-            setBlockValue(settingId: string, value: string) {},
+            setBlockValue(): void {},
         };
         const setBlockValueSpy = jest.spyOn(bundle, 'setBlockValue');
         pxAutocomplete(bundle, 'my_setting_id');
--- packages/shared/src/helpers/settings/pxAutocomplete.ts
+++ packages/shared/src/helpers/settings/pxAutocomplete.ts
@@ -1,18 +1,16 @@
-//TODO: Remove when PR with sidebar types is merged
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-
 /* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import { ApiBundle } from '@frontify/guideline-blocks-settings';
 
 /**
  * Checks if value is a numeric value without "px" and creates string from value with "px"
  *
- * @param {Object} bundle Sidebar bundle object
- * @param {number} settingId Setting id
+ * @param {ApiBundle} bundle Sidebar bundle object
+ * @param {string} settingId Setting id
  * @returns {string} Set block value to string with "px"
  */
 
-export const pxAutocomplete = (bundle: any, settingId: string): void => {
+export const pxAutocomplete = (bundle: ApiBundle, settingId: string): void => {
     const value = Number(bundle.getBlock(settingId)?.value);
     if (!Number.isNaN(value)) {
         bundle.setBlockValue(settingId, `${value}px`);
