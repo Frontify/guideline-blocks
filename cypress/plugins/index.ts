5580f53618736925add4551f5f6791909b0476b1
Samuel Alev <samuel.alev@frontify.com>
2021-11-28 15:11:48 +0100

    chore: regenerate package-lock and prettify files

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,12 +1,17 @@
-import path from 'path';
-import { startDevServer } from '@cypress/vite-dev-server';
+import path from "path";
+import { startDevServer } from "@cypress/vite-dev-server";
 
 const pluginConfig: Cypress.PluginConfig = (on, config) => {
-    on('dev-server:start', async (options) =>
+    on("dev-server:start", async (options) =>
         startDevServer({
             options,
             viteConfig: {
-                configFile: path.resolve(__dirname, '..', '..', 'vite.test.config.ts'),
+                configFile: path.resolve(
+                    __dirname,
+                    "..",
+                    "..",
+                    "vite.test.config.ts"
+                ),
             },
         })
     );
