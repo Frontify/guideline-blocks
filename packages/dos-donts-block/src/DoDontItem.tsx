4572fc23e2b03dfaf1b67d4f302c7fca52f713fa
Samuel Alev <samuel.alev@frontify.com>
2021-11-29 10:57:42 +0100

    chore: use more precise type (#93)

--- packages/dos-donts-block/src/DoDontItem.tsx
+++ packages/dos-donts-block/src/DoDontItem.tsx
@@ -3,7 +3,7 @@
 import { useEditorState } from '@frontify/app-bridge';
 import { Color, IconApprove, IconRejectCircle, IconSize, RichTextEditor } from '@frontify/arcade';
 import { mapRgbaToString } from '@frontify/guideline-blocks-shared';
-import { FC } from 'react';
+import { FC, CSSProperties } from 'react';
 import { DoDontStyle, DoDontType } from './types';
 
 export type ItemProps = {
@@ -22,12 +22,12 @@ export const DoDontItem: FC<ItemProps> = ({ id, type, style, doColor, dontColor,
     const doColorString = doColor.rgba && mapRgbaToString(doColor.rgba);
     const dontColorString = dontColor.rgba && mapRgbaToString(dontColor.rgba);
 
-    const headingStyles: Record<DoDontType, object> = {
+    const headingStyles: Record<DoDontType, CSSProperties> = {
         [DoDontType.Do]: { color: doColorString },
         [DoDontType.Dont]: { color: dontColorString },
     };
 
-    const dividerStyles: Record<DoDontType, object> = {
+    const dividerStyles: Record<DoDontType, CSSProperties> = {
         [DoDontType.Do]: { backgroundColor: doColorString },
         [DoDontType.Dont]: { backgroundColor: dontColorString },
     };
