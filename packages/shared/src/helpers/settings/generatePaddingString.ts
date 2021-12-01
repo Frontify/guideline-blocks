123f192cf6b23d16a88dc2932507f31dc723cf69
Sam Creasey <sam.creasey@frontify.com>
2021-12-01 15:07:26 +0100

    moved padding string generation to shared

--- packages/shared/src/helpers/settings/generatePaddingString.ts
+++ packages/shared/src/helpers/settings/generatePaddingString.ts
@@ -1,4 +1,5 @@
 export const generatePaddingString = (paddingSpiderInput: (string | null)[]): string => {
+    if (!paddingSpiderInput) return '0px';
     const [top, left, right, bottom] = paddingSpiderInput;
     return [top, right, bottom, left].map((pixels) => pixels || '0px').join(' ');
 };
--- packages/shared/src/helpers/settings/index.ts
+++ packages/shared/src/helpers/settings/index.ts
@@ -1,4 +1,4 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
-
+export * from './generatePaddingString';
 export * from './presetCustomValue';
 export * from './appendUnit';
