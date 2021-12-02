0b8b3e078e5033b754f29abb73c3e25e5a1e3ddd
Sam Creasey <sam.creasey@frontify.com>
2021-12-02 13:56:42 +0100

    remove tailwind modification

--- packages/checklist-block/tailwind.config.js
+++ packages/checklist-block/tailwind.config.js
@@ -1,7 +1,5 @@
 const arcadeTailwindConfig = require('@frontify/arcade/config').tailwindConfig;
 const tailwindConfig = arcadeTailwindConfig({
-    mode: 'jit',
-    purge: ['./src/**/*.{tsx}'],
     corePlugins: {
         preflight: false,
     },
