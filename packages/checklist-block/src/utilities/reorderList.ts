a30b32a4eb3c58f0eba6cef617f11d07aa81a012
Sam Creasey <sam.creasey@frontify.com>
2021-12-02 11:27:09 +0100

    added new helper functions

---
+++ packages/checklist-block/src/utilities/reorderList.ts
@@ -0,0 +1,6 @@
+export const reorderList = <T>(array: T[], originalIndex: number, newIndex: number): T[] => {
+    const newArray = array.slice();
+    const [itemToSwap] = newArray.splice(originalIndex, 1);
+    newArray.splice(newIndex, 0, itemToSwap);
+    return newArray;
+};
