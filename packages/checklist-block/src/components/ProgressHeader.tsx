c6ce34937c46f2f6a73859a68d8346348cb50a15
Sam Creasey <sam.creasey@frontify.com>
2021-12-02 10:18:34 +0100

    added calculation utilities and refactored progress header prop type

--- packages/checklist-block/src/components/ProgressHeader.tsx
+++ packages/checklist-block/src/components/ProgressHeader.tsx
@@ -1,12 +1,13 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import React, { ReactElement } from 'react';
+import React, { FC } from 'react';
 import { ProgressHeaderProps } from '../types';
 
-export const ProgressHeader = ({ value }: ProgressHeaderProps): ReactElement => {
+export const ProgressHeader: FC<ProgressHeaderProps> = ({ value }) => {
     return (
         <div className="tw-pl-1.5">
-            <span className="tw-font-bold tw-text-black-100">{value}</span>{' '}
+            <span className="tw-font-bold tw-text-black-100">{value}</span>
+            &nbsp;
             <span className="tw-black-80">Completed</span>
         </div>
     );
