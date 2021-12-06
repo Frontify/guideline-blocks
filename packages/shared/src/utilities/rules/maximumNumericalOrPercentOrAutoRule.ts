be70cbf2b5df160940efccc52b81707c57381b05
Nadia Posch <nadia@smartive.ch>
2021-12-06 14:58:16 +0100

    revert changes in min/max rule files

--- packages/shared/src/utilities/rules/maximumNumericalOrPercentOrAutoRule.ts
+++ packages/shared/src/utilities/rules/maximumNumericalOrPercentOrAutoRule.ts
@@ -3,9 +3,8 @@
 import { numericalOrPercentRule } from './numericalOrPercentRule';
 import { Rule } from './types';
 
-export const maximumNumericalOrPercentOrAutoRule = (maximumValue: number, includeAuto = true): Rule<string> => ({
+export const maximumNumericalOrPercentOrAutoRule = (maximumValue: number): Rule<string> => ({
     errorMessage: `Please use a value smaller than ${maximumValue}`,
     validate: (value: string): boolean =>
-        (value === 'auto' && includeAuto) ||
-        (numericalOrPercentRule.validate(value) && Number(value.replace(/%/, '')) <= maximumValue),
+        value === 'auto' || (numericalOrPercentRule.validate(value) && Number(value.replace(/%/, '')) <= maximumValue),
 });
--- packages/shared/src/utilities/rules/minimumNumericalOrPercentOrAutoRule.ts
+++ packages/shared/src/utilities/rules/minimumNumericalOrPercentOrAutoRule.ts
@@ -3,9 +3,8 @@
 import { numericalOrPercentRule } from './numericalOrPercentRule';
 import { Rule } from './types';
 
-export const minimumNumericalOrPercentOrAutoRule = (minimumValue: number, includeAuto = true): Rule<string> => ({
+export const minimumNumericalOrPercentOrAutoRule = (minimumValue: number): Rule<string> => ({
     errorMessage: `Please use a value bigger or than ${minimumValue}`,
     validate: (value: string): boolean =>
-        (value === 'auto' && includeAuto) ||
-        (numericalOrPercentRule.validate(value) && Number(value.replace(/%/, '')) >= minimumValue),
+        value === 'auto' || (numericalOrPercentRule.validate(value) && Number(value.replace(/%/, '')) >= minimumValue),
 });
