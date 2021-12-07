ad5f73cd7a1e10e52272fcf98f5cae708f7e3301
Sam Creasey <sam.creasey@frontify.com>
2021-12-07 13:09:12 +0100

    add direct returns where necessary

--- packages/shared/src/helpers/settings/generatePaddingString.ts
+++ packages/shared/src/helpers/settings/generatePaddingString.ts
@@ -1,7 +1,9 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 export const generatePaddingString = (paddingSpiderInput: (string | null)[]): string => {
-    if (!paddingSpiderInput.length) return '0px';
+    if (!paddingSpiderInput.length) {
+        return '0px';
+    }
     const [top, left, right, bottom] = paddingSpiderInput;
     return [top, right, bottom, left].map((pixels) => pixels || '0px').join(' ');
 };
