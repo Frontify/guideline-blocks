95aeda3d7abc294d463ed92c808eab8e4aeef29f
Sam Creasey <sam.creasey@frontify.com>
2021-12-09 17:16:13 +0100

    fixed spelling mistakes

--- packages/shared/src/utilities/color/mapRgbaToString.spec.ts
+++ packages/shared/src/utilities/color/mapRgbaToString.spec.ts
@@ -5,7 +5,7 @@ import { mapRgbaToString } from './mapRgbaToString';
 describe('mapRgbaToString', () => {
     const data = [{ rgbaObject: { r: 1, g: 2, b: 3, a: 0.5 }, expected: 'rgba(1, 2, 3, 0.5)' }];
 
-    it.each(data)('validate correctly values', ({ rgbaObject, expected }) => {
+    it.each(data)('validates against expected values', ({ rgbaObject, expected }) => {
         expect(mapRgbaToString(rgbaObject)).toBe(expected);
     });
 });
