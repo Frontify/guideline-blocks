0a63bc82fe69ec38372707f86913c1e8a6cb009b
Sam Creasey <sam.creasey@frontify.com>
2021-12-09 17:06:40 +0100

    added tests for label and editor

---
+++ packages/checklist-block/src/components/CheckboxLabel.spec.tsx
@@ -0,0 +1,25 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import { CheckboxLabel } from './CheckboxLabel';
+import { mount } from '@cypress/react';
+import { CheckboxLabelProps } from '../types';
+
+const CHECKBOX_LABEL = '[data-test-id="checkbox-label"]';
+const CHECKBOX_DATE = '[data-test-id="checkbox-date"]';
+
+const DefaultCheckboxLabel = (props: Partial<CheckboxLabelProps>) => {
+    const defaults = { htmlFor: 'test', disabled: false, dateInMs: 0, children: 'Label' };
+    const checkboxProps = { ...defaults, ...props };
+    return <CheckboxLabel {...checkboxProps} />;
+};
+
+describe('Checkbox Label', () => {
+    it('renders a checkbox label', () => {
+        mount(<DefaultCheckboxLabel />);
+        cy.get(CHECKBOX_LABEL).should('exist');
+    });
+    it('renders the date in a readable style', () => {
+        mount(<DefaultCheckboxLabel dateInMs={Date.now()} />);
+        cy.get(CHECKBOX_DATE).should('have.text', 'a few seconds ago');
+    });
+});
