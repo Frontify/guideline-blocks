d4ff13260695bd6ffb78d025d349c8ac3ba1b959
Samuel Alev <samuel.alev@frontify.com>
2021-12-13 15:41:28 +0100

    chore: rename file

--- packages/shared/src/utilities/tests/_getRgbCssFromHex.spec.ts
+++ packages/shared/src/utilities/tests/_getRgbCssFromHex.spec.ts
@@ -1,6 +1,6 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { getRgbCssFromHex } from './getRgbCssFromHex';
+import { getRgbCssFromHex } from './_getRgbCssFromHexx';
 
 describe('getRgbCSSFromHex', () => {
     const data = [
