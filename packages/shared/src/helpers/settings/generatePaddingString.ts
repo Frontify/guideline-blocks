d2fdb3ebac81c8027c90498103087c264e1ead7f
Sam Creasey <sam.creasey@frontify.com>
2021-12-14 16:11:40 +0100

    added line breaks before all returns

--- packages/shared/src/helpers/settings/generatePaddingString.ts
+++ packages/shared/src/helpers/settings/generatePaddingString.ts
@@ -5,5 +5,6 @@ export const generatePaddingString = (paddingSpiderInput: (string | null)[]): st
         return '0px';
     }
     const [top, left, right, bottom] = paddingSpiderInput;
+
     return [top, right, bottom, left].map((pixels) => pixels || '0px').join(' ');
 };
--- packages/shared/src/utilities/tests/getRgbCssFromHex.ts
+++ packages/shared/src/utilities/tests/getRgbCssFromHex.ts
@@ -6,5 +6,6 @@ export const getRgbCssFromHex = (hex: string) => {
     const r = (base10Hex >> 16) & 255;
     const g = (base10Hex >> 8) & 255;
     const b = base10Hex & 255;
+
     return `rgb(${r}, ${g}, ${b})`;
 };
