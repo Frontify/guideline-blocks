e2efe6f2da8f604eea42e8236e4434ce68892a07
Kevin Walker <kevin@smartive.ch>
2022-01-24 14:01:33 +0100

    fix: colorToHexAlpha tests

--- packages/shared/src/utilities/color/colorToHexAlpha.spec.ts
+++ packages/shared/src/utilities/color/colorToHexAlpha.spec.ts
@@ -4,10 +4,10 @@ import { colorToHexAlpha } from '.';
 
 describe('colorToHexAlpha', () => {
     const data = [
-        { color: { r: 255, g: 255, b: 255 }, expected: '#FFFFFFFF' },
-        { color: { r: 255, g: 221, b: 255, a: 1 }, expected: '#FFDDFFFF' },
-        { color: { r: 255, g: 255, b: 15, a: 0 }, expected: '#FFFFFG00' },
-        { color: { r: 238, g: 35, b: 84, a: 0.4 }, expected: '#EE235466' },
+        { color: { r: 255, g: 255, b: 255 }, expected: '#ffffffff' },
+        { color: { r: 255, g: 221, b: 255, a: 1 }, expected: '#ffddffff' },
+        { color: { r: 255, g: 255, b: 15, a: 0 }, expected: '#ffff0f00' },
+        { color: { r: 238, g: 35, b: 84, a: 0.4 }, expected: '#ee235466' },
     ];
 
     it.each(data)('validates against expected values', ({ color, expected }) => {
