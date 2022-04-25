44328f8c97fe673765e27fd0ce7dad04b56e8436
Samuel Alev <samuel.alev@frontify.com>
2022-04-25 17:23:26 +0200

    add asset chooser for test

--- packages/example/src/ExampleBlock.tsx
+++ packages/example/src/ExampleBlock.tsx
@@ -2,23 +2,43 @@
 
 import 'tailwindcss/tailwind.css';
 import { FC } from 'react';
-import { Asset, useBlockAssets } from '@frontify/app-bridge';
+import { Asset, AssetChooserResult, IAppBridgeNative, useBlockAssets } from '@frontify/app-bridge';
+import { Button } from '@frontify/arcade';
 
-export const ExampleBlock: FC = ({ appBridge }: any) => {
-    const { blockAssets } = useBlockAssets(appBridge);
+export const ExampleBlock: FC<{ appBridge: IAppBridgeNative }> = ({ appBridge }) => {
+    const { blockAssets, updateAssetIdsFromKey } = useBlockAssets(appBridge);
+
+    const onOpenAssetChooser = () => {
+        appBridge.openAssetChooser(
+            (result: AssetChooserResult) => {
+                const resultId = result.screenData[0].id;
+                updateAssetIdsFromKey('images', [resultId]);
+                appBridge.closeAssetChooser();
+            },
+            {
+                selectedValueId: blockAssets['images']?.[0]?.id,
+            }
+        );
+    };
 
     return (
-        <div data-test-id="example-block">
-            {blockAssets['images'] ? (
-                blockAssets['images'].map((asset: Asset) => (
-                    <div key={asset.id}>
-                        <img src={asset.preview_url} />
-                        <p>{asset.title}</p>
-                    </div>
-                ))
-            ) : (
-                <p>No image set</p>
-            )}
+        <div className="tw-flex tw-flex-col tw-gap-4">
+            <div>
+                <Button onClick={onOpenAssetChooser}>Open asset chooser</Button>
+            </div>
+
+            <div data-test-id="example-block">
+                {blockAssets['images'] ? (
+                    blockAssets['images'].map((asset: Asset) => (
+                        <div key={asset.id}>
+                            <img src={asset.preview_url} />
+                            <p>{asset.title}</p>
+                        </div>
+                    ))
+                ) : (
+                    <p>No image set</p>
+                )}
+            </div>
         </div>
     );
 };
