39a02d79a8e53f38f82393ec940b1e17d9b3da16
Kevin Walker <kevin@smartive.ch>
2022-04-29 14:54:43 +0200

    feat(BlockSettings): add showInMainLanguageOnly to BaseBlock type (#225)

--- packages/block-settings/types/blocks/base.ts
+++ packages/block-settings/types/blocks/base.ts
@@ -8,6 +8,7 @@ export type BaseBlock<T = undefined> = {
     info?: string;
     value?: T;
     defaultValue?: T;
+    showInMainLanguageOnly?: boolean;
     show?: (bundle: Bundle) => boolean;
     onChange?: (bundle: Bundle) => void;
 };
