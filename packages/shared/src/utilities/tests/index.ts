460e7042e0d6394187274e21f282d9532fbdfdd3
Samuel Alev <samuel.alev@frontify.com>
2022-05-02 22:00:20 +0200

    test: add stub for `useBlockAsset` and `useAssetChooser`

--- packages/shared/src/utilities/tests/index.ts
+++ packages/shared/src/utilities/tests/index.ts
@@ -1,3 +1,4 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 export { withAppBridgeStubs } from './withAppBridgeStubs';
+export * from './AssetDummy';
--- packages/shared/src/utilities/tests/withAppBridgeStubs.tsx
+++ packages/shared/src/utilities/tests/withAppBridgeStubs.tsx
@@ -1,24 +1,46 @@
-import { AppBridgeNativeMock, IAppBridgeNative } from '@frontify/app-bridge';
+import { AppBridgeNativeMock, Asset, IAppBridgeNative } from '@frontify/app-bridge';
 import { ComponentType } from 'react';
 import { stub } from 'sinon';
 
 type useStubedAppBridgeProps = {
     blockSettings?: Record<string, unknown>;
+    blockAssets?: Record<string, Asset[]>;
     editorState?: boolean;
+    openAssetChooser?: () => void;
+    closeAssetChooser?: () => void;
 };
 
 interface Window {
     blockSettings: Record<number, Record<string, unknown>>;
+    emitter: Record<string, any>;
 }
 
-const useStubedAppBridge = ({ blockSettings = {}, editorState = false }: useStubedAppBridgeProps): IAppBridgeNative => {
+const useStubedAppBridge = ({
+    blockSettings = {},
+    blockAssets = {},
+    editorState = false,
+    openAssetChooser = () => null,
+    closeAssetChooser = () => null,
+}: useStubedAppBridgeProps): IAppBridgeNative => {
     const appBridge = new AppBridgeNativeMock(0, 0);
 
     cy.window().then((window) => {
         (window as unknown as Window).blockSettings = { 0: blockSettings };
+        (window as unknown as Window).emitter = {
+            on: () => null,
+            off: () => null,
+            emit: () => null,
+        };
+        stub((window as unknown as Window).emitter, 'on');
+        stub((window as unknown as Window).emitter, 'off');
+        stub((window as unknown as Window).emitter, 'emit');
     });
+
     stub(appBridge, 'getBlockSettings').returns(new Promise((resolve) => resolve(blockSettings)));
     stub(appBridge, 'getEditorState').returns(editorState);
+    stub(appBridge, 'openAssetChooser').callsFake(openAssetChooser);
+    stub(appBridge, 'closeAssetChooser').callsFake(closeAssetChooser);
+    stub(appBridge, 'getBlockAssets').callsFake(() => new Promise((resolve) => resolve(blockAssets)));
 
     return appBridge;
 };
@@ -27,9 +49,9 @@ type withAppBridgeStubsProps = { appBridge: IAppBridgeNative };
 
 export function withAppBridgeStubs<T>(
     WrappedComponent: ComponentType<T>,
-    props: useStubedAppBridgeProps
+    props?: useStubedAppBridgeProps
 ): [ComponentType<Omit<T, keyof withAppBridgeStubsProps>>, IAppBridgeNative] {
-    const appBridge = useStubedAppBridge(props);
+    const appBridge = useStubedAppBridge(props ?? {});
     const ComponentWithAppBridgeStubs = (props: Omit<T, keyof withAppBridgeStubsProps>) => {
         return <WrappedComponent appBridge={appBridge} {...(props as T)} />;
     };
