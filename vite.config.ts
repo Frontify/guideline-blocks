1f31132310bc8294d0f5c5b9e8326d06cdd7ef83
Samuel Alev <samuel.alev@frontify.com>
2022-05-02 21:19:46 +0200

    chore: allow linked package in tests

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
+    server: {
+        fs: {
+            // INFO: Allow linked packages `../..`.
+            strict: false,
+        },
+    },
+    optimizeDeps: {
+        // INFO: Remove possible linked packages from the optimization.
+        include: ["@frontify/arcade", "@frontify/app-bridge"],
+    },
 });
