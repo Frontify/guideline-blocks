9b548e19b0967e92a306b22c1dbef1446112b5af
Marco Bohler <triggertoo@gmail.com>
2022-06-10 02:20:50 +0200

    add example

---
+++ examples/guideline-design-tokens/src/ExampleGuidelineDesignTokensBlock.spec.ct.tsx
@@ -0,0 +1,61 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import { mount } from '@cypress/react';
+import { withAppBridgeStubs } from '@frontify/app-bridge';
+import type { Emitter } from 'mitt';
+import mitt from 'mitt';
+import { ExampleGuidelineDesignTokensBlock } from './ExampleGuidelineDesignTokensBlock';
+
+const EXAMPLE_GUIDELINE_DESIGN_TOKENS_BLOCK_SELECTOR = '[data-test-id="example-guideline-design-tokens-block"]';
+
+declare global {
+    interface Window {
+        emitter: Emitter<any>;
+    }
+}
+
+window.emitter = mitt();
+
+describe('Example Guideline Design Token Block', () => {
+    before(() => {
+        document.body.setAttribute('data-hub', '1');
+        document.body.setAttribute('data-document', '1');
+    });
+
+    beforeEach(() => {
+        cy.intercept(`${window.location.origin}/api/hub/settings/1/1`, {
+            statusCode: 200,
+            body: {
+                hub: {
+                    appearance: {
+                        heading1: {
+                            family: 'Arial',
+                            size: '24px',
+                        },
+                        heading2: {
+                            family: 'Helvetica Neue',
+                            size: '18px',
+                        },
+                    },
+                },
+            },
+        }).as('todo');
+    });
+
+    it('renders an example block with styles from the api', () => {
+        const [ExampleGuidelineDesignTokensBlockWithStubs] = withAppBridgeStubs(ExampleGuidelineDesignTokensBlock, {});
+
+        mount(<ExampleGuidelineDesignTokensBlockWithStubs />);
+        cy.get(EXAMPLE_GUIDELINE_DESIGN_TOKENS_BLOCK_SELECTOR).should('exist');
+
+        cy.get(`${EXAMPLE_GUIDELINE_DESIGN_TOKENS_BLOCK_SELECTOR} h1`)
+            .should('exist')
+            .and('have.css', 'font-family', 'Arial')
+            .and('have.css', 'font-size', '24px');
+
+        cy.get(`${EXAMPLE_GUIDELINE_DESIGN_TOKENS_BLOCK_SELECTOR} h2`)
+            .should('exist')
+            .should('have.css', 'font-family', '"Helvetica Neue"')
+            .and('have.css', 'font-size', '18px');
+    });
+});
--- packages/example/src/ExampleBlock.tsx
+++ packages/example/src/ExampleBlock.tsx
@@ -1,23 +1,13 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 import '@frontify/arcade-tokens/styles';
-import { useGuidelineDesignTokens } from '@frontify/guideline-blocks-shared';
-import { FC } from 'react';
 import 'tailwindcss/tailwind.css';
+import { FC } from 'react';
 
 export const ExampleBlock: FC = () => {
-    const { styleCategories, error, isLoading } = useGuidelineDesignTokens();
-
     return (
         <div data-test-id="example-block">
             <span className="tw-text-text tw-underline">A custom block in violet and underlined</span>
-            {(!isLoading && !error && (
-                <>
-                    <p style={styleCategories?.heading1}>Heading 1</p>
-                    <p style={styleCategories?.heading2}>Heading 2</p>
-                </>
-            )) ||
-                (isLoading && <>loading...</>)}
         </div>
     );
 };
