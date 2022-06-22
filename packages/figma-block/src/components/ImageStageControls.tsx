d86405f4e3ea0054da3e22a12efe2aee679cdbb1
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2022-06-22 15:28:21 +0200

    redisign and reposition zoom buttons

--- packages/figma-block/src/components/ImageStageControls.tsx
+++ packages/figma-block/src/components/ImageStageControls.tsx
@@ -1,15 +1,6 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import {
-    Button,
-    ButtonGroup,
-    ButtonSize,
-    ButtonStyle,
-    IconAddSimple,
-    IconExpand,
-    IconMinus,
-    IconReject,
-} from '@frontify/fondue';
+import { Button, ButtonStyle, IconAddSimple, IconExpand, IconMinus, IconReject } from '@frontify/fondue';
 import { DrawFullScreenActionButtonProps, DrawZoomInOutButtonsProps } from '../types';
 
 export const DrawFullScreenActionButton = ({ isFullScreen = false, onClick }: DrawFullScreenActionButtonProps) => (
@@ -19,10 +10,8 @@ export const DrawFullScreenActionButton = ({ isFullScreen = false, onClick }: Dr
 );
 
 export const DrawZoomInOutButtons = ({ onClickZoomIn, onClickZoomOut }: DrawZoomInOutButtonsProps) => (
-    <div className="tw-absolute tw-top-4 tw-left-4">
-        <ButtonGroup size={ButtonSize.Medium}>
-            <Button icon={<IconAddSimple />} onClick={onClickZoomOut} data-test-id="zoom-out" />
-            <Button icon={<IconMinus />} onClick={onClickZoomIn} data-test-id="zoom-in" />
-        </ButtonGroup>
+    <div className="tw-absolute tw-bottom-4 tw-right-4 tw-rounded tw-bg-black-90">
+        <Button icon={<IconAddSimple />} onClick={onClickZoomOut} data-test-id="zoom-out" />
+        <Button icon={<IconMinus />} onClick={onClickZoomIn} data-test-id="zoom-in" />
     </div>
 );
