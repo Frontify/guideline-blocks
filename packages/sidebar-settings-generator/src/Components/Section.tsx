775755be828db787a07fb2c73e82a62a5191e5d9
Sam Creasey <sam.creasey@frontify.com>
2022-07-05 11:39:38 +0200

    added accordion

---
+++ packages/sidebar-settings-generator/src/Components/Section.tsx
@@ -0,0 +1,17 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import { SettingBlock } from '@frontify/guideline-blocks-settings';
+import React, { FC } from 'react';
+import { Block } from './Block';
+
+type Props = {
+    blocks: SettingBlock[];
+};
+
+export const Section: FC<Props> = ({ blocks }) => (
+    <div className="tw-space-y-5" data-test-id="settings-sidebar-section">
+        {blocks.map((block) => (
+            <Block key={block.id} block={block} />
+        ))}
+    </div>
+);
--- packages/sidebar-settings-generator/src/Components/SettingsUI.tsx
+++ packages/sidebar-settings-generator/src/Components/SettingsUI.tsx
@@ -1,3 +1,35 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-export const SettingsUI = () => <div>SettingsUI</div>;
+import { Accordion, AccordionItem, FieldsetHeaderType } from '@frontify/fondue';
+import { useContext } from 'react';
+import { SettingsContext } from '../settingsContext';
+import { Section } from './Section';
+
+export const SettingsUI = () => {
+    const { settings } = useContext(SettingsContext);
+
+    const mainSection = settings?.main;
+    const otherSections = settings ? Object.entries(settings).filter(([key]) => key !== 'main') : [];
+
+    return (
+        <div>
+            {mainSection && <Section blocks={mainSection} />}
+            {otherSections.length > 0 && (
+                <Accordion>
+                    {otherSections.map(([title, settings]) => (
+                        <AccordionItem
+                            key={title}
+                            header={{
+                                children: title,
+                                type: FieldsetHeaderType.Accordion,
+                                active: true,
+                            }}
+                        >
+                            <Section blocks={settings} />
+                        </AccordionItem>
+                    ))}
+                </Accordion>
+            )}
+        </div>
+    );
+};
