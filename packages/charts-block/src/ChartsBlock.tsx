12515333e77919413fdf234bb4d81af7e7a56b64
Kevin Walker <kevin@smartive.ch>
2022-07-05 10:58:35 +0200

    switch to chartjs

--- packages/charts-block/src/ChartsBlock.tsx
+++ packages/charts-block/src/ChartsBlock.tsx
@@ -6,7 +6,6 @@ import { FC, useEffect, useState } from 'react';
 import 'tailwindcss/tailwind.css';
 import { BlockBarChart } from './charts/Bar';
 import { ChartType, ChartsBlockProps, Settings } from './types';
-import { ResponsiveContainer } from 'recharts';
 
 export const ChartsBlock: FC<ChartsBlockProps> = ({ appBridge }) => {
     const [blockSettings] = useBlockSettings<Settings>(appBridge);
@@ -14,14 +13,6 @@ export const ChartsBlock: FC<ChartsBlockProps> = ({ appBridge }) => {
     const [chartData, setChartData] = useState<string>('');
 
     const { type = ChartType.Bar } = blockSettings;
-
-    const data = [
-        { name: 'Page A', uv: 400 },
-        { name: 'Page B', uv: 500 },
-        { name: 'Page C', uv: 350 },
-    ];
-
-    console.log({ chartData });
 
     useEffect(() => {
         const url = blockAssets.chartData?.shift()?.origin_url;
@@ -34,18 +25,16 @@ export const ChartsBlock: FC<ChartsBlockProps> = ({ appBridge }) => {
 
     return (
         <div data-test-id="carts-block">
-            <ResponsiveContainer width="100%" height={600}>
-                {(() => {
-                    switch (type) {
-                        case ChartType.Bar:
-                            return <BlockBarChart data={data} />;
-                        case ChartType.Line:
-                            return <></>;
-                        case ChartType.Pie:
-                            return <></>;
-                    }
-                })()}
-            </ResponsiveContainer>
+            {(() => {
+                switch (type) {
+                    case ChartType.Bar:
+                        return <BlockBarChart data={chartData} />;
+                    case ChartType.Line:
+                        return <></>;
+                    case ChartType.Pie:
+                        return <></>;
+                }
+            })()}
         </div>
     );
 };
--- packages/charts-block/src/charts/Bar.tsx
+++ packages/charts-block/src/charts/Bar.tsx
@@ -1,28 +1,9 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 import { FC } from 'react';
-import { Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis } from 'recharts';
 import { ChartProps } from '../types';
 
 export const BlockBarChart: FC<ChartProps> = ({ data }) => {
-    return (
-        <BarChart data={data}>
-            <XAxis dataKey="name" stroke="#8884d8" />
-            <YAxis />
-            <Tooltip wrapperStyle={{ width: 100, backgroundColor: '#ccc' }} />
-            <Legend
-                width={100}
-                wrapperStyle={{
-                    top: 40,
-                    right: 20,
-                    backgroundColor: '#f5f5f5',
-                    border: '1px solid #d5d5d5',
-                    borderRadius: 3,
-                    lineHeight: '40px',
-                }}
-            />
-            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-            <Bar dataKey="uv" fill="#8884d8" barSize={30} />
-        </BarChart>
-    );
+    console.log({ data });
+    return <div>bar chart</div>;
 };
