0f149eba954bfe9d29a03cdf2e8ebcf703988a12
Marco Bohler <triggertoo@gmail.com>
2022-07-05 15:30:44 +0200

    use different padding

--- packages/sidebar-settings-generator/src/Components/EmptySection.tsx
+++ packages/sidebar-settings-generator/src/Components/EmptySection.tsx
@@ -3,7 +3,7 @@
 import { Button, ButtonStyle, IconAddSimple } from '@frontify/fondue';
 
 export const EmptySection = ({ onClick, text }: { onClick: () => void; text: string }) => (
-    <div className="tw-border tw-border-dashed tw-border-line tw-rounded tw-p-10 tw-flex tw-items-center tw-justify-center">
+    <div className="tw-border tw-border-dashed tw-border-line tw-rounded tw-p-6 tw-flex tw-items-center tw-justify-center">
         <Button onClick={onClick} style={ButtonStyle.Secondary}>
             <IconAddSimple /> {text}
         </Button>
