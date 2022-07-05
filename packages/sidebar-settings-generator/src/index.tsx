2dafb08a7bc3b8136a04270a89a814cc3fc873f7
Sam Creasey <sam.creasey@frontify.com>
2022-07-05 10:38:12 +0200

    added tabs

--- packages/sidebar-settings-generator/src/index.tsx
+++ packages/sidebar-settings-generator/src/index.tsx
@@ -1,8 +1,35 @@
+import { TabItem, TabSize, Tabs } from '@frontify/fondue';
+import { useState } from 'react';
 import { FC } from 'react';
+import { SettingsCode } from './Components/SettingsCode';
+import { SettingsUI } from './Components/SettingsUI';
 import { SettingsContext } from './settingsContext';
 
-const SidebarSettingsGenerator: FC = () => (
-    <SettingsContext.Provider value={{ settings: null }}>Block content</SettingsContext.Provider>
-);
+const SidebarSettingsGenerator: FC = () => {
+    const [activeTab, setActiveTab] = useState('ui');
+
+    const tabs = [
+        {
+            id: 'ui',
+            label: 'UI',
+            children: <SettingsUI />,
+        },
+        {
+            id: 'code',
+            label: 'Code',
+            children: <SettingsCode />,
+        },
+    ];
+
+    return (
+        <SettingsContext.Provider value={{ settings: null }}>
+            <Tabs activeItemId={activeTab} onChange={setActiveTab} size={TabSize.Large}>
+                {tabs.map((tab) => (
+                    <TabItem {...tab} key={tab.id} />
+                ))}
+            </Tabs>
+        </SettingsContext.Provider>
+    );
+};
 
 export default SidebarSettingsGenerator;
