a676fbbdd5fece4957452243838b29dba736f108
Luca Hochreutener <luca.hochreutener@frontify.com>
2022-07-05 10:40:23 +0200

    Use asset upload for data

--- packages/charts-block/src/ChartsBlock.tsx
+++ packages/charts-block/src/ChartsBlock.tsx
@@ -1,15 +1,30 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { useBlockSettings } from '@frontify/app-bridge';
+import { useBlockAssets, useBlockSettings } from '@frontify/app-bridge';
 import '@frontify/fondue-tokens/styles';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import 'tailwindcss/tailwind.css';
 import { ChartType, ChartsBlockProps, Settings } from './types';
 
 export const ChartsBlock: FC<ChartsBlockProps> = ({ appBridge }) => {
     const [blockSettings] = useBlockSettings<Settings>(appBridge);
+    const { blockAssets } = useBlockAssets(appBridge);
+    const [chartData, setChartData] = useState<string>('');
 
     const { type = ChartType.Bar } = blockSettings;
 
-    return <div data-test-id="carts-block">Charts block with type {type}!</div>;
+    useEffect(() => {
+        const url = blockAssets.chartData?.shift()?.origin_url;
+        if (url !== undefined) {
+            fetch(url)
+                .then((response) => response.text())
+                .then((text) => setChartData(text));
+        }
+    }, [blockAssets, setChartData]);
+
+    return (
+        <div data-test-id="carts-block">
+            Charts block with type {type} and data: {chartData}!
+        </div>
+    );
 };
