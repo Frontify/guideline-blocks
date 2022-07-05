aede80b5599c0284a37f2375605d53781d8c70d3
Raphi <raphael@smartive.ch>
2022-07-05 11:58:04 +0200

    feat(settingsGenerator): Add Codeblock with copyToClipboard Functionality

--- packages/sidebar-settings-generator/src/SettingsContext.ts
+++ packages/sidebar-settings-generator/src/SettingsContext.ts
@@ -1,8 +1,36 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
+import { IconEnum } from '@frontify/fondue';
 import { createContext } from 'react';
 import { SidebarSettingsGeneratorContext } from './types';
 
 export const SettingsContext = createContext<SidebarSettingsGeneratorContext>({
-    settings: null,
+    settings: {
+        main: [
+            {
+                id: 'STYLE_ID',
+                type: 'dropdown',
+                choices: [
+                    {
+                        value: 'true',
+                        icon: IconEnum.Iframe,
+                        label: 'Story (with add-ons)',
+                    },
+                    {
+                        value: 'StorybookStyle.WithoutAddons',
+                        icon: IconEnum.Iframe,
+                        label: 'Story (no add-ons)',
+                    },
+                ],
+            },
+        ],
+        content: [
+            {
+                id: 'url',
+                label: 'Link',
+                type: 'input',
+                placeholder: 'URL_INPUT_PLACEHOLDER',
+            },
+        ],
+    },
 });
