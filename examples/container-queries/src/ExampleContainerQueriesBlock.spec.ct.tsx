76abcbdfbf586c7326b1c3e3d9d3e34135dde9e8
Samuel Alev <samuel.alev@frontify.com>
2022-07-15 10:59:18 +0200

    feat: add example for responsive blocks

---
+++ examples/container-queries/src/ExampleContainerQueriesBlock.spec.ct.tsx
@@ -0,0 +1,30 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import { mount } from '@cypress/react';
+import { PocContainerQueriesBlock } from './ExampleContainerQueriesBlock';
+
+const EXAMPLE_CONTAINER_QUERIES_BLOCK_SELECTOR = '[data-test-id="example-container-queries-block"]';
+const BREAKPOINT = '[data-test-id="breakpoint"]';
+
+describe('Example Container Queries Block', () => {
+    it('renders an example block', () => {
+        mount(<PocContainerQueriesBlock />);
+        cy.get(EXAMPLE_CONTAINER_QUERIES_BLOCK_SELECTOR).should('exist');
+    });
+
+    it('renders with different breakpoints', () => {
+        mount(<PocContainerQueriesBlock />);
+
+        cy.viewport(100, 2000);
+        cy.get(BREAKPOINT).should('contain', 'sm');
+
+        cy.viewport(700, 2000);
+        cy.get(BREAKPOINT).should('contain', 'md');
+
+        cy.viewport(1000, 2000);
+        cy.get(BREAKPOINT).should('contain', 'l');
+
+        cy.viewport(2000, 2000);
+        cy.get(BREAKPOINT).should('contain', 'xl');
+    });
+});
---
+++ examples/container-queries/src/index.tsx
@@ -0,0 +1,3 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+export { PocContainerQueriesBlock as default } from './ExampleContainerQueriesBlock';
