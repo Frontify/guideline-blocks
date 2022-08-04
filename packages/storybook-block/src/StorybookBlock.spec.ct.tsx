3a099a315b9e77418c61ff156c7a02b7ba191010
Raphi <raphael@smartive.ch>
2022-08-04 10:33:35 +0200

    feat(Storybook Block):Add more tests for decoding entities

--- packages/storybook-block/src/StorybookBlock.spec.ct.tsx
+++ packages/storybook-block/src/StorybookBlock.spec.ct.tsx
@@ -69,9 +69,22 @@ describe('Storybook Block', () => {
         cy.get(IframeSelector).should('have.css', 'border-radius', '5px');
     });
 
-    it('decodes URL', () => {
-        const encodedUrl = 'https://fondue-components.frontify.com/?path&#61;/story/tokens--alias-tokens';
-        const decodedUrl = 'https://fondue-components.frontify.com/?path=/story/tokens--alias-tokens';
-        expect(decodedUrl).to.equal(decodeEntities(encodedUrl));
+    describe('decodeEntities', () => {
+        it('decodes URL', () => {
+            const encodedUrl = 'https://fondue-components.frontify.com/?path&#61;/story/tokens--alias-tokens';
+            const decodedUrl = 'https://fondue-components.frontify.com/?path=/story/tokens--alias-tokens';
+            expect(decodedUrl).to.equal(decodeEntities(encodedUrl));
+        });
+
+        it('does nothing with already decoded URL', () => {
+            const decodedUrl = 'https://fondue-components.frontify.com/?path=/story/tokens--alias-tokens';
+            expect(decodedUrl).to.equal(decodeEntities(decodedUrl));
+        });
+
+        it('decodes characters', () => {
+            const encodedChars = '&#162;this&#38;&#174;&#60;is&#62;&#38;&#34&#39;awesome&#162;&#163;';
+            const decodedChats = '¢this&®<is>&"\'awesome¢£';
+            expect(decodedChats).to.equal(decodeEntities(encodedChars));
+        });
     });
 });
--- packages/storybook-block/src/utilities.ts
+++ packages/storybook-block/src/utilities.ts
@@ -1,3 +1,5 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
 export const decodeEntities = (encodedString: string): string => {
     if (!encodedString) {
         return '';
