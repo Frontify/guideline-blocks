fdd338426ad85f1da2661b8ed258fb8412d662dc
Samuel Alev <samuel.alev@frontify.com>
2022-08-04 14:32:21 +0200

    refactor: simplify callout block (#287)

--- packages/callout-block/src/CalloutBlock.tsx
+++ packages/callout-block/src/CalloutBlock.tsx
@@ -3,42 +3,11 @@
 import { useBlockAssets, useBlockSettings, useEditorState } from '@frontify/app-bridge';
 import { RichTextEditor } from '@frontify/fondue';
 import '@frontify/fondue-tokens/styles';
-import { Radius, joinClassNames, radiusStyleMap, useGuidelineDesignTokens } from '@frontify/guideline-blocks-shared';
-import { CSSProperties, FC, createRef, useEffect, useState } from 'react';
+import { joinClassNames, radiusStyleMap, useGuidelineDesignTokens } from '@frontify/guideline-blocks-shared';
+import { FC } from 'react';
 import 'tailwindcss/tailwind.css';
 import { ICON_ASSET_ID } from './settings';
-import {
-    Alignment,
-    BlockSettings,
-    CalloutBlockProps,
-    CustomPaddingStyles,
-    Padding,
-    Type,
-    Width,
-    alignmentMap,
-    outerWidthMap,
-    paddingMap,
-    typeMap,
-} from './types';
-
-const getInnerDivClassName = (
-    type: Type,
-    width: Width,
-    alignment: Alignment,
-    isCustomPadding: boolean,
-    padding: Padding,
-    isCustomRadius: boolean,
-    cornerRadius: Radius
-): string =>
-    joinClassNames([
-        `tw-flex tw-items-center tw-text-white ${typeMap[type]}`,
-        width === Width.FullWidth && alignmentMap[alignment],
-        !isCustomPadding && padding && paddingMap[padding],
-        !isCustomRadius && cornerRadius && radiusStyleMap[cornerRadius],
-    ]);
-
-const getOuterDivClassName = (width: Width, alignment: Alignment): string =>
-    joinClassNames([outerWidthMap[width], width === Width.HugContents && alignmentMap[alignment]]);
+import { BlockSettings, CalloutBlockProps, Width, alignmentMap, outerWidthMap, paddingMap, typeMap } from './types';
 
 export const CalloutBlock: FC<CalloutBlockProps> = ({ appBridge }) => {
     const [blockSettings, setBlockSettings] = useBlockSettings<BlockSettings>(appBridge);
@@ -46,88 +15,50 @@ export const CalloutBlock: FC<CalloutBlockProps> = ({ appBridge }) => {
     const { blockAssets } = useBlockAssets(appBridge);
     const { designTokens } = useGuidelineDesignTokens();
 
-    const {
-        type = Type.Warning,
-        alignment = Alignment.Left,
-        iconSwitch = true,
-        width = Width.FullWidth,
-        paddingChoice = Padding.M,
-        extendedRadiusChoice = Radius.None,
-        extendedRadiusTopLeft = '0px',
-        extendedRadiusTopRight = '0px',
-        extendedRadiusBottomLeft = '0px',
-        extendedRadiusBottomRight = '0px',
-        paddingTop = '0px',
-        paddingBottom = '0px',
-        paddingLeft = '0px',
-        paddingRight = '0px',
-        hasCustomPadding,
-        hasExtendedCustomRadius,
-        textValue,
-    } = blockSettings;
+    const containerDivClassNames = joinClassNames([
+        outerWidthMap[blockSettings.width],
+        blockSettings.width === Width.HugContents && alignmentMap[blockSettings.alignment],
+    ]);
 
-    const [customPaddingStyle, setCustomPaddingStyle] = useState<CustomPaddingStyles>();
-    const [customCornerRadiusStyle, setCustomCornerRadiusStyle] = useState<CSSProperties>();
-    const [iconUrl, setIconUrl] = useState<string>();
-    const [iconAltText, setIconAltText] = useState<string>();
-    const blockRef = createRef<HTMLDivElement>();
+    const textDivClassNames = joinClassNames([
+        'tw-flex tw-items-center tw-text-white',
+        typeMap[blockSettings.type],
+        blockSettings.width === Width.FullWidth && alignmentMap[blockSettings.alignment],
+        !blockSettings.hasCustomPadding && paddingMap[blockSettings.paddingChoice],
+    ]);
 
-    useEffect(() => {
-        if (hasCustomPadding) {
-            setCustomPaddingStyle({
-                paddingTop,
-                paddingRight,
-                paddingBottom,
-                paddingLeft,
-            });
-        }
+    const customPaddingStyle = {
+        padding: blockSettings.hasCustomPadding
+            ? `${blockSettings.paddingTop} ${blockSettings.paddingRight} ${blockSettings.paddingBottom} ${blockSettings.paddingLeft}`
+            : '',
+    };
 
-        if (hasExtendedCustomRadius) {
-            setCustomCornerRadiusStyle({
-                borderRadius: [
-                    extendedRadiusTopLeft,
-                    extendedRadiusTopRight,
-                    extendedRadiusBottomRight,
-                    extendedRadiusBottomLeft,
-                ].join(' '),
-            });
-        }
+    const customCornerRadiusStyle = {
+        borderRadius: blockSettings.hasExtendedCustomRadius
+            ? `${blockSettings.extendedRadiusTopLeft} ${blockSettings.extendedRadiusTopRight} ${blockSettings.extendedRadiusBottomRight} ${blockSettings.extendedRadiusBottomLeft}`
+            : radiusStyleMap[blockSettings.extendedRadiusChoice],
+    };
 
-        if (iconSwitch && blockAssets[ICON_ASSET_ID]) {
-            const iconAsset = blockAssets[ICON_ASSET_ID][0];
-            setIconUrl(iconAsset.generic_url);
-            setIconAltText(`Callout Block Icon: ${iconAsset.title}`);
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [blockSettings, blockAssets]);
+    const iconUrl = blockSettings.iconSwitch ? blockAssets?.[ICON_ASSET_ID]?.[0]?.generic_url : '';
 
-    const onTextChange = (value: string): Promise<void> => setBlockSettings({ textValue: value });
+    const onTextChange = (value: string) => setBlockSettings({ textValue: value });
 
     return (
-        <div data-test-id="callout-block" className={getOuterDivClassName(width, alignment)}>
+        <div data-test-id="callout-block" className={containerDivClassNames}>
             <div
                 data-test-id="callout-wrapper"
-                className={getInnerDivClassName(
-                    type,
-                    width,
-                    alignment,
-                    hasCustomPadding,
-                    paddingChoice,
-                    hasExtendedCustomRadius,
-                    extendedRadiusChoice
-                )}
+                className={textDivClassNames}
                 style={{ ...customPaddingStyle, ...customCornerRadiusStyle }}
-                ref={blockRef}
             >
-                {iconSwitch && iconUrl && (
+                {blockSettings.iconSwitch && iconUrl && (
                     <span className="tw-mr-3 tw-flex-none tw-w-6 tw-h-6">
-                        <img data-test-id="callout-icon" alt={iconAltText} src={iconUrl} />
+                        <img data-test-id="callout-icon" alt="Callout icon" src={iconUrl} />
                     </span>
                 )}
                 <RichTextEditor
                     onTextChange={onTextChange}
                     readonly={!isEditing}
-                    value={textValue}
+                    value={blockSettings.textValue}
                     placeholder="Type your text here"
                     designTokens={designTokens ?? undefined}
                 />
