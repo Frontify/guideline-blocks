ef5f3a0d0c4554e9ee4d1d26ffea95389746c074
Dominique Moreno-Baltierra <conejoplata@gmail.com>
2022-08-15 11:32:21 +0200

    Re-creating color-scale-block

--- packages/color-scale-block/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    extends: ['@frontify/eslint-config-react'],
-    settings: {
-        react: {
-            version: 'detect',
-        },
-    },
-};
--- packages/color-scale-block/src/settings.ts
+++ packages/color-scale-block/src/settings.ts
@@ -1,143 +1,26 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { DropdownSize, IconEnum } from '@frontify/fondue';
-import { BlockSettings, Bundle, NotificationStyleType } from '@frontify/guideline-blocks-settings';
+import type { DropdownSize, IconEnum } from '@frontify/fondue';
+import type { BlockSettings } from '@frontify/guideline-blocks-settings';
 
 export const settings: BlockSettings = {
     main: [
         {
-            id: 'view',
+            id: 'main-dropdown',
             type: 'dropdown',
-            defaultValue: 'cards',
-            size: DropdownSize.Large,
+            defaultValue: 'custom_block',
+            size: 'Large' as DropdownSize.Large,
+            disabled: true,
             choices: [
                 {
-                    value: 'list',
-                    icon: IconEnum.ListBullets,
-                    label: 'List',
-                },
-                {
-                    value: 'drops',
-                    icon: IconEnum.Drops,
-                    label: 'Drops',
-                },
-                {
-                    value: 'cards',
-                    icon: IconEnum.Card,
-                    label: 'Cards',
+                    value: 'custom_block',
+                    icon: 'Snippet' as IconEnum.Snippet,
+                    label: 'Custom Block',
                 },
             ],
         },
     ],
-    // content: [
-    //     {
-    //         id: 'colorspaces',
-    //         type: 'checklist',
-    //         defaultValue: ['hex', 'rgb'],
-    //         label: 'Color Spaces',
-    //         choices: [
-    //             {
-    //                 id: 'hex',
-    //                 label: 'HEX',
-    //             },
-    //             {
-    //                 id: 'rgb',
-    //                 label: 'RGB',
-    //             },
-    //             {
-    //                 id: 'cmyk',
-    //                 label: 'CMYK',
-    //                 tooltip: {
-    //                     content:
-    //                         'We automatically convert color values to CMYK. \n These values might differ, depending on what color profile you use. \n You can always change the values manually here.',
-    //                 },
-    //             },
-    //             {
-    //                 id: 'cmyk_coated',
-    //                 label: 'CMYK Coated',
-    //             },
-    //             {
-    //                 id: 'cmyk_uncoated',
-    //                 label: 'CMYK Uncoated',
-    //             },
-    //             {
-    //                 id: 'cmyk_newspaper',
-    //                 label: 'CMYK Newspaper',
-    //             },
-    //             {
-    //                 id: 'pantone',
-    //                 label: 'Pantone',
-    //             },
-    //             {
-    //                 id: 'pantone_coated',
-    //                 label: 'Pantone Coated',
-    //             },
-    //             {
-    //                 id: 'pantone_uncoated',
-    //                 label: 'Pantone Uncoated',
-    //             },
-    //             {
-    //                 id: 'pantone_cp',
-    //                 label: 'Pantone CP',
-    //             },
-    //             {
-    //                 id: 'pantone_textile',
-    //                 label: 'Pantone Textile',
-    //             },
-    //             {
-    //                 id: 'pantone_plastics',
-    //                 label: 'Pantone Plastics',
-    //             },
-    //             {
-    //                 id: 'ral',
-    //                 label: 'RAL',
-    //             },
-
-    //             {
-    //                 id: 'variable',
-    //                 label: 'SCSS/LESS',
-    //             },
-    //             {
-    //                 id: 'lab',
-    //                 label: 'LAB',
-    //             },
-    //             {
-    //                 id: 'ncs',
-    //                 label: 'NCS',
-    //             },
-
-    //             {
-    //                 id: 'hks',
-    //                 label: 'HKS',
-    //             },
-    //             {
-    //                 id: 'three_m',
-    //                 label: '3M',
-    //             },
-    //             {
-    //                 id: 'oracal',
-    //                 label: 'Oracal',
-    //             },
-    //         ],
-    //         showClearAndSelectAllButtons: true,
-    //         columns: 2,
-    //     },
-    //     {
-    //         id: 'colorspacesInfo',
-    //         type: 'notification',
-    //         title: 'If there is no Color Space selected, viewers might miss some information or the Block could lack visual appeal.',
-    //         styles: {
-    //             type: NotificationStyleType.Warning,
-    //         },
-    //         show: (bundle: Bundle) => {
-    //             const colorSpacesValue = bundle.getBlock('colorspaces')?.value;
-
-    //             return !colorSpacesValue || (colorSpacesValue as string[]).length === 0;
-    //         },
-    //     },
-    // ],
 };
-
 
 // import { IconEnum, /*DropdownSize,*/ MultiInputLayout } from '@frontify/fondue';
 // import { BlockSettings } from '@frontify/guideline-blocks-settings';
