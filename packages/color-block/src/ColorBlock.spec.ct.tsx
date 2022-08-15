d08581f7036a4962cb7599021043ea6f01762caf
Samuel Alev <samuel.alev@frontify.com>
2022-08-15 11:53:49 +0200

    test: use correct import

--- packages/color-block/src/ColorBlock.spec.ct.tsx
+++ packages/color-block/src/ColorBlock.spec.ct.tsx
@@ -1,14 +1,14 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 import { mount } from '@cypress/react';
-import { withAppBridgeStubs } from '@frontify/app-bridge';
+import { withAppBridgeBlockStubs } from '@frontify/app-bridge';
 import { ColorBlock } from './ColorBlock';
 
 const ColorBlockSelector = '[data-test-id="color-block"]';
 
 describe('Color Block', () => {
     it('renders the color block', () => {
-        const [ColorBlockWithStubs] = withAppBridgeStubs(ColorBlock, {});
+        const [ColorBlockWithStubs] = withAppBridgeBlockStubs(ColorBlock, {});
 
         mount(<ColorBlockWithStubs />);
         cy.get(ColorBlockSelector).should('exist');
