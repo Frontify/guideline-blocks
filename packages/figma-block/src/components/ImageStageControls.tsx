e221dc85b8b3a3cb882ddebc67238b4423280fda
Kevin Walker <kevin@smartive.ch>
2022-08-17 16:49:16 +0200

    update figma block

--- packages/figma-block/src/components/ImageStageControls.tsx
+++ packages/figma-block/src/components/ImageStageControls.tsx
@@ -1,17 +1,21 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { Button, ButtonStyle, IconAddSimple, IconExpand, IconMinus, IconReject } from '@frontify/fondue';
+import { Button, ButtonStyle, IconArrowExpand, IconCross, IconMinus, IconPlus } from '@frontify/fondue';
 import { DrawFullScreenActionButtonProps, DrawZoomInOutButtonsProps } from '../types';
 
 export const DrawFullScreenActionButton = ({ isFullScreen = false, onClick }: DrawFullScreenActionButtonProps) => (
     <div className="tw-invisible group-hover:tw-visible tw-absolute tw-top-4 tw-right-4">
-        <Button icon={isFullScreen ? <IconReject /> : <IconExpand />} style={ButtonStyle.Secondary} onClick={onClick} />
+        <Button
+            icon={isFullScreen ? <IconCross /> : <IconArrowExpand />}
+            style={ButtonStyle.Secondary}
+            onClick={onClick}
+        />
     </div>
 );
 
 export const DrawZoomInOutButtons = ({ onClickZoomIn, onClickZoomOut }: DrawZoomInOutButtonsProps) => (
     <div className="tw-invisible group-hover:tw-visible tw-absolute tw-bottom-4 tw-right-4 tw-rounded tw-bg-black-90">
-        <Button icon={<IconAddSimple />} onClick={onClickZoomOut} data-test-id="zoom-out" />
+        <Button icon={<IconPlus />} onClick={onClickZoomOut} data-test-id="zoom-out" />
         <Button icon={<IconMinus />} onClick={onClickZoomIn} data-test-id="zoom-in" />
     </div>
 );
