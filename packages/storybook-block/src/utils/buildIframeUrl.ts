73fbe911c67ee7f194748dac7b7a4ae1da915a2b
Céline <42832501+celine-s@users.noreply.github.com>
2022-08-31 13:33:26 +0200

    fix(StorybookBlock): handle http:// and therefore localhost urls (#317)

--- packages/storybook-block/src/utils/buildIframeUrl.ts
+++ packages/storybook-block/src/utils/buildIframeUrl.ts
@@ -1,7 +1,7 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 import { StorybookPosition } from '../types';
-import { ensureHttps } from './ensureHttps';
+import { addMissingUrlProtocol } from './addMissingUrlProtocol';
 import { isValidStorybookUrl } from './isValidStorybookUrl';
 
 export const buildIframeUrl = (url: string, hasAddons: boolean, positioning: StorybookPosition): URL | null => {
@@ -9,7 +9,7 @@ export const buildIframeUrl = (url: string, hasAddons: boolean, positioning: Sto
         return null;
     }
 
-    const iframeUrl = new URL(ensureHttps(url));
+    const iframeUrl = new URL(addMissingUrlProtocol(url));
     iframeUrl.searchParams.set('nav', 'false');
 
     const positionValue = positioning === StorybookPosition.Horizontal ? 'right' : 'bottom';
