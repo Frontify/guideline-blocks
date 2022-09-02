a5cda3a93b6383d458f6711aaa5838ecb5019216
Samuel Alev <samuel.alev@frontify.com>
2022-09-02 11:37:41 +0200

    feat: add function possibility for choices (#320)

--- packages/block-settings/types/blocks/checklist.ts
+++ packages/block-settings/types/blocks/checklist.ts
@@ -1,11 +1,14 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { BaseBlock } from './base';
-import { Checkbox } from './checkbox';
+import type { BaseBlock } from './base';
+import type { Checkbox } from './checkbox';
+import type { AppBridgeBlock } from '@frontify/app-bridge';
+
+type ChoicesFn = (props: { appBridge: AppBridgeBlock }) => Promise<Checkbox[]>;
 
 export type ChecklistBlock = {
     type: 'checklist';
-    choices: Checkbox[];
+    choices: Checkbox[] | ChoicesFn;
     showClearAndSelectAllButtons?: boolean;
     columns?: 1 | 2;
 } & BaseBlock<string[] | null>;
