fd0331da3d0d610b9816708aa397ae534e9b172d
leko91 <nikola.milenkovic@frontify.com>
2022-09-19 14:39:26 +0200

    chore: sort import correctyl

--- packages/color-block/src/helpers/mapColorSpaces.ts
+++ packages/color-block/src/helpers/mapColorSpaces.ts
@@ -1,6 +1,7 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 import { FrontifyColor } from '@frontify/app-bridge';
+
 import { ColorSpaceInputValues } from '../types';
 
 type ColorSpace = {
