98935e49da0a19e9ec2be0a4fa117ac3821fde3c
leko91 <nikola.milenkovic@frontify.com>
2022-09-21 18:55:42 +0200

    remove unused

--- packages/color-block/tailwind.config.js
+++ packages/color-block/tailwind.config.js
@@ -5,16 +5,10 @@ module.exports = {
         preflight: false,
     },
     theme: {
-        boxShadow: {
-            line: '0 0 0 var(--line-width) var(--line-color)',
-            'line-strong': '0 0 0 var(--line-width) var(--line-color-strong)',
-        },
         extend: {
             boxShadow: {
-                line: '0 0 0 1px var(--line-color)',
                 'inner-line': 'inset 0 0 0 var(--line-width) var(--line-color)',
                 't-inner-line': 'inset 0 var(--line-width) 0 0 var(--line-color)',
-                'line-strong': '0 0 0 1px var(--line-color-strong)',
                 'inner-line-strong': 'inset 0 0 0 var(--line-width) var(--line-color-strong)',
                 't-inner-line-strong': 'inset 0 var(--line-width) 0 0 var(--line-color-strong)',
             },
