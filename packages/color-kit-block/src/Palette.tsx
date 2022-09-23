c36542277e0247e78e10730bb0655457dc8a2807
leko91 <nikola.milenkovic@frontify.com>
2022-09-23 12:34:46 +0200

    chore: show transparent colors with backgournd

--- packages/color-kit-block/src/Palette.tsx
+++ packages/color-kit-block/src/Palette.tsx
@@ -1,6 +1,8 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
+import { FrontifyColor } from '@frontify/app-bridge';
 import { Text, Tooltip, TooltipPosition } from '@frontify/fondue';
+import { toRgbaString } from '@frontify/guideline-blocks-shared';
 
 import { TooltipContent } from './TooltipContent';
 import type { PaletteProps } from './types';
@@ -12,25 +14,30 @@ export const Palette = ({ palette }: PaletteProps) => {
         <div data-test-id="palette" className="tw-flex tw-flex-col tw-space-y-2">
             <Text color="x-weak">{name}</Text>
             <div className="tw-flex tw-flex-wrap [&>div]:tw-h-6 [&>div:first-child>div]:tw-shadow-inner-line-first [&>div:last-child>div]:tw-shadow-inner-line-last">
-                {colors.map(({ id, hex }) => {
-                    if (!hex) {
-                        return <></>;
-                    }
+                {colors.map((color) => {
+                    const colorWithDecimalAlpha: FrontifyColor = {
+                        ...color,
+                        alpha: color.alpha && parseFloat((color.alpha / 255).toFixed(2)),
+                    };
 
                     return (
                         <Tooltip
                             withArrow
-                            key={id}
+                            key={color.id}
                             hoverDelay={0}
                             position={TooltipPosition.Right}
-                            content={<TooltipContent color={hex} />}
+                            content={<TooltipContent color={color.hex} />}
                             triggerElement={
-                                <div
-                                    key={id}
-                                    data-test-id="color"
-                                    style={{ backgroundColor: `#${hex}` }}
-                                    className="tw-w-6 tw-h-6 tw-inline-block tw-shadow-inner-line-y tw-transition-shadow  hover:tw-shadow-inner-line-strong"
-                                />
+                                <div className="tw-bg-[url('https://cdn.frontify.com/img/transparent.png')] tw-bg-[length:10px_10px]">
+                                    <div
+                                        key={color.id}
+                                        data-test-id="color"
+                                        style={{
+                                            backgroundColor: toRgbaString(colorWithDecimalAlpha),
+                                        }}
+                                        className="tw-w-6 tw-h-6 tw-overflow-hidden tw-shadow-inner-line-y tw-transition-shadow hover:!tw-shadow-inner-line-strong"
+                                    />
+                                </div>
                             }
                         />
                     );
