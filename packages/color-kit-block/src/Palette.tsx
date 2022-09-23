f1886c3653fa1613b2ccf1f1360ec351a7e02020
leko91 <nikola.milenkovic@frontify.com>
2022-09-23 12:02:51 +0200

    chore: create color boxes box shadow

--- packages/color-kit-block/src/Palette.tsx
+++ packages/color-kit-block/src/Palette.tsx
@@ -1,16 +1,17 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { Stack, Text, Tooltip, TooltipPosition } from '@frontify/fondue';
+import { Text, Tooltip, TooltipPosition } from '@frontify/fondue';
 
 import { TooltipContent } from './TooltipContent';
 import type { PaletteProps } from './types';
 
 export const Palette = ({ palette }: PaletteProps) => {
     const { name, colors = [] } = palette;
+
     return (
         <div data-test-id="palette" className="tw-flex tw-flex-col tw-space-y-2">
             <Text color="x-weak">{name}</Text>
-            <Stack padding="none" spacing="none">
+            <div className="tw-flex tw-flex-wrap [&>div]:tw-h-6 [&>div:first-child>div]:tw-shadow-inner-line-first [&>div:last-child>div]:tw-shadow-inner-line-last">
                 {colors.map(({ id, hex }) => {
                     if (!hex) {
                         return <></>;
@@ -20,7 +21,7 @@ export const Palette = ({ palette }: PaletteProps) => {
                         <Tooltip
                             withArrow
                             key={id}
-                            hoverDelay={100}
+                            hoverDelay={0}
                             position={TooltipPosition.Right}
                             content={<TooltipContent color={hex} />}
                             triggerElement={
@@ -28,13 +29,13 @@ export const Palette = ({ palette }: PaletteProps) => {
                                     key={id}
                                     data-test-id="color"
                                     style={{ backgroundColor: `#${hex}` }}
-                                    className="tw-w-6 tw-h-6 tw-inline-block"
+                                    className="tw-w-6 tw-h-6 tw-inline-block tw-shadow-inner-line-y tw-transition-shadow  hover:tw-shadow-inner-line-strong"
                                 />
                             }
                         />
                     );
                 })}
-            </Stack>
+            </div>
         </div>
     );
 };
