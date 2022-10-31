682000ea85a1e6acc10b2f0871a747eedcd13902
Samuel Alev <samuel.alev@frontify.com>
2022-10-31 09:51:50 +0100

    feat: add lifecycle hooks

--- packages/color-block/src/index.ts
+++ packages/color-block/src/index.ts
@@ -1,5 +1,6 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
+import { AppBridgeBlock } from '@frontify/app-bridge';
 import '@frontify/fondue-tokens/styles';
 import 'tailwindcss/tailwind.css';
 
@@ -9,4 +10,15 @@ import { settings } from './settings';
 export default {
     block: ColorBlock,
     settings,
+    onBlockCreated: async ({ appBridge }: { appBridge: AppBridgeBlock }) => {
+        const colorPalette = await appBridge.createColorPalette({ name: 'Untitled palette' });
+        appBridge.updateBlockSettings({ colorPaletteId: colorPalette.id });
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        //@ts-ignore
+        window.blockSettings[appBridge.getBlockId()]['colorPaletteId'] = colorPalette.id;
+    },
+    onBlockDeleted: async ({ appBridge }: { appBridge: AppBridgeBlock }) => {
+        const settings = await appBridge.getBlockSettings<{ colorPaletteId: number }>();
+        await appBridge.deleteColorPalette(settings.colorPaletteId);
+    },
 };
