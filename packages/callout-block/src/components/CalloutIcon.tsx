9a6449a1230bf71a874a083b9023ad2de652c78b
Céline <42832501+celine-s@users.noreply.github.com>
2022-11-09 16:49:55 +0100

    feat(CalloutBlock): add predefined icons (#404)

---
+++ packages/callout-block/src/components/CalloutIcon.tsx
@@ -0,0 +1,31 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import { IconInfo20, IconLightbulb20, IconMegaphone20 } from '@frontify/fondue';
+import { joinClassNames } from '@frontify/guideline-blocks-shared';
+import { ReactElement } from 'react';
+import { Icon } from '../types';
+
+type CalloutIconProps = {
+    iconType: Icon;
+    isActive: boolean;
+    iconUrl?: string;
+};
+export const CalloutIcon = ({ iconType, isActive, iconUrl }: CalloutIconProps) => (
+    <div
+        data-test-id="callout-icon"
+        className={joinClassNames([
+            isActive ? 'tw-opacity-100' : 'tw-opacity-30',
+            'tw-mr-2 tw-w-6 tw-h-full tw-flex tw-justify-center tw-items-center',
+        ])}
+    >
+        {calloutIconMap(iconUrl)[iconType]}
+    </div>
+);
+
+export const calloutIconMap = (iconUrl?: string): Record<Icon, ReactElement> => ({
+    none: <></>,
+    info: <IconInfo20 data-test-id="callout-icon-info" />,
+    lightbulb: <IconLightbulb20 />,
+    megaphone: <IconMegaphone20 />,
+    custom: <img data-test-id="callout-icon-custom" src={iconUrl} />,
+});
