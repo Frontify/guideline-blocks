cf9daf602c252970ea0a577fa2bee6cfab217bb5
Dominique Moreno-Baltierra <conejoplata@gmail.com>
2022-12-14 11:59:43 +0100

    calculateDefaultWidths helper includes padding and border width in its calculation

--- packages/color-scale-block/src/helpers/calculateDefaultColorWidth.ts
+++ packages/color-scale-block/src/helpers/calculateDefaultColorWidth.ts
@@ -1,14 +1,23 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 import { ColorScaleBlockRef } from '../types';
-import { DEFAULT_COLOR_SQUARE_WIDTH } from './constants';
+import {
+    COLOR_SCALE_BLOCK_BORDER_WIDTH,
+    COLOR_SCALE_BLOCK_OUTER_HORIZONTAL_PADDING,
+    COLOR_SQUARE_SPACING,
+    DEFAULT_COLOR_SQUARE_WIDTH,
+} from './constants';
 
 export const calculateDefaultColorWidth = (colorArrayLength: number, colorScaleBlockRef: ColorScaleBlockRef) => {
     if (!colorScaleBlockRef.current) {
         return DEFAULT_COLOR_SQUARE_WIDTH;
     }
 
-    const colorScaleBlockWidth = colorScaleBlockRef.current.getBoundingClientRect().width;
+    const colorScaleBlockWidth =
+        colorScaleBlockRef.current.getBoundingClientRect().width -
+        COLOR_SCALE_BLOCK_BORDER_WIDTH -
+        COLOR_SCALE_BLOCK_OUTER_HORIZONTAL_PADDING -
+        4;
 
     return colorScaleBlockWidth / colorArrayLength;
 };
