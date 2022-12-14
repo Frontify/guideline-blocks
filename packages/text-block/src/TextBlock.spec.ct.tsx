8db9cccae2857621a68a9eb424bc63d2e51a3d3e
Céline <celine@smartive.ch>
2022-12-14 10:42:16 +0100

    fix: tests

--- packages/text-block/src/TextBlock.spec.ct.tsx
+++ packages/text-block/src/TextBlock.spec.ct.tsx
@@ -22,32 +22,18 @@ describe('Text Block', () => {
         cy.get(RichTextEditor).find('[contenteditable=true]').should('not.exist');
     });
 
-    it('should be able input to two text blocks when in edit mode', () => {
-        const [TextBlockWithStubs] = withAppBridgeBlockStubs(TextBlock, {
-            blockSettings: {
-                columnNumber: 2,
-            },
-            editorState: true,
-        });
-
-        mount(<TextBlockWithStubs />);
-        cy.get(RichTextEditor).first().find('[contenteditable=true]').type('Hello, this is my text.').blur();
-        cy.get(RichTextEditor).first().find('[contenteditable=true]').contains('Hello, this is my text.');
-        cy.get(RichTextEditor).eq(1).find('[contenteditable=true]').type('And this is my other text.').blur();
-        cy.get(RichTextEditor).eq(1).find('[contenteditable=true]').contains('And this is my other text.');
-    });
-
     it('should render a text block with the correct amount of columns and correct spacing', () => {
         const [TextBlockWithStubs] = withAppBridgeBlockStubs(TextBlock, {
             blockSettings: {
                 columnNumber: 4,
+                content:
+                    'Hello, this is my text. Hello, this is my text. Hello, this is my text. Hello, this is my text. Hello, this is my text. Hello, this is my text. Hello, this is my text. Hello, this is my text. Hello, this is my text. ',
                 isColumnGutterCustom: true,
                 columnGutterCustom: '13px',
             },
         });
         mount(<TextBlockWithStubs />);
-        cy.get(RichTextEditor).should('have.length', 4);
-        cy.get(TextBlockSelector).should('have.class', 'tw-grid-cols-4');
+        cy.get(TextBlockSelector).should('have.class', 'lg:tw-columns-4');
         cy.get(TextBlockSelector).should('have.css', 'gap', '13px');
     });
 });
