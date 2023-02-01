563d933c970954ddc47f87195143f88fe2ddd3da
Kevin Walker <kevin@smartive.ch>
2023-02-01 08:06:19 +0100

    remove unused stuff

--- packages/image-block/src/ImageBlock.tsx
+++ packages/image-block/src/ImageBlock.tsx
@@ -1,18 +1,9 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { useBlockSettings, useEditorState } from '@frontify/app-bridge';
 import '@frontify/fondue-tokens/styles';
 import { BlockProps } from '@frontify/guideline-blocks-settings';
-import { useGuidelineDesignTokens } from '@frontify/guideline-blocks-shared';
 import 'tailwindcss/tailwind.css';
-import { Settings } from './types';
 
-export const ImageBlock = ({ appBridge }: BlockProps) => {
-    const isEditing = useEditorState(appBridge);
-    const [blockSettings] = useBlockSettings<Settings>(appBridge);
-    const { designTokens } = useGuidelineDesignTokens();
-
-    console.log({ isEditing, blockSettings, designTokens });
-
+export const ImageBlock = ({}: BlockProps) => {
     return <div data-test-id="image-block"></div>;
 };
