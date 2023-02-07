3c9559d52e98b2d63205071dac1eb9c99bb1162c
Kevin Walker <kevin@smartive.ch>
2023-02-07 09:02:34 +0100

    feat(ImageBlock): add settings (#456)

--- packages/image-block/src/types.ts
+++ packages/image-block/src/types.ts
@@ -3,3 +3,61 @@
 export type Settings = {
     hasLink?: boolean;
 };
+
+export enum ImageSecurity {
+    Global = 'Global',
+    Custom = 'Custom',
+}
+
+export enum Alignment {
+    Left = 'Left',
+    Center = 'Center',
+    Right = 'Right',
+}
+
+export enum CaptionPosition {
+    Below = 'Below',
+    Above = 'Above',
+    Right = 'Right',
+    Left = 'Left',
+}
+
+export enum Ratio {
+    Ratio2To1 = '2:1',
+    Ratio1To1 = '1:1',
+    Ratio1To2 = '1:2',
+}
+
+export enum Padding {
+    None = 'None',
+    Small = 'Small',
+    Medium = 'Medium',
+    Large = 'Large',
+}
+
+export const paddingValues: Record<Padding, string> = {
+    [Padding.None]: '0px',
+    [Padding.Small]: '10px',
+    [Padding.Medium]: '20px',
+    [Padding.Large]: '50px',
+};
+
+export enum BorderStyle {
+    Dotted = 'Dotted',
+    Dashed = 'Dashed',
+    Solid = 'Solid',
+}
+
+export enum CornerRadius {
+    None = 'None',
+    Small = 'Small',
+    Medium = 'Medium',
+    Large = 'Large',
+}
+
+export const radiusValues: Record<CornerRadius, string> = {
+    [CornerRadius.None]: '0px',
+    [CornerRadius.Small]: '2px',
+    [CornerRadius.Medium]: '4px',
+    [CornerRadius.Large]: '12px',
+};
