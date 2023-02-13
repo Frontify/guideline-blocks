2c6c812acb034fe708f62923ceca4edc58eede79
Daniel Fulop <daniel.fulop@frontify.com>
2023-02-13 11:19:37 +0100

    feat(BrandPositioning): add skeleton (#462)

---
+++ packages/brand-positioning-block/tailwind.config.js
@@ -0,0 +1,9 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+module.exports = {
+    presets: [require('@frontify/fondue/tailwind'), require('../../tailwind.config')],
+    content: ['src/**/*.{ts,tsx}'],
+    corePlugins: {
+        preflight: false,
+    },
+};
