cd72a01d86d1a6e46540d7ac203f79276ca16112
Thomas <thomas.tosch@frontify.com>
2023-02-14 18:12:06 +0100

    feat: added settings pannel and starting to implement the block
    Signed-off-by: Thomas <thomas.tosch@frontify.com>

--- packages/press-kit-block/src/PressKitBlock.tsx
+++ packages/press-kit-block/src/PressKitBlock.tsx
@@ -6,11 +6,5 @@ import { FC } from 'react';
 import 'tailwindcss/tailwind.css';
 
 export const PressKitBlock: FC<BlockProps> = ({ appBridge }) => {
-    appBridge.getProjectId(); // project id
-    // to get the asset ids refeer to the documentation
-    return (
-        <div data-test-id="example-block">
-            <span className="tw-text-violet-60 tw-underline">A custom block in violet and underlined test 5</span>
-        </div>
-    );
+    return <></>;
 };
