06c69f1862154b65455e3aa92029765d07409379
Samuel Alev <samuel.alev@frontify.com>
2023-02-14 16:28:04 +0100

    fix hmr

--- packages/self-recording-block/src/utilities/bindCameraToVideoElement.ts
+++ packages/self-recording-block/src/utilities/bindCameraToVideoElement.ts
@@ -6,8 +6,10 @@ export const bindCameraToVideoElement = async (videoElement: HTMLVideoElement) =
     return new Promise<void>(async (resolve, reject) => {
         try {
             videoElement.srcObject = await navigator.mediaDevices.getUserMedia(CAMERA_CONSTRAINTS);
-            videoElement.addEventListener('loadedmetadata', () => resolve());
-            videoElement.play();
+            videoElement.addEventListener('loadedmetadata', () => {
+                videoElement.play();
+                resolve();
+            });
         } catch (error) {
             reject('No permission to record the camera.');
         }
