96e008e93a05046fc554325c09b65c66b0c543de
silviojaeger <silvio.jaeger@hotmail.com>
2023-02-16 15:05:25 +0100

    adjust playerTooldbar sizing

--- packages/self-recording-block/src/components/PlayerToolbar.tsx
+++ packages/self-recording-block/src/components/PlayerToolbar.tsx
@@ -33,32 +33,40 @@ export const PlayerToolbar = ({
 }: PlayerToolbarProps) => (
     <div className="tw-flex tw-gap-1 tw-bg-base tw-rounded-lg tw-items-center tw-shadow tw-divide-x tw-divide-box-selected tw-border tw-border-box-selected tw-p-1">
         <div className="tw-flex tw-gap-1 tw-items-center tw-mr-2">
-            <Button
-                emphasis={ButtonEmphasis.Weak}
-                icon={isPlaying ? <IconPause16 /> : <IconPlay16 />}
-                onClick={onPlayPauseClicked}
-            />
-            <Dropdown
-                emphasis={TriggerEmphasis.Weak}
-                menuBlocks={[
-                    {
-                        ariaLabel: 'playback speed',
-                        id: 'playbackSpeed',
-                        menuItems: AVAILABLE_PLAYBACK_SPEEDS.map((value) => {
-                            return {
-                                id: value,
-                                title: `${value}x`,
-                            };
-                        }),
-                    },
-                ]}
-                activeItemId={playSpeed.toString()}
-                onChange={(speed) => {
-                    onPlaySpeedSelected(parseFloat(speed as string));
-                }}
-                size={DropdownSize.Small}
-            />
-            <Text>{time}</Text>
+            <div>
+                <Button
+                    emphasis={ButtonEmphasis.Weak}
+                    icon={isPlaying ? <IconPause16 /> : <IconPlay16 />}
+                    onClick={onPlayPauseClicked}
+                />
+            </div>
+
+            <div className="tw-flex tw-w-20">
+                <Dropdown
+                    emphasis={TriggerEmphasis.Weak}
+                    menuBlocks={[
+                        {
+                            ariaLabel: 'playback speed',
+                            id: 'playbackSpeed',
+                            menuItems: AVAILABLE_PLAYBACK_SPEEDS.map((value) => {
+                                return {
+                                    id: value,
+                                    title: `${value}x`,
+                                };
+                            }),
+                        },
+                    ]}
+                    activeItemId={playSpeed.toString()}
+                    onChange={(speed) => {
+                        onPlaySpeedSelected(parseFloat(speed as string));
+                    }}
+                    size={DropdownSize.Small}
+                />
+            </div>
+
+            <div className="tw-pl-2 tw-w-12">
+                <Text>{time}</Text>
+            </div>
         </div>
         {onDeleteClick ? (
             <div className="tw-pl-1">
