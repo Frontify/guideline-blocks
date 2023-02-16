f940f2c65abae80e555050c19cbbd92449c7d9c3
Daniel Fulop <daniel.fulop@frontify.com>
2023-02-16 09:38:59 +0100

    feat(BrandPositioning): add image upload (#470)

--- packages/brand-positioning-block/src/components/Item.tsx
+++ packages/brand-positioning-block/src/components/Item.tsx
@@ -4,9 +4,9 @@ import { joinClassNames } from '@frontify/guideline-blocks-shared';
 import { useDraggable } from '@dnd-kit/core';
 import { ItemProps } from '../types';
 import { Toolbar } from './Toolbar';
+import { LoadingCircle } from '@frontify/fondue';
 
-export const Item = ({ src, xPosition, yPosition, id }: ItemProps) => {
-    const editing = true;
+export const Item = ({ src, xPosition, yPosition, id, deleteItem, isEditing }: ItemProps) => {
     const { attributes, isDragging, listeners, setNodeRef, transform } = useDraggable({
         id,
     });
@@ -23,18 +23,33 @@ export const Item = ({ src, xPosition, yPosition, id }: ItemProps) => {
                 ...transformStyle,
             }}
             ref={setNodeRef}
-            className="tw-h-[50px] tw-w-[50px] tw-bg-white tw-group tw-rounded tw-border tw-border-line tw-p-1 tw-absolute hover:tw-outline-offset-1 hover:tw-outline hover:tw-outline-1 hover:tw-outline-box-selected-inverse"
+            className={joinClassNames([
+                'tw-h-[50px] tw-w-[50px] tw-bg-white tw-group tw-rounded tw-border tw-border-line tw-p-1 tw-absolute ',
+                isEditing &&
+                    'hover:tw-outline-offset-1 hover:tw-outline hover:tw-outline-1 hover:tw-outline-box-selected-inverse',
+            ])}
         >
             <div
                 className={joinClassNames([
                     'tw-absolute tw-z-20 tw-bottom-[calc(100%-4px)] -tw-right-[3px]',
-                    editing && ' group-hover:tw-visible',
+                    isEditing && ' group-hover:tw-visible',
                     isDragging ? 'tw-visible' : 'tw-invisible',
                 ])}
             >
-                <Toolbar draggableAttributes={attributes} draggableListeners={listeners} isDragging={isDragging} />
+                <Toolbar
+                    deleteItem={deleteItem}
+                    draggableAttributes={attributes}
+                    draggableListeners={listeners}
+                    isDragging={isDragging}
+                />
             </div>
-            <img src={src} className="tw-w-full tw-h-full" />
+            {src ? (
+                <img src={src} className="tw-w-full tw-h-full tw-object-contain" />
+            ) : (
+                <div className="tw-w-full tw-h-full tw-flex tw-items-center tw-justify-center">
+                    <LoadingCircle />
+                </div>
+            )}
         </div>
     );
 };
