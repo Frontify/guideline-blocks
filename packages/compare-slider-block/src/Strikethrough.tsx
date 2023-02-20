f8573f4c008f84004d617875446062af9a01d4bc
urietberger <urs.rietberger@frontify.com>
2023-02-20 11:20:46 +0100

    fix(CompareSlider): use different color for strikethrough to be consistent with the do's & don't block

--- packages/compare-slider-block/src/Strikethrough.tsx
+++ packages/compare-slider-block/src/Strikethrough.tsx
@@ -3,7 +3,15 @@
 export const Strikethrough = () => {
     return (
         <svg width="100%" height="100%" viewBox="0 0 100% 100%" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <line x1="0" y1="100%" x2="100%" y2="0" stroke="#FE8EA2" strokeWidth="3" strokeLinecap="round" />
+            <line
+                x1="0"
+                y1="100%"
+                x2="100%"
+                y2="0"
+                stroke="rgba(227,20,13,1.00)"
+                strokeWidth="3"
+                strokeLinecap="round"
+            />
         </svg>
     );
 };
