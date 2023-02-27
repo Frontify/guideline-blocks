ea642d8e3b0c22a471525e5f6dafdc062a5d63c2
Kevin Walker <kevin@smartive.ch>
2023-02-27 16:16:16 +0100

    fix type

--- packages/shared/src/helpers/index.ts
+++ packages/shared/src/helpers/index.ts
@@ -4,5 +4,4 @@ export * from './defaultTokens';
 export * from './downloadAsset';
 export * from './hasRichTextValue';
 export * from './mapToGuidelineDesignTokens';
-export * from './mergeDeep';
 export * from './provideDefaultCalloutColors';
