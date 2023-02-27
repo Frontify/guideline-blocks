10b89fbd6c1fedc0a2d18cd8d14327b24ccd0d4c
ragi96 <rafael.giezendanner@frontify.com>
2023-02-27 16:45:12 +0100

    improved tailwind.confi.js

--- packages/audio-block/tailwind.config.js
+++ packages/audio-block/tailwind.config.js
@@ -1,7 +1,7 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 module.exports = {
-    presets: [require('@frontify/fondue/tailwind'), require('../../tailwind.config')],
+    presets: [require('@frontify/fondue/tailwind')],
     content: ['src/**/*.{ts,tsx}', '../shared/src/**/*.{ts,tsx}'],
     corePlugins: {
         preflight: false,
