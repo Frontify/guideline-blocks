232c73fe84eff39839fb4f62d5516057ea4d2fa7
ragi96 <rafael.giezendanner@frontify.com>
2023-03-03 15:36:18 +0100

    AddColorButton Tests

--- packages/gradient-block/src/components/AddColorButton.spec.ct.tsx
+++ packages/gradient-block/src/components/AddColorButton.spec.ct.tsx
@@ -6,7 +6,7 @@ import { AddColorButton } from './AddColorButton';
 const AddColorButtonSelector = '[data-test-id="add-color-button"]';
 
 describe('AddColorButton', () => {
-    it('renders a gradient block', () => {
+    it('renders an add color button', () => {
         mount(
             <AddColorButton
                 blockWidth={800}
@@ -16,5 +16,34 @@ describe('AddColorButton', () => {
             />
         );
         cy.get(AddColorButtonSelector).should('exist');
+    });
+
+    it('renders color with position left', () => {
+        mount(
+            <AddColorButton
+                blockWidth={800}
+                addButtonPositionLeft={42}
+                setShowColorModal={cy.stub()}
+                setCurrentlyEditingPosition={cy.stub()}
+            />
+        );
+        cy.get(AddColorButtonSelector).should('have.css', 'left', '42px');
+    });
+
+    it('add color button, click calls functions', () => {
+        const setShowColorModalSpy = cy.spy().as('setShowColorModal');
+        const setCurrentlyEditingPositionSpy = cy.spy().as('setCurrentlyEditingPosition');
+        mount(
+            <AddColorButton
+                blockWidth={800}
+                addButtonPositionLeft={42}
+                setShowColorModal={setShowColorModalSpy}
+                setCurrentlyEditingPosition={setCurrentlyEditingPositionSpy}
+            />
+        );
+
+        cy.get(AddColorButtonSelector).click();
+        cy.get('@setShowColorModal').should('have.been.calledWith', true);
+        cy.get('@setCurrentlyEditingPosition').should('have.been.calledWith', 5.25);
     });
 });
