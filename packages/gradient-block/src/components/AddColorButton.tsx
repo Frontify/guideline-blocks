495356502c2655a15182a91bdd041a272739cf86
ragi96 <rafael.giezendanner@frontify.com>
2023-03-07 13:00:52 +0100

    improved naming

--- packages/gradient-block/src/components/AddColorButton.tsx
+++ packages/gradient-block/src/components/AddColorButton.tsx
@@ -4,7 +4,7 @@ import { IconPlus12 } from '@frontify/fondue';
 import { AddColorButtonProps } from '../types';
 
 export const AddColorButton = ({
-    addButtonPositionLeft,
+    positionLeft,
     blockWidth,
     setCurrentlyEditingPosition,
     setShowColorModal,
@@ -18,9 +18,9 @@ export const AddColorButton = ({
         <div
             data-test-id="add-color-button"
             className="tw-absolute tw-top-1/2 -tw-translate-y-1/2 tw-bg-box-selected-strong tw-flex tw-items-center tw-justify-center tw-rounded tw-cursor-pointer tw-h-4 tw-w-4"
-            onClick={() => handleAdd(addButtonPositionLeft)}
+            onClick={() => handleAdd(positionLeft)}
             style={{
-                left: addButtonPositionLeft,
+                left: positionLeft,
             }}
         >
             <span className="tw-text-white tw-h-4 tw-w-4 tw-flex tw-justify-center tw-items-center tw-pt-0.5">
