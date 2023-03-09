8829d57853dd323bbfc15b12e915f7316af7f6bf
ragi96 <rafael.giezendanner@frontify.com>
2023-03-09 16:46:27 +0100

    removed fragment

--- packages/shared/src/components/DownloadButton/DownloadButton.tsx
+++ packages/shared/src/components/DownloadButton/DownloadButton.tsx
@@ -6,14 +6,12 @@ import { IconArrowCircleDown16 } from '@frontify/fondue';
 
 export const DownloadButton = ({ onDownload }: DownloadButtonProps) => {
     return (
-        <>
-            <button
-                data-test-id="download-button"
-                onClick={onDownload}
-                className="tw-rounded-full tw-mb-auto tw-bg-box-neutral-strong-inverse hover:tw-bg-box-neutral-strong-inverse-hover active:tw-bg-box-neutral-strong-inverse-pressed  tw-text-box-neutral-strong tw-outline tw-outline-1 tw-outline-offset-[1px] tw-p-1.5 tw-outline-line"
-            >
-                <IconArrowCircleDown16 />
-            </button>
-        </>
+        <button
+            data-test-id="download-button"
+            onClick={onDownload}
+            className="tw-rounded-full tw-mb-auto tw-bg-box-neutral-strong-inverse hover:tw-bg-box-neutral-strong-inverse-hover active:tw-bg-box-neutral-strong-inverse-pressed  tw-text-box-neutral-strong tw-outline tw-outline-1 tw-outline-offset-[1px] tw-p-1.5 tw-outline-line"
+        >
+            <IconArrowCircleDown16 />
+        </button>
     );
 };
