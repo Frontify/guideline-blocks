c8ef245a81ed376a03482aae3585050aa3724a9c
ragi96 <rafael.giezendanner@frontify.com>
2023-03-13 09:46:11 +0100

    changed hover handling of tooltip

--- packages/gradient-block/src/components/ColorTooltip.tsx
+++ packages/gradient-block/src/components/ColorTooltip.tsx
@@ -17,36 +17,39 @@ export const ColorTooltip = ({
     const { hoverProps, isHovered } = useHover({});
 
     const pointClasses = joinClassNames([
-        'tw-w-2 tw-h-2 tw-rounded-full tw- tw-bg-line-x-strong -tw-mt-[22px]',
+        'tw-w-2 tw-h-2 tw-rounded-full -tw-mt-[22px]',
         isHovered ? 'tw-bg-black-100' : 'tw-bg-black-20',
+    ]);
+
+    const tooltipClasses = joinClassNames([
+        'tw-inline-block tw-mt-2.5 -tw-ml-1',
+        isHovered ? 'tw-visible' : 'tw-invisible',
     ]);
 
     return (
         <div className="tw-absolute tw-z-10" style={{ left }} {...hoverProps}>
             <div data-test-id="color-points" className={pointClasses} />
-            {isHovered ? (
-                <div data-test-id="color-tooltip" role="tooltip" className="tw-inline-block tw-mt-2.5 -tw-ml-1">
-                    <div className="tw-absolute tw-w-2 tw-h-2 tw-left-0 tw-pointer-events-none -tw-top-[11px] before:tw-absolute before:tw-bg-white before:tw-w-2 before:tw-h-2 before:tw-rotate-45 before:tw-border before:tw-border-line before:tw-border-b-0 before:tw-border-r-0 before:tw-z-1000 before:tw-border-box-selected-strong" />
+            <div data-test-id="color-tooltip" role="tooltip" className={tooltipClasses}>
+                <div className="tw-absolute tw-w-2 tw-h-2 tw-left-0 tw-pointer-events-none -tw-top-[11px] before:tw-absolute before:tw-bg-white before:tw-w-2 before:tw-h-2 before:tw-rotate-45 before:tw-border before:tw-border-b-0 before:tw-border-r-0 before:tw-z-1000 before:tw-border-box-selected-strong" />
 
-                    <div className="tw-w-4 tw-h-4 tw-rounded tw-outline tw-outline-1 tw-outline-offset-2 tw-outline-box-selected-inverse">
-                        <div
-                            className="tw-absolute tw-w-4 tw-h-4 tw-rounded"
-                            style={{
-                                backgroundColor: toHexString(gradientColor.color),
-                            }}
-                        />
-                    </div>
-                    {!showColorModal ? (
-                        <EditAndDeleteColorBox
-                            color={gradientColor}
-                            gradientColors={gradientColors}
-                            setColors={setColors}
-                            setShowColorModal={setShowColorModal}
-                            setCurrentlyEditingPosition={setCurrentlyEditingPosition}
-                        />
-                    ) : null}
+                <div className="tw-w-4 tw-h-4 tw-rounded tw-outline tw-outline-1 tw-outline-offset-2 tw-outline-box-selected-inverse">
+                    <div
+                        className="tw-absolute tw-w-4 tw-h-4 tw-rounded"
+                        style={{
+                            backgroundColor: toHexString(gradientColor.color),
+                        }}
+                    />
                 </div>
-            ) : null}
+                {!showColorModal ? (
+                    <EditAndDeleteColorBox
+                        color={gradientColor}
+                        gradientColors={gradientColors}
+                        setColors={setColors}
+                        setShowColorModal={setShowColorModal}
+                        setCurrentlyEditingPosition={setCurrentlyEditingPosition}
+                    />
+                ) : null}
+            </div>
         </div>
     );
 };
