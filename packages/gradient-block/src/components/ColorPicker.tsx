459873fbccaa3c51be40b0387c30f5e18e60797e
ragi96 <rafael.giezendanner@frontify.com>
2023-03-13 13:35:25 +0100

    validate colorPosition

--- packages/gradient-block/src/components/ColorPicker.tsx
+++ packages/gradient-block/src/components/ColorPicker.tsx
@@ -13,6 +13,7 @@ import {
     TextInput,
     TextInputType,
     TooltipIcon,
+    Validation,
 } from '@frontify/fondue';
 import { useState } from 'react';
 import { ColorPickerProps, GradientColor } from '../types';
@@ -25,10 +26,14 @@ export const ColorPicker = ({
 }: ColorPickerProps) => {
     const actualColor = gradientColors.find((item) => item.position === currentlyEditingPosition);
     const defaultColor = { red: 0, green: 0, blue: 0, alpha: 1 };
-    const [colorPosition, setColorPosition] = useState(currentlyEditingPosition.toString());
+    const [colorPosition, setColorPosition] = useState(currentlyEditingPosition.toFixed(0));
     const [color, setColor] = useState<Color>(actualColor?.color ?? defaultColor);
+    const [colorPositionValidation, setColorPositionValidation] = useState<Validation>(Validation.Default);
 
     const editColor = () => {
+        if (colorPositionValidation === Validation.Error) {
+            return;
+        }
         const newGradientColors = gradientColors.map((item) => {
             if (item === actualColor) {
                 return {
@@ -47,6 +52,19 @@ export const ColorPicker = ({
             return a.position - b.position;
         });
         setColors(newGradientColors);
+    };
+
+    const setValidColorPosition = (value: string) => {
+        const valueAsNumber = parseInt(value);
+        if (valueAsNumber >= 0 && valueAsNumber <= 100) {
+            setColorPositionValidation(Validation.Default);
+            setColorPosition(valueAsNumber.toString());
+        } else if (value === '') {
+            setColorPositionValidation(Validation.Error);
+            setColorPosition(value);
+        } else {
+            setColorPositionValidation(Validation.Error);
+        }
     };
 
     return (
@@ -101,9 +119,7 @@ export const ColorPicker = ({
                         currentColor={color}
                         onClose={() => setShowColorModal(false)}
                         onSelect={setColor}
-                        onClick={() => {
-                            color && editColor();
-                        }}
+                        onClick={() => color && editColor()}
                     />
                     <span className="tw-flex tw-mt-6">
                         <Text color="weak">Stop</Text>
@@ -118,7 +134,12 @@ export const ColorPicker = ({
                             />
                         </span>
                     </span>
-                    <TextInput value={colorPosition} onChange={setColorPosition} type={TextInputType.Number} />
+                    <TextInput
+                        value={colorPosition}
+                        onChange={setValidColorPosition}
+                        type={TextInputType.Number}
+                        validation={colorPositionValidation}
+                    />
                 </div>
             </div>
         </Flyout>
