5935ad7bc887b7b78d72a5fde7d0ebfaa519569f
ragi96 <rafael.giezendanner@frontify.com>
2023-03-14 09:30:50 +0100

    added focus to thumbnailItems

--- packages/asset-kit/src/components/ThumbnailItem.tsx
+++ packages/asset-kit/src/components/ThumbnailItem.tsx
@@ -1,11 +1,19 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 import { Button, ButtonEmphasis, ButtonRounding, ButtonSize, IconCross16 } from '@frontify/fondue';
+import { joinClassNames } from '@frontify/guideline-blocks-shared';
+import { useFocusRing } from '@react-aria/focus';
 import { ThumbnailItemProps } from '../types';
 
 export const ThumbnailItem = ({ asset, isEditing, onRemoveAsset, thumbnailStyle }: ThumbnailItemProps) => {
+    const { isFocused, focusProps } = useFocusRing();
     return (
-        <div data-test-id="asset-kit-block-thumbnail" className="tw-aspect-square tw-group tw-relative">
+        <div
+            {...focusProps}
+            tabIndex={0}
+            data-test-id="asset-kit-block-thumbnail"
+            className="tw-aspect-square tw-group tw-relative"
+        >
             <img
                 className="tw-object-scale-down tw-w-full tw-h-full"
                 src={asset.previewUrl}
@@ -14,7 +22,10 @@ export const ThumbnailItem = ({ asset, isEditing, onRemoveAsset, thumbnailStyle 
             />
             {isEditing ? (
                 <div
-                    className="tw-invisible	group-hover:tw-visible tw-absolute tw-top-0.5 tw-right-0.5"
+                    className={joinClassNames([
+                        'group-hover:tw-visible tw-absolute tw-top-0.5 tw-right-0.5',
+                        (isFocused && 'tw-visible') || 'tw-invisible',
+                    ])}
                     data-test-id="asset-kit-block-remove-thumbnail"
                 >
                     <Button
