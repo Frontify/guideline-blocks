a5c9db097fce59004d011dfb4b962846563b95fd
ragi96 <rafael.giezendanner@frontify.com>
2023-03-15 08:24:27 +0100

    customPlugin for title

--- packages/asset-kit/src/components/InformationSection.tsx
+++ packages/asset-kit/src/components/InformationSection.tsx
@@ -1,13 +1,32 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 import { hasRichTextValue, useGuidelineDesignTokens } from '@frontify/guideline-blocks-shared';
-import { RichTextEditor } from '@frontify/fondue';
+import {
+    BoldPlugin,
+    ItalicPlugin,
+    PluginComposer,
+    ResetFormattingPlugin,
+    RichTextEditor,
+    SoftBreakPlugin,
+    StrikethroughPlugin,
+    TextStylePlugin,
+    UnderlinePlugin,
+} from '@frontify/fondue';
+import { Plugin, PluginProps } from '@frontify/fondue/dist/components/RichTextEditor/Plugins/Plugin';
 import { InformationSectionProps } from '../types';
+import { useMemo } from 'react';
 
 const DEFAULT_CONTENT_VALUE = '[{"type":"heading3","children":[{"text":""}]}]';
 
 export const InformationSection = ({ description, isEditing, setBlockSettings, title }: InformationSectionProps) => {
     const { designTokens } = useGuidelineDesignTokens();
+
+    const customTitlePlugins = useMemo(() => {
+        return new PluginComposer()
+            .setPlugin([new SoftBreakPlugin(), new TextStylePlugin() as Plugin<PluginProps>])
+            .setPlugin([new BoldPlugin(), new ItalicPlugin(), new UnderlinePlugin(), new StrikethroughPlugin()])
+            .setPlugin([new ResetFormattingPlugin()]);
+    }, []);
 
     const saveDescription = (newDescription: string) => {
         if (description !== newDescription) {
@@ -32,6 +51,7 @@ export const InformationSection = ({ description, isEditing, setBlockSettings, t
                         onBlur={saveTitle}
                         placeholder={isEditing ? 'Add a title here ...' : ''}
                         border={false}
+                        plugins={customTitlePlugins}
                     />
                 </div>
             ) : null}
