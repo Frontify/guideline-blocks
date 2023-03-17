d96fec132251c93a46ecd5c999df52524159717b
ragi96 <rafael.giezendanner@frontify.com>
2023-03-17 15:47:41 +0100

    accessibility improvements for the BlockItemWrapper

--- packages/asset-kit-block/src/components/ThumbnailItem.tsx
+++ packages/asset-kit-block/src/components/ThumbnailItem.tsx
@@ -1,40 +1,75 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { useState } from 'react';
-import { useFocusRing } from '@react-aria/focus';
-import { useKeyboard } from '@react-aria/interactions';
-import { mergeProps } from '@react-aria/utils';
+import { useState, useEffect } from 'react';
 import { Settings, ThumbnailItemProps } from '../types';
-import { ThumbnailToolbar } from '.';
-import { LoadingCircle } from '@frontify/fondue';
-import { joinClassNames } from '@frontify/guideline-blocks-shared';
-import { useBlockSettings } from '@frontify/app-bridge';
+import { IconImageStack20, LoadingCircle, IconTrashBin16, IconTrashBin20, IconArrowCircleUp20 } from '@frontify/fondue';
+import { BlockItemWrapper } from '@frontify/guideline-blocks-shared';
+import { AssetChooserObjectType, useBlockSettings, useFileInput, useAssetUpload } from '@frontify/app-bridge';
 import { thumbnailStyle } from '../helpers';
 
 export const ThumbnailItem = ({ asset, isEditing, appBridge, onRemoveAsset, onReplaceAsset }: ThumbnailItemProps) => {
-    const { isFocused, focusProps } = useFocusRing();
     const [isUploading, setIsUploading] = useState(false);
     const [blockSettings] = useBlockSettings<Settings>(appBridge);
-
-    const { keyboardProps } = useKeyboard({
-        onKeyUp: (event) => {
-            if ((event.key === 'Backspace' || event.key === 'Delete') && isEditing && isFocused) {
-                onRemoveAsset(asset.id);
-            }
-        },
+    const [openFileDialog, { selectedFiles }] = useFileInput({ accept: 'image/*' });
+    const [uploadFile, { results: uploadResults, doneAll }] = useAssetUpload({
+        onUploadProgress: () => !isUploading && setIsUploading(true),
     });
 
+    const openAssetChooser = () => {
+        appBridge.openAssetChooser(
+            async (result) => {
+                onReplaceAsset(asset.id, result[0].id);
+                appBridge.closeAssetChooser();
+            },
+            {
+                objectTypes: [AssetChooserObjectType.ImageVideo],
+            }
+        );
+    };
+
+    useEffect(() => {
+        if (selectedFiles) {
+            uploadFile(selectedFiles);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [selectedFiles]);
+
+    useEffect(() => {
+        if (doneAll && uploadResults) {
+            onReplaceAsset(asset.id, uploadResults[0].id);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [doneAll, uploadResults]);
+
     return (
-        <div
-            {...mergeProps(focusProps, keyboardProps)}
-            tabIndex={0}
-            data-test-id="block-thumbnail"
-            className={joinClassNames([
-                'tw-aspect-square tw-group tw-relative tw-outline-1 tw-outline-offset-1 tw-outline-box-selected-inverse focus:tw-outline ',
-                isEditing && 'hover:tw-outline',
-            ])}
+        <BlockItemWrapper
+            shouldHideWrapper={!isEditing}
+            toolbarFlyoutItems={[
+                [
+                    {
+                        title: 'Replace with upload',
+                        icon: <IconArrowCircleUp20 />,
+                        onClick: openFileDialog,
+                    },
+                    {
+                        title: 'Replace with asset',
+                        icon: <IconImageStack20 />,
+                        onClick: openAssetChooser,
+                    },
+                ],
+                [
+                    {
+                        title: 'Delete',
+                        icon: <IconTrashBin20 />,
+                        onClick: () => onRemoveAsset(asset.id),
+                    },
+                ],
+            ]}
+            toolbarItems={[
+                { icon: <IconTrashBin16 />, tooltip: 'Delete Item', onClick: () => onRemoveAsset(asset.id) },
+            ]}
         >
-            <>
+            <div data-test-id="block-thumbnail" className="tw-aspect-square">
                 {isUploading ? (
                     <div className="tw-relative tw-w-full tw-h-full" style={thumbnailStyle(blockSettings)}>
                         <div className="tw-absolute tw-top-1/2 tw-left-1/2 -tw-translate-y-1/2 -tw-translate-x-1/2">
@@ -50,19 +85,7 @@ export const ThumbnailItem = ({ asset, isEditing, appBridge, onRemoveAsset, onRe
                         alt={asset.title}
                     />
                 )}
-
-                {isEditing ? (
-                    <ThumbnailToolbar
-                        appBridge={appBridge}
-                        asset={asset}
-                        isFocused={isFocused}
-                        onRemoveAsset={onRemoveAsset}
-                        onReplaceAsset={onReplaceAsset}
-                        setIsUploading={setIsUploading}
-                        isUploading={isUploading}
-                    />
-                ) : null}
-            </>
-        </div>
+            </div>
+        </BlockItemWrapper>
     );
 };
--- packages/shared/src/components/BlockItemWrapper/BlockItemWrapper.tsx
+++ packages/shared/src/components/BlockItemWrapper/BlockItemWrapper.tsx
@@ -4,6 +4,7 @@ import React, { PropsWithChildren, useState } from 'react';
 import { joinClassNames } from '../../utilities';
 import Toolbar from './Toolbar';
 import { BlockItemWrapperProps } from './types';
+import { useFocusWithin } from '@react-aria/interactions';
 
 export const BlockItemWrapper = ({
     children,
@@ -14,13 +15,20 @@ export const BlockItemWrapper = ({
     isDragging,
 }: PropsWithChildren<BlockItemWrapperProps>) => {
     const [isFlyoutOpen, setIsFlyoutOpen] = useState(false);
+    const [isFocused, setIsFocused] = useState(false);
+    const { focusWithinProps } = useFocusWithin({
+        onFocusWithin: () => setIsFocused(true),
+        onBlurWithin: () => setIsFocused(false),
+    });
+
     return (
         <div
+            {...focusWithinProps}
             data-test-id="block-item-wrapper"
             className={joinClassNames([
-                'tw-relative tw-group',
-                !shouldHideWrapper &&
-                    'hover:tw-outline-offset-[2px] hover:tw-outline hover:tw-outline-[1px] hover:tw-outline-box-selected-inverse',
+                'tw-relative tw-group tw-outline-offset-2 tw-outline-1 tw-outline-box-selected-inverse',
+                !shouldHideWrapper && ' hover:tw-outline',
+                isFocused && 'tw-outline',
             ])}
         >
             <div className={joinClassNames([shouldHideComponent && 'tw-opacity-0'])}>
@@ -28,7 +36,7 @@ export const BlockItemWrapper = ({
                     className={joinClassNames([
                         'tw-absolute tw-bottom-[calc(100%-4px)] tw-right-[-3px] tw-w-full',
                         !shouldHideWrapper && 'group-hover:tw-opacity-100',
-                        isFlyoutOpen && !shouldHideWrapper ? 'tw-opacity-100' : 'tw-opacity-0',
+                        (isFocused || isFlyoutOpen) && !shouldHideWrapper ? 'tw-opacity-100' : 'tw-opacity-0',
                     ])}
                 >
                     <Toolbar
