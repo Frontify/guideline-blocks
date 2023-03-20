511cf160201eeb9e3278eb606a5c3616154824f8
ragi96 <rafael.giezendanner@frontify.com>
2023-03-20 13:08:16 +0100

    get rid of react-aria/interactions, plain tw

--- packages/shared/src/components/BlockItemWrapper/BlockItemWrapper.tsx
+++ packages/shared/src/components/BlockItemWrapper/BlockItemWrapper.tsx
@@ -4,7 +4,6 @@ import React, { PropsWithChildren, useState } from 'react';
 import { joinClassNames } from '../../utilities';
 import Toolbar from './Toolbar';
 import { BlockItemWrapperProps } from './types';
-import { useFocusWithin } from '@react-aria/interactions';
 
 export const BlockItemWrapper = ({
     children,
@@ -15,29 +14,24 @@ export const BlockItemWrapper = ({
     isDragging,
 }: PropsWithChildren<BlockItemWrapperProps>) => {
     const [isFlyoutOpen, setIsFlyoutOpen] = useState(false);
-    const [isFocused, setIsFocused] = useState(false);
-    const { focusWithinProps } = useFocusWithin({
-        onFocusWithin: () => setIsFocused(true),
-        onBlurWithin: () => setIsFocused(false),
-    });
 
     return (
         <div
-            {...focusWithinProps}
             tabIndex={0}
             data-test-id="block-item-wrapper"
             className={joinClassNames([
                 'tw-relative tw-group tw-outline-offset-2 tw-outline-1 tw-outline-box-selected-inverse',
-                !shouldHideWrapper && ' hover:tw-outline',
-                isFocused && 'tw-outline',
+                !shouldHideWrapper && ' hover:tw-outline focus:tw-outline focus-within:tw-outline',
+                isFlyoutOpen && ' tw-outline',
             ])}
         >
             <div className={joinClassNames([shouldHideComponent && 'tw-opacity-0'])}>
                 <div
                     className={joinClassNames([
-                        'tw-absolute tw-bottom-[calc(100%-4px)] tw-right-[-3px] tw-w-full',
-                        !shouldHideWrapper && 'group-hover:tw-opacity-100',
-                        (isFocused || isFlyoutOpen) && !shouldHideWrapper ? 'tw-opacity-100' : 'tw-opacity-0',
+                        'tw-absolute tw-bottom-[calc(100%-4px)] tw-right-[-3px] tw-w-full tw-opacity-0',
+                        !shouldHideWrapper &&
+                            ' group-hover:tw-opacity-100 group-focus:tw-opacity-100 focus-within:tw-opacity-100',
+                        isFlyoutOpen && ' tw-opacity-100',
                     ])}
                 >
                     <Toolbar
