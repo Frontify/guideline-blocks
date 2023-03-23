3a7a733068fc615c9890a45155a45e89975b78de
triggertoo <triggertoo@gmail.com>
2023-03-23 13:44:55 +0100

    fix

--- packages/shared/src/helpers/convertToRTEValue.spec.ts
+++ packages/shared/src/helpers/convertToRTEValue.spec.ts
@@ -1,32 +1,32 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { TextStyles } from '@frontify/fondue';
+import type { TextStyles } from '@frontify/fondue';
 import { describe, expect, test } from 'vitest';
 import { convertToRTEValue } from './convertToRTEValue';
 
 describe('String converted to Richtext value', () => {
     test('It should return RTE value with corret textStyle', () => {
         const text = 'with text';
-        const result = convertToRTEValue(text, TextStyles.ELEMENT_PARAGRAPH);
+        const result = convertToRTEValue(text, 'p' as TextStyles);
         expect(result).toBe('[{"type":"p","children":[{"text":"with text"}]}]');
     });
 
     test('It should return RTE value with corret textStyle', () => {
         const text = 'Heading 1';
-        const result = convertToRTEValue(text, TextStyles.ELEMENT_HEADING1);
+        const result = convertToRTEValue(text, 'heading1' as TextStyles);
         expect(result).toBe('[{"type":"heading1","children":[{"text":"Heading 1"}]}]');
     });
 
     test('It should return RTE value with corret textStyle and alignment', () => {
         const text = 'text';
-        const result = convertToRTEValue(text, TextStyles.ELEMENT_PARAGRAPH, 'center');
+        const result = convertToRTEValue(text, 'p' as TextStyles, 'center');
         expect(result).toBe('[{"type":"p","children":[{"text":"text"}],"align":"center"}]');
     });
 
     test('It value is RTE value, it returns the RTE Value', () => {
         const text =
             '[{"type":"heading1","children":[{"text":"Hoi","bold":true,"underline":true}]},{"type":"heading1","children":[{"bold":true,"underline":true,"text":" "}]},{"type":"heading1","children":[{"text":"With Text"}]},{"type":"custom2","children":[{"text":""}]}]';
-        const result = convertToRTEValue(text, TextStyles.ELEMENT_PARAGRAPH);
+        const result = convertToRTEValue(text, 'p' as TextStyles);
         expect(result).toBe(text);
     });
 });
