921fb502c9033da351e8561569be90c6d0e007c5
Celine <celine@smartive.ch>
2023-03-27 09:48:42 +0200

    add type to hasRichTextValue

--- packages/shared/src/helpers/hasRichTextValue.ts
+++ packages/shared/src/helpers/hasRichTextValue.ts
@@ -1,6 +1,6 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-export const hasRichTextValue = (string?: string) => {
+export const hasRichTextValue = (string?: string): boolean => {
     if (!string) {
         return false;
     }
