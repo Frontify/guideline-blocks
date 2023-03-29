f84bc85b92da3babf9b9b9d06bc983277a184cb4
ragi96 <rafael.giezendanner@frontify.com>
2023-03-29 15:41:51 +0200

    added bulkDownloadHook

--- packages/asset-kit-block/src/AssetKitBlock.tsx
+++ packages/asset-kit-block/src/AssetKitBlock.tsx
@@ -3,13 +3,19 @@
 import { joinClassNames, useGuidelineDesignTokens } from '@frontify/guideline-blocks-shared';
 import '@frontify/fondue-tokens/styles';
 import { BlockProps } from '@frontify/guideline-blocks-settings';
-import { ReactElement, useState } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
 import 'tailwindcss/tailwind.css';
-import { useBlockAssets, useBlockSettings, useEditorState } from '@frontify/app-bridge';
+import {
+    useBlockAssets,
+    useBlockSettings,
+    useEditorState,
+    useBulkDownload,
+    BulkDownloadState,
+} from '@frontify/app-bridge';
 import { ASSET_SETTINGS_ID } from './settings';
 import { Settings } from './types';
 import { AssetGrid, AssetSelection, DownloadMessage, InformationSection } from './components';
-import { blockStyle, generateBulkDownload } from './helpers';
+import { blockStyle } from './helpers';
 
 export const AssetKitBlock = ({ appBridge }: BlockProps): ReactElement => {
     const { designTokens } = useGuidelineDesignTokens();
@@ -17,9 +23,30 @@ export const AssetKitBlock = ({ appBridge }: BlockProps): ReactElement => {
     const isEditing = useEditorState(appBridge);
     const { blockAssets, addAssetIdsToKey, deleteAssetIdsFromKey, updateAssetIdsFromKey } = useBlockAssets(appBridge);
     const [isUploadingAssets, setIsUploadingAssets] = useState<boolean>(false);
-    const [isDownloadingAssets, setIsDownloadingAssets] = useState<boolean>(false);
-    const { title, description, hasBorder_blocks, hasBackgroundBlocks } = blockSettings;
+    const { title, description, hasBorder_blocks, hasBackgroundBlocks, downloadUrlBlock } = blockSettings;
     const currentAssets = blockAssets[ASSET_SETTINGS_ID] ?? [];
+    const { generateBulkDownload, status, downloadUrl } = useBulkDownload(appBridge);
+
+    const downloadAssets = async () => {
+        if (downloadUrlBlock) {
+            window.open(downloadUrlBlock, '_blank');
+        } else {
+            generateBulkDownload(currentAssets.map((asset) => asset.id));
+        }
+    };
+
+    const saveDownloadUrl = (newDownloadUrlBlock: string) => {
+        if (downloadUrlBlock !== newDownloadUrlBlock) {
+            setBlockSettings({ downloadUrlBlock: newDownloadUrlBlock });
+        }
+    };
+
+    useEffect(() => {
+        if (status === BulkDownloadState.Ready && downloadUrl) {
+            window.open(downloadUrl, '_blank');
+            saveDownloadUrl(downloadUrl);
+        }
+    }, [downloadUrl]);
 
     return (
         <div
@@ -39,10 +66,12 @@ export const AssetKitBlock = ({ appBridge }: BlockProps): ReactElement => {
                 <div className="tw-flex-none">
                     <button
                         data-test-id="asset-kit-block-download-button"
-                        disabled={isDownloadingAssets || isUploadingAssets || currentAssets.length <= 0}
-                        onClick={() =>
-                            generateBulkDownload(appBridge.getProjectId(), currentAssets, setIsDownloadingAssets)
+                        disabled={
+                            [BulkDownloadState.Error, BulkDownloadState.Pending, BulkDownloadState.Started].includes(
+                                status
+                            ) || currentAssets.length <= 0
                         }
+                        onClick={() => downloadAssets()}
                         style={designTokens.buttonPrimary}
                     >
                         Download package
@@ -50,13 +79,16 @@ export const AssetKitBlock = ({ appBridge }: BlockProps): ReactElement => {
                 </div>
             </div>
 
-            {isDownloadingAssets && <DownloadMessage blockStyle={blockStyle(blockSettings)} />}
+            {![BulkDownloadState.Init, BulkDownloadState.Ready].includes(status) && (
+                <DownloadMessage blockStyle={blockStyle(blockSettings)} status={BulkDownloadState.Error} />
+            )}
 
             <AssetGrid
                 appBridge={appBridge}
                 currentAssets={currentAssets}
                 deleteAssetIdsFromKey={deleteAssetIdsFromKey}
                 updateAssetIdsFromKey={updateAssetIdsFromKey}
+                saveDownloadUrl={saveDownloadUrl}
                 isEditing={isEditing}
             />
 
@@ -66,6 +98,7 @@ export const AssetKitBlock = ({ appBridge }: BlockProps): ReactElement => {
                     isUploadingAssets={isUploadingAssets}
                     setIsUploadingAssets={setIsUploadingAssets}
                     addAssetIdsToKey={addAssetIdsToKey}
+                    saveDownloadUrl={saveDownloadUrl}
                     currentAssets={currentAssets}
                 />
             )}
--- packages/asset-kit-block/src/components/DownloadMessage.tsx
+++ packages/asset-kit-block/src/components/DownloadMessage.tsx
@@ -3,8 +3,9 @@
 import { Padding, paddingStyleMap } from '@frontify/guideline-blocks-shared';
 import { LoadingCircle } from '@frontify/fondue';
 import { DownloadMessageProps } from '../types';
+import { BulkDownloadState } from '@frontify/app-bridge';
 
-export const DownloadMessage = ({ blockStyle }: DownloadMessageProps) => {
+export const DownloadMessage = ({ blockStyle, status }: DownloadMessageProps) => {
     return (
         <div
             data-test-id="asset-kit-download-message"
@@ -14,13 +15,23 @@ export const DownloadMessage = ({ blockStyle }: DownloadMessageProps) => {
             }}
             className="tw-flex tw-gap-8 tw-items-center tw-mb-8"
         >
-            <div className="tw-flew-0">
-                <LoadingCircle />
-            </div>
+            {status === BulkDownloadState.Pending && (
+                <div className="tw-flew-0">
+                    <LoadingCircle />
+                </div>
+            )}
             <div className="tw-flex-1">
-                <span>
-                    This may take a little while. Your package will download automatically when it&apos;s ready.
-                </span>
+                {status === BulkDownloadState.Pending && (
+                    <span>
+                        This may take a little while. Your package will download automatically when it&apos;s ready.
+                    </span>
+                )}
+
+                {status === BulkDownloadState.Error && (
+                    <span className="tw-text-red-60">
+                        Sorry, we&apos;re experiencing some technical difficulties. Please try again in a few minutes.
+                    </span>
+                )}
             </div>
         </div>
     );
