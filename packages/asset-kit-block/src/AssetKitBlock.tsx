930d8423aff88c5985b786900eec03274d5f94b7
ragi96 <rafael.giezendanner@frontify.com>
2023-04-03 16:50:10 +0200

    added tw prefix for overflow-hidden

--- packages/asset-kit-block/src/AssetKitBlock.tsx
+++ packages/asset-kit-block/src/AssetKitBlock.tsx
@@ -92,7 +92,7 @@ export const AssetKitBlock = ({ appBridge }: BlockProps): ReactElement => {
                             data-test-id="asset-kit-block-screen-reader"
                             ref={screenReaderRef}
                             role="status"
-                            className="tw-absolute -tw-left-[10000px] tw-top-auto tw-w-1 tw-h-1 overflow-hidden"
+                            className="tw-absolute -tw-left-[10000px] tw-top-auto tw-w-1 tw-h-1 tw-overflow-hidden"
                         />
                     </button>
                 </div>
