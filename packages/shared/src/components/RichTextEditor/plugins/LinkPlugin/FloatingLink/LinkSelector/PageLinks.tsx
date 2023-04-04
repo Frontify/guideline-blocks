ffe60ffc2483498e0ca23d1649e840b5d9cc7918
Kevin Walker <kevin@smartive.ch>
2023-04-04 15:11:41 +0200

    improve loading & empty pages

--- packages/shared/src/components/RichTextEditor/plugins/LinkPlugin/FloatingLink/LinkSelector/PageLinks.tsx
+++ packages/shared/src/components/RichTextEditor/plugins/LinkPlugin/FloatingLink/LinkSelector/PageLinks.tsx
@@ -12,22 +12,33 @@ type PageLinksProps = {
 };
 
 export const PageLinks = ({ appBridge, documentId, selectedUrl, onSelectUrl }: PageLinksProps): ReactElement => {
-    const { pages } = useDocumentPages(appBridge, documentId);
+    const { pages, isLoading } = useDocumentPages(appBridge, documentId);
     const pagesArray = [...pages.values()];
+    const hasPages = !isLoading && pagesArray.length > 0;
+
+    if (isLoading) {
+        return <></>;
+    }
 
     return (
         <>
-            {pagesArray.map((page) => {
-                return (
-                    <PageLink
-                        key={page.id}
-                        page={page}
-                        appBridge={appBridge}
-                        selectedUrl={selectedUrl}
-                        onSelectUrl={onSelectUrl}
-                    />
-                );
-            })}
+            {hasPages ? (
+                pagesArray.map((page) => {
+                    return (
+                        <PageLink
+                            key={page.id}
+                            page={page}
+                            appBridge={appBridge}
+                            selectedUrl={selectedUrl}
+                            onSelectUrl={onSelectUrl}
+                        />
+                    );
+                })
+            ) : (
+                <div className="tw-py-2 tw-px-2.5 tw-pl-7 tw-leading-5 tw-text-s tw-text-text-weak">
+                    This document does not contain any pages.
+                </div>
+            )}
         </>
     );
 };
