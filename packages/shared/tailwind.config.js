d8f4111f01760958c4b920450fedf91f691a98b1
Samuel Alev <samuel.alev@frontify.com>
2023-04-04 11:21:17 +0200

    refactor: remove build step for shared (#529)
    Co-authored-by: Rafael Giezendanner <rafael.giezendanner@frontify.com>

---
+++ packages/shared/tailwind.config.js
@@ -0,0 +1,22 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                'blank-state-weak': '#ABADAD',
+                'blank-state-weak-inverse': '#FFFFFF',
+                'blank-state-shaded': '#727474',
+                'blank-state-shaded-inverse': '#FAFAFA',
+                'blank-state-weighted': '#696B6B',
+                'blank-state-weighted-inverse': '#F1F1F1',
+                'blank-state-hover': '#1A1C1C',
+                'blank-state-hover-inverse': '#FFFFFF',
+                'blank-state-pressed': '#080808',
+                'blank-state-pressed-inverse': '#F1F1F1',
+                'blank-state-line': '#A3A5A5',
+                'blank-state-line-hover': '#1A1C1C',
+            },
+        },
+    },
+};
