fac40c1537e6a8bd8c2626672f308f08500290f0
Celine <celine@smartive.ch>
2023-04-11 12:43:57 +0200

    loading serializer asynchronously

--- packages/shared/src/components/RichTextEditor/SerializedText.tsx
+++ packages/shared/src/components/RichTextEditor/SerializedText.tsx
@@ -1,17 +1,27 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 
 import { serializeRawToHtml } from '@frontify/fondue';
 import { SerializedTextProps } from './types';
 
 export const SerializedText = ({ value = '', designTokens, gap, columns }: SerializedTextProps) => {
-    const html = useMemo(
-        () => serializeRawToHtml(value, designTokens, columns, gap),
-        [value, designTokens, columns, gap]
-    );
+    const [html, setHtml] = useState<string | null>(null);
 
-    return html !== '<br />' ? (
-        <div data-test-id="rte-content-html" dangerouslySetInnerHTML={{ __html: html }} />
-    ) : null;
+    const serializedText = useMemo(async () => {
+        return await serializeRawToHtml(value, designTokens, columns, gap);
+    }, [value, designTokens, columns, gap]);
+
+    useEffect(() => {
+        (async () => {
+            setHtml(await serializedText);
+        })();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [serializedText]);
+
+    return html ? (
+        html !== '<br />' && <div data-test-id="rte-content-html" dangerouslySetInnerHTML={{ __html: html }} />
+    ) : (
+        <div className="tw-rounded-sm tw-bg-base-alt tw-animate-pulse tw-h-full tw-min-h-[10px] tw-w-full" />
+    );
 };
