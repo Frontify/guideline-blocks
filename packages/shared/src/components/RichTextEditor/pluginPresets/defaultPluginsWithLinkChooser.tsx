3eafe361391b2160aed11e10fd56a76706ae7e32
Daniel Fulop <daniel.fulop@frontify.com>
2023-04-24 08:10:48 +0200

    feat(DoDontBlock): add internal link chooser to RTE (#545)

---
+++ packages/shared/src/components/RichTextEditor/pluginPresets/defaultPluginsWithLinkChooser.tsx
@@ -0,0 +1,52 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import { AppBridgeBlock } from '@frontify/app-bridge';
+import {
+    AlignCenterPlugin,
+    AlignJustifyPlugin,
+    AlignLeftPlugin,
+    AlignRightPlugin,
+    BoldPlugin,
+    CheckboxListPlugin,
+    CodePlugin,
+    EmojiPlugin,
+    ItalicPlugin,
+    OrderedListPlugin,
+    ParagraphPlugin,
+    PluginComposer,
+    ResetFormattingPlugin,
+    SoftBreakPlugin,
+    StrikethroughPlugin,
+    TextStylePlugin,
+    UnderlinePlugin,
+    UnorderedListPlugin,
+} from '@frontify/fondue';
+import { ButtonPlugin, LinkPlugin } from '../plugins';
+
+export const getDefaultPluginsWithLinkChooser = (appBridge: AppBridgeBlock) => {
+    return new PluginComposer()
+        .setPlugin(new SoftBreakPlugin(), new ParagraphPlugin())
+        .setPlugin(new TextStylePlugin())
+        .setPlugin(
+            [
+                new BoldPlugin(),
+                new ItalicPlugin(),
+                new UnderlinePlugin(),
+                new StrikethroughPlugin(),
+                new LinkPlugin({ appBridge }),
+                new ButtonPlugin({ appBridge }),
+                new CodePlugin(),
+            ],
+            [
+                new AlignLeftPlugin(),
+                new AlignCenterPlugin(),
+                new AlignRightPlugin(),
+                new AlignJustifyPlugin(),
+                new UnorderedListPlugin(),
+                new CheckboxListPlugin(),
+                new OrderedListPlugin(),
+                new ResetFormattingPlugin(),
+                new EmojiPlugin(),
+            ]
+        );
+};
