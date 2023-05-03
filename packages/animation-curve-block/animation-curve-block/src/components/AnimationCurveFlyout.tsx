ff54df7ab87ac1c69bdb44ce21dc3f8191a8d310
ragi96 <rafael.giezendanner@frontify.com>
2023-05-03 13:54:48 +0200

    added animation curve block

---
+++ packages/animation-curve-block/animation-curve-block/src/components/AnimationCurveFlyout.tsx
@@ -0,0 +1,224 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import { useEffect, useState } from 'react';
+
+import {
+    ButtonEmphasis,
+    ButtonStyle,
+    Dropdown,
+    Flyout,
+    FlyoutFooter,
+    FlyoutPlacement,
+    IconCaretRight16,
+    IconCheckMark16,
+    TextInput,
+    TextInputType,
+    Validation,
+} from '@frontify/fondue';
+
+import { DROPDOWN_MENU_ITEMS } from '../constants';
+import { roundAnimationCurveParameters } from '../helpers';
+import {
+    AnimationCurve,
+    AnimationCurveFlyoutProps,
+    AnimationCurveParametersPatch,
+    AnimationCurveType,
+    AnimationFunctionPatch,
+    defaultAnimationCurveTypeValues,
+} from '../types';
+
+import { AnimationCanvas } from './';
+
+export const validateXValue = (value: number): Validation => {
+    return value < 0 || value > 1 ? Validation.Error : Validation.Default;
+};
+
+export const AnimationCurveFlyout = ({
+    animationCurve,
+    isFlyoutOpen,
+    lineColor,
+    endpointColor,
+    gridColor,
+    onSave,
+    onCancel,
+    onOpenChange,
+    onAnimationCurveUpdate,
+    onAnimationCurveChange,
+}: AnimationCurveFlyoutProps) => {
+    const [x1Validation, setX1Validation] = useState<Validation>(Validation.Default);
+    const [x2Validation, setX2Validation] = useState<Validation>(Validation.Default);
+    const [localAnimationFunction, setLocalAnimationFunction] = useState(animationCurve.animationFunction);
+
+    useEffect(() => {
+        const { x1, x2 } = localAnimationFunction.parameters;
+        setX1Validation(validateXValue(x1));
+        setX2Validation(validateXValue(x2));
+    }, [localAnimationFunction]);
+
+    const handleSave = () => {
+        onSave(animationCurve.id, { ...animationCurve });
+    };
+
+    const updateLocalCurve = (animationCurvePatch: AnimationCurve): void => {
+        const roundedAnimationCurve = roundAnimationCurveParameters(animationCurvePatch);
+
+        setLocalAnimationFunction(animationCurvePatch.animationFunction);
+        if (![x1Validation, x2Validation].includes(Validation.Error)) {
+            onAnimationCurveUpdate &&
+                onAnimationCurveUpdate(animationCurve.id, {
+                    ...animationCurve,
+                    ...roundedAnimationCurve,
+                });
+
+            onAnimationCurveChange && onAnimationCurveChange({ ...animationCurve, ...animationCurvePatch });
+        }
+    };
+
+    const updateAnimationFunction = (animationFunctionPatch: AnimationFunctionPatch): void => {
+        updateLocalCurve({
+            ...animationCurve,
+            animationFunction: {
+                ...animationCurve.animationFunction,
+                ...animationFunctionPatch,
+            },
+        });
+    };
+
+    const updateAnimationCurveParameters = (parametersPatch: AnimationCurveParametersPatch): void => {
+        updateAnimationFunction({
+            parameters: {
+                ...animationCurve.animationFunction.parameters,
+                ...parametersPatch,
+            },
+        });
+    };
+
+    return (
+        <Flyout
+            trigger={<></>}
+            isOpen={isFlyoutOpen}
+            onOpenChange={onOpenChange}
+            onCancel={onCancel}
+            legacyFooter={false}
+            fixedFooter={
+                <FlyoutFooter
+                    buttons={[
+                        {
+                            style: ButtonStyle.Default,
+                            emphasis: ButtonEmphasis.Default,
+                            children: 'Cancel',
+                            onClick: onCancel,
+                        },
+                        {
+                            style: ButtonStyle.Default,
+                            emphasis: ButtonEmphasis.Strong,
+                            icon: <IconCheckMark16 />,
+                            children: 'Save',
+                            onClick: handleSave,
+                            disabled: [x1Validation, x2Validation].includes(Validation.Error),
+                        },
+                    ]}
+                />
+            }
+            offset={20}
+            placement={FlyoutPlacement.BottomLeft}
+        >
+            <div className="tw-p-6 tw-w-[498px]">
+                <Dropdown
+                    activeItemId={localAnimationFunction.type}
+                    onChange={(id) =>
+                        updateAnimationFunction({
+                            type: id as AnimationCurveType,
+                            parameters:
+                                id === AnimationCurveType.Custom
+                                    ? { ...animationCurve.animationFunction.parameters }
+                                    : defaultAnimationCurveTypeValues[id as AnimationCurveType],
+                        })
+                    }
+                    menuBlocks={DROPDOWN_MENU_ITEMS}
+                />
+                <div className="tw-my-3 tw-bg-base-alt tw-border tw-border-line">
+                    <AnimationCanvas
+                        animationFunction={localAnimationFunction}
+                        setAnimationFunction={updateAnimationFunction}
+                        showGrid={false}
+                        showEndPoints={false}
+                        showHandles
+                        lineColor={lineColor}
+                        endpointColor={endpointColor}
+                        gridColor={gridColor}
+                        viewBox={{
+                            width: 450,
+                            height: 200,
+                        }}
+                        shouldAnimate={false}
+                    />
+                </div>
+                <div className="tw-flex tw-items-center tw-justify-between">
+                    <div className="tw-flex tw-justify-start tw-items-center tw-gap-1">
+                        <div className="tw-relative tw-w-[74px]">
+                            <span className="tw-absolute tw-top-2.5 tw-left-[3px] tw-z-10 tw-text-s tw-text-text-weak">
+                                X
+                            </span>
+                            <TextInput
+                                onChange={(value) => updateAnimationCurveParameters({ x1: Number(value) || 0 })}
+                                type={TextInputType.Number}
+                                value={localAnimationFunction.parameters.x1 as unknown as string}
+                                validation={x1Validation}
+                                required
+                            />
+                        </div>
+                        <div className="tw-relative tw-w-[74px]">
+                            <span className="tw-absolute tw-top-2.5 tw-left-[3px] tw-z-10 tw-text-s tw-text-text-weak">
+                                Y
+                            </span>
+                            <TextInput
+                                onChange={(value) => updateAnimationCurveParameters({ y1: Number(value) || 0 })}
+                                type={TextInputType.Number}
+                                value={localAnimationFunction.parameters.y1 as unknown as string}
+                                required
+                            />
+                        </div>
+                        <IconCaretRight16 />
+                        <div className="tw-relative tw-w-[74px]">
+                            <span className="tw-absolute tw-top-2.5 tw-left-[3px] tw-z-10 tw-text-s tw-text-text-weak">
+                                X
+                            </span>
+                            <TextInput
+                                onChange={(value) => updateAnimationCurveParameters({ x2: Number(value) || 0 })}
+                                type={TextInputType.Number}
+                                value={localAnimationFunction.parameters.x2.toString()}
+                                validation={x2Validation}
+                                required
+                            />
+                        </div>
+                        <div className="tw-relative tw-w-[74px]">
+                            <span className="tw-absolute tw-top-2.5 tw-left-[3px] tw-z-10 tw-text-s tw-text-text-weak">
+                                Y
+                            </span>
+                            <TextInput
+                                onChange={(value) => updateAnimationCurveParameters({ y2: Number(value) || 0 })}
+                                type={TextInputType.Number}
+                                value={localAnimationFunction.parameters.y2.toString()}
+                                required
+                            />
+                        </div>
+                    </div>
+                    <div className="tw-w-20 tw-relative">
+                        <TextInput
+                            onChange={(value) =>
+                                updateAnimationFunction({
+                                    ...animationCurve.animationFunction,
+                                    duration: Number(value) || 0,
+                                })
+                            }
+                            type={TextInputType.Number}
+                            value={animationCurve.animationFunction.duration.toString()}
+                        />
+                        <span className="tw-absolute tw-right-3 tw-top-2.5 tw-text-s tw-text-text-weak">sec</span>
+                    </div>
+                </div>
+            </div>
+        </Flyout>
+    );
+};
---
+++ packages/animation-curve-block/animation-curve-block/src/settings.ts
@@ -0,0 +1,170 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import {
+    appendUnit,
+    defineSettings,
+    numericalOrPixelRule,
+    presetCustomValue,
+} from '@frontify/guideline-blocks-settings';
+import {
+    Radius,
+    getBackgroundSettings,
+    getBorderRadiusSettings,
+    getBorderSettings,
+} from '@frontify/guideline-blocks-shared';
+
+import { Spacing, spacingValues } from './types';
+
+const COLUMNS_ID = 'columns';
+const CUSTOM_SPACING_ID = 'hasCustomSpacing';
+const SPACING_CHOICE_ID = 'spacingChoice';
+const SPACING_CUSTOM_ID = 'spacingCustom';
+const LINE_COLOR_ID = 'lineColor';
+const END_POINTS_ID = 'hasEndpoints';
+const END_POINTS_COLOR_ID = 'endpointsColor';
+const GRID_ID = 'hasGrid';
+const GRID_COLOR_ID = 'gridColor';
+const MOTION_ID = 'hasMotion';
+
+export const settings = defineSettings({
+    layout: [
+        {
+            id: 'itemsLayout',
+            type: 'sectionHeading',
+            label: 'Items',
+            blocks: [
+                {
+                    id: COLUMNS_ID,
+                    label: 'Number',
+                    type: 'slider',
+                    defaultValue: 3,
+                    choices: [
+                        {
+                            value: 1,
+                            label: '1',
+                        },
+                        {
+                            value: 2,
+                            label: '2',
+                        },
+                        {
+                            value: 3,
+                            label: '3',
+                        },
+                        {
+                            value: 4,
+                            label: '4',
+                        },
+                    ],
+                },
+                {
+                    id: CUSTOM_SPACING_ID,
+                    type: 'switch',
+                    defaultValue: false,
+                    switchLabel: 'Custom',
+                    label: 'Spacing',
+                    info: 'Another word for ‘gap’. Refers to both column gutter and row gutter.',
+                    onChange: (bundle) =>
+                        presetCustomValue(bundle, SPACING_CHOICE_ID, SPACING_CUSTOM_ID, spacingValues),
+                    show: (bundle) => bundle.getBlock(COLUMNS_ID)?.value !== '1',
+                    on: [
+                        {
+                            id: SPACING_CUSTOM_ID,
+                            type: 'input',
+                            rules: [numericalOrPixelRule],
+                            onChange: (bundle) => appendUnit(bundle, SPACING_CUSTOM_ID),
+                        },
+                    ],
+                    off: [
+                        {
+                            id: SPACING_CHOICE_ID,
+                            type: 'slider',
+                            defaultValue: Spacing.Medium,
+                            choices: [
+                                {
+                                    value: Spacing.None,
+                                    label: 'None',
+                                },
+                                {
+                                    value: Spacing.Small,
+                                    label: 'S',
+                                },
+                                {
+                                    value: Spacing.Medium,
+                                    label: 'M',
+                                },
+                                {
+                                    value: Spacing.Large,
+                                    label: 'L',
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            id: 'dataLayout',
+            type: 'sectionHeading',
+            label: 'Data',
+            blocks: [
+                {
+                    id: 'hasDuration',
+                    label: 'Duration',
+                    type: 'switch',
+                    defaultValue: true,
+                },
+                {
+                    id: 'hasParameter',
+                    label: 'Parameter',
+                    type: 'switch',
+                    defaultValue: true,
+                },
+            ],
+        },
+    ],
+    style: [
+        getBackgroundSettings({ defaultValue: true }),
+        getBorderSettings(),
+        getBorderRadiusSettings({ defaultRadius: Radius.Medium }),
+        {
+            id: LINE_COLOR_ID,
+            label: 'Line',
+            type: 'colorInput',
+            defaultValue: { red: 45, green: 50, blue: 50, alpha: 1 },
+        },
+        {
+            id: END_POINTS_ID,
+            type: 'switch',
+            defaultValue: true,
+            label: 'End Points',
+            on: [
+                {
+                    id: END_POINTS_COLOR_ID,
+                    type: 'colorInput',
+                    defaultValue: { red: 45, green: 50, blue: 50, alpha: 1 },
+                },
+            ],
+        },
+        {
+            id: GRID_ID,
+            type: 'switch',
+            defaultValue: false,
+            label: 'Grid',
+            on: [
+                {
+                    id: GRID_COLOR_ID,
+                    type: 'colorInput',
+                    defaultValue: { red: 153, green: 153, blue: 153, alpha: 1 },
+                },
+            ],
+        },
+        {
+            id: MOTION_ID,
+            type: 'switch',
+            defaultValue: false,
+            label: 'Motion',
+            info: 'Hover over the card to see an animated version of the animation curve in action.',
+        },
+    ],
+});
