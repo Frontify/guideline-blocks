aca7cf3588cf0a2c04ae709efc6da85c578c57fb
Kevin Walker <kevin@smartive.ch>
2023-05-11 07:41:30 +0200

    fix background & border

--- packages/glyphs-block/src/GlyphsBlock.tsx
+++ packages/glyphs-block/src/GlyphsBlock.tsx
@@ -1,23 +1,21 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { AppBridgeBlock, useBlockSettings } from '@frontify/app-bridge';
+import { useBlockSettings } from '@frontify/app-bridge';
 import { getRadiusStyles, toRgbaString } from '@frontify/guideline-blocks-shared';
 import { ReactElement } from 'react';
 import { DEFAULT_CHARS } from './settings';
-import { Settings } from './types';
-
-type BlockProps = {
-    appBridge: AppBridgeBlock;
-};
+import { BlockProps, Settings } from './types';
 
 export const GlyphsBlock = ({ appBridge }: BlockProps): ReactElement => {
     const [blockSettings] = useBlockSettings<Settings>(appBridge);
 
     const {
-        backgroundColor,
         chars = DEFAULT_CHARS,
+        hasBackground,
+        backgroundColor,
         fontColor,
         fontFamily,
+        hasBorder,
         borderStyle,
         borderWidth,
         borderColor,
@@ -26,21 +24,23 @@ export const GlyphsBlock = ({ appBridge }: BlockProps): ReactElement => {
         radiusValue,
     } = blockSettings;
 
-    const itemCustomStyles = {
-        backgroundColor: toRgbaString(backgroundColor),
-        outlineStyle: borderStyle,
-        outlineWidth: borderWidth,
-        outlineColor: toRgbaString(borderColor),
-        ...getRadiusStyles(radiusChoice, hasRadius, radiusValue),
-    };
-
     const items = chars.split(',').map((char, index) => {
         const isLetter = char.length === 1 && char.match(/[a-z]/i);
         return (
             <li
                 key={index}
                 className="tw-aspect-square tw-flex tw-items-center tw-justify-center tw-text-center"
-                style={itemCustomStyles}
+                style={{
+                    ...(hasBackground && {
+                        backgroundColor: toRgbaString(backgroundColor),
+                    }),
+                    ...(hasBorder && {
+                        outlineStyle: borderStyle,
+                        outlineWidth: borderWidth,
+                        outlineColor: toRgbaString(borderColor),
+                    }),
+                    ...getRadiusStyles(radiusChoice, hasRadius, radiusValue),
+                }}
             >
                 {char}
                 {isLetter && char.toLowerCase()}
--- packages/glyphs-block/src/types.ts
+++ packages/glyphs-block/src/types.ts
@@ -1,15 +1,22 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
+import { AppBridgeBlock } from '@frontify/app-bridge';
 import { Color } from '@frontify/fondue';
 import { Radius } from '@frontify/guideline-blocks-shared';
 
+export type BlockProps = {
+    appBridge: AppBridgeBlock;
+};
+
 export type Settings = {
+    hasBackground: boolean;
     backgroundColor: Color;
     chars: string;
     fontColor: Color;
     fontWeight: string;
     fontSize: string;
     fontFamily: string;
+    hasBorder: boolean;
     borderStyle: string;
     borderWidth: string;
     borderColor: Color;
