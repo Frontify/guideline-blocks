58b71482ee7298590ba4e5c511b0ee39bc3eb45d
ragi96 <rafael.giezendanner@frontify.com>
2023-05-23 16:53:10 +0200

    removed more unused stuff, that i missed

--- packages/shared/src/helpers/font.ts
+++
@@ -1,8 +0,0 @@
-/* (c) Copyright Frontify Ltd., all rights reserved. */
-
-export const getSystemFont = (elementFont: string) => (elementFont === 'default' ? 'system-ui' : elementFont);
-
-export const getFont = (elementFont: string, mainFontFamily?: string) => {
-    const mainFont = elementFont === 'inherit' ? mainFontFamily : undefined;
-    return mainFont ?? getSystemFont(elementFont);
-};
