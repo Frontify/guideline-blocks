71f50694a3e7b4e571e78b0a0a919eae42126e51
Kevin Walker <kevin@smartive.ch>
2023-06-06 09:07:32 +0200

    fix(AnimationCurve & ThumbnailGrid): use cypress/react18 (#582)

--- packages/thumbnail-grid-block/src/ThumbnailGridBlock.spec.ct.tsx
+++ packages/thumbnail-grid-block/src/ThumbnailGridBlock.spec.ct.tsx
@@ -1,6 +1,6 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { mount } from 'cypress/react';
+import { mount } from 'cypress/react18';
 import { AssetDummy, withAppBridgeBlockStubs } from '@frontify/app-bridge';
 import { ThumbnailGridBlock } from './ThumbnailGridBlock';
 import { CaptionPosition, HorizontalAlignment, VerticalAlignment } from './types';
