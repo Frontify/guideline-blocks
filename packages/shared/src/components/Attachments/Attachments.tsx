85f572225445d3d3c78de428f2d62c96528fd724
Celine <celine@smartive.ch>
2023-06-29 16:43:09 +0200

    add delay to download button

--- packages/shared/src/components/Attachments/Attachments.tsx
+++ packages/shared/src/components/Attachments/Attachments.tsx
@@ -149,7 +149,7 @@ export const Attachments = ({
                                 fitContent
                                 legacyFooter={false}
                                 trigger={
-                                    <div className="tw-flex -tw-mx-3 tw-text-[13px] tw-font-body tw-items-center tw-gap-1 tw-rounded-full tw-bg-box-neutral-strong-inverse hover:tw-bg-box-neutral-strong-inverse-hover active:tw-bg-box-neutral-strong-inverse-pressed tw-text-box-neutral-strong tw-p-[6px] tw-outline-line">
+                                    <div className="tw-flex -tw-mx-3 tw-text-[13px] tw-font-body tw-items-center tw-gap-1 tw-rounded-full tw-bg-box-neutral-strong-inverse hover:tw-bg-box-neutral-strong-inverse-hover active:tw-bg-box-neutral-strong-inverse-pressed tw-text-box-neutral-strong tw-outline tw-outline-1 tw-outline-offset-[1px] tw-p-[6px] tw-outline-line">
                                         <IconPaperclip16 />
                                         <div>{items.length > 0 ? items.length : 'Add'}</div>
                                         <IconCaretDown12 />
