910b3bf7834d1ccc73142602fab01da6472d81fa
Rafael Giezendanner <rafael.giezendanner@frontify.com>
2023-07-24 08:36:09 +0200

    added aria-label (#621)

--- packages/dos-donts-block/src/DoDontItem.tsx
+++ packages/dos-donts-block/src/DoDontItem.tsx
@@ -292,6 +292,7 @@ export const DoDontItem = React.forwardRef<HTMLDivElement, DoDontItemProps>(
                                     }
                                     value={internalTitle}
                                     disabled={!editing}
+                                    aria-label="Title"
                                     placeholder={editing ? 'Add a title' : ''}
                                     className="tw-text-s tw-pointer-ev tw-w-full tw-placeholder-[var(--placeholder-color)] placeholder:tw-opacity-30 tw-placeholder-opacity-30 tw-bg-transparent tw-resize-none tw-text-text-weak tw-break-words tw-outline-none tw-whitespace-pre-wrap"
                                 />
