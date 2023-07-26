8c0f0ef018eb74eb6c74b8dcaea3cc279a05c2af
fulopdaniel <daniel.fulop@frontify.com>
2023-07-26 09:30:23 +0200

    fix types

--- packages/dos-donts-block/src/types.ts
+++ packages/dos-donts-block/src/types.ts
@@ -76,12 +76,8 @@ export type SortableDoDontItemProps = {
     style: DoDontStyle;
     doColor: Color;
     dontColor: Color;
-    onChangeItem: (id: string, value: string | number | undefined, type: 'title' | 'body' | 'type' | 'imageId') => void;
-    onChangeLocalItem: (
-        id: string,
-        value: string | number | undefined,
-        type: 'title' | 'body' | 'type' | 'imageId'
-    ) => void;
+    onChangeItem: (id: string, value: string | number | undefined, type: ChangeType) => void;
+    onChangeLocalItem: (id: string, value: string | number | undefined, type: ChangeType) => void;
     title?: string;
     body?: string;
     editing: boolean;
