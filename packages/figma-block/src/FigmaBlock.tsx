4d02a4457d084aed916c61da38933a5850725a11
ragi96 <rafael.giezendanner@frontify.com>
2023-09-08 14:17:39 +0200

    fixed figma-block issue

--- packages/figma-block/src/FigmaBlock.tsx
+++ packages/figma-block/src/FigmaBlock.tsx
@@ -103,17 +103,16 @@ export const FigmaBlock = ({ appBridge }: BlockProps): ReactElement => {
     };
 
     const FigmaEmptyBlock = () => (
-        // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
-        <div
+        <button
             data-test-id="figma-empty-block"
-            className="tw-group tw-py-16 tw-px-4 tw-border-dashed tw-border tw-cursor-pointer tw-text-center tw-border-line-x-strong hover:tw-border-black"
+            className="tw-group tw-w-full tw-py-16 tw-px-4 tw-border-dashed tw-border tw-cursor-pointer tw-text-center tw-border-line-x-strong hover:tw-border-black"
             onClick={onOpenAssetChooser}
         >
             <div className="tw-text-xl tw-mb-4 tw-flex tw-justify-center tw-text-black-40 group-hover:tw-text-violet-60">
                 <IconSuitcase size={IconSize.Size32} />
             </div>
             <span className="tw-text-text-x-weak group-hover:tw-text-black">Choose Figma asset</span>
-        </div>
+        </button>
     );
 
     const ShowFigmaLink = useCallback(
