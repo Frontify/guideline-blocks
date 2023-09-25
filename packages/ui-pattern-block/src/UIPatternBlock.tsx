5b769c959b4723cb4f665c00220ab0621cbc9416
ragi96 <rafael.giezendanner@frontify.com>
2023-09-25 15:54:40 +0200

    finish settings

--- packages/ui-pattern-block/src/UIPatternBlock.tsx
+++ packages/ui-pattern-block/src/UIPatternBlock.tsx
@@ -1,11 +1,23 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 import { ReactElement } from 'react';
+import { useBlockSettings } from '@frontify/app-bridge';
 import { BlockProps } from '@frontify/guideline-blocks-settings';
+import { Sandpack, SandpackPredefinedTemplate } from '@codesandbox/sandpack-react';
 
 import 'tailwindcss/tailwind.css';
 import '@frontify/guideline-blocks-settings/styles';
 
+import { type Settings, sandpackThemeValues } from './types';
+
 export const UIPatternBlock = ({ appBridge }: BlockProps): ReactElement => {
-    return <div className="ui-pattern-block"></div>;
+    const [blockSettings] = useBlockSettings<Settings>(appBridge);
+    const { sandpackTemplate, sandpackTheme } = blockSettings;
+
+    return (
+        <Sandpack
+            template={sandpackTemplate as SandpackPredefinedTemplate}
+            theme={sandpackThemeValues[sandpackTheme]}
+        />
+    );
 };
