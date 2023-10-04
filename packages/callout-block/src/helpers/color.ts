28968535753c0d1c52bc0dac888427fa0e1f5fae
Rafael Giezendanner <rafael.giezendanner@frontify.com>
2023-10-04 13:58:18 +0200

    fix(CalloutBlock): change colors based on HubAppearance (#675)
    * add observer on accent color
    
    * fix: remove observer
    
    * fix: add destruction of listener

---
+++ packages/callout-block/src/helpers/color.ts
@@ -0,0 +1,36 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import { THEME_PREFIX, getReadableColor, isDark, setAlpha } from '@frontify/guideline-blocks-settings';
+import { Appearance, Type } from '../types';
+
+const getTextColor = (appearance: Appearance, color: string, backgroundColor: string): string => {
+    const isDarkColor = isDark(color, 185);
+    const defaultTextColor = isDarkColor ? 'white' : 'black';
+
+    if (appearance === Appearance.Light) {
+        return isDarkColor ? color : getReadableColor(color, backgroundColor);
+    }
+
+    return defaultTextColor;
+};
+
+const getAccentColor = (type: Type): string => {
+    const style = getComputedStyle(document.body);
+    switch (type) {
+        case Type.Info:
+            return style.getPropertyValue(`${THEME_PREFIX}accent-color-info-color`);
+        case Type.Note:
+            return style.getPropertyValue(`${THEME_PREFIX}accent-color-note-color`);
+        case Type.Tip:
+            return style.getPropertyValue(`${THEME_PREFIX}accent-color-tip-color`);
+        case Type.Warning:
+            return style.getPropertyValue(`${THEME_PREFIX}accent-color-warning-color`);
+    }
+};
+
+export const computeStyles = (type: Type, appearance: Appearance) => {
+    const accentColor = getAccentColor(type);
+    const backgroundColor = appearance === Appearance.Strong ? accentColor : setAlpha(0.1, accentColor);
+    const textColor = getTextColor(appearance, accentColor, backgroundColor);
+    return { backgroundColor, textColor };
+};
