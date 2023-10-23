42914701f51ddfd98e77faaf1c011fa7831fc54d
Ruslan Sazonov <ruslan.sazonov@frontify.com>
2023-10-23 15:15:34 +0200

    AlertError component aria-live is now less aggressive

--- packages/template-block/src/components/AlertError.tsx
+++ packages/template-block/src/components/AlertError.tsx
@@ -9,7 +9,7 @@ export type AlertErrorProps = {
 export const AlertError = ({ errorMessage }: AlertErrorProps) => {
     return (
         <span
-            aria-live="assertive"
+            aria-live="polite"
             className="tw-p-5 tw-flex tw-gap-4 tw-mb-4 tw-items-center tw-rounded-sm tw-transition-opacity tw-ease-in-out tw-bg-box-negative tw-text-box-negative-inverse"
         >
             <span>
