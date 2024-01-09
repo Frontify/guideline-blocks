75f3bc20c1f5fce9c11295787242922e7a8e1228
fulopdaniel <daniel.fulop@frontify.com>
2024-01-09 14:19:10 +0100

    revert root postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,8 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 module.exports = {
-    plugins: [
-        require("tailwindcss"),
-        require("autoprefixer"),
-        require("../../postcss/scope")({ scope: ".image-block" }),
-    ],
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
 };
