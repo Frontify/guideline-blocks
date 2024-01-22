295826feae539c69cba54a00879ba26c2a04f4d3
Fabian Keller <fabian@frontify.com>
2024-01-22 22:11:43 +0100

    Refactored logic into functions

--- packages/template-block/src/hooks/useTemplateBlockData.ts
+++ packages/template-block/src/hooks/useTemplateBlockData.ts
@@ -15,9 +15,11 @@ import { type BlockProps, convertToRteValue, radiusStyleMap, toRgbaString } from
 
 import { TEMPLATE_BLOCK_SETTING_ID } from '../constants';
 import {
+    AnchoringType,
     PreviewType,
     type Settings,
     TextPositioningType,
+    TextRatioType,
     horizontalAlignmentToCtaSelfAlign,
     horizontalAlignmentToTextAlign,
     textPositioningToContentFlexDirection,
@@ -59,37 +61,6 @@ export const useTemplateBlockData = (appBridge: BlockProps['appBridge']) => {
     const border = hasBorder_blockCard
         ? `${borderWidth_blockCard} ${borderStyle_blockCard} ${toRgbaString(borderColor_blockCard)}`
         : 'none';
-
-    // Content
-    const alignContentItems = [TextPositioningType.Right, TextPositioningType.Left].includes(textPositioning)
-        ? verticalAlignmentToItemAlign[textAnchoringVertical]
-        : '';
-    const contentClasses = `tw-flex tw-gap-x-8 tw-gap-y-4 ${textPositioningToContentFlexDirection[textPositioning]} ${alignContentItems}`;
-
-    // Preview
-    const previewFlexBasis =
-        hasPreview && [TextPositioningType.Right, TextPositioningType.Left].includes(textPositioning)
-            ? textRatioToPreviewFlexBasis[textRatio]
-            : '';
-    const previewClasses = `${previewFlexBasis}`;
-
-    // Text/CTA Wrapper
-    const textCtaWrapperFlexDirection = hasPreview ? 'tw-flex-col' : '';
-    const textCtaWrapperClasses = `tw-flex tw-grow ${textCtaWrapperFlexDirection} tw-gap-y-2 tw-gap-x-8`;
-
-    // Text
-    const textAlign =
-        hasPreview && [TextPositioningType.Top, TextPositioningType.Bottom].includes(textPositioning)
-            ? horizontalAlignmentToTextAlign[textAnchoringHorizontal]
-            : 'tw-text-left';
-    const textClasses = `tw-grow ${textAlign}`;
-
-    // CTA
-    const ctaAlignSelf =
-        hasPreview && [TextPositioningType.Top, TextPositioningType.Bottom].includes(textPositioning)
-            ? horizontalAlignmentToCtaSelfAlign[textAnchoringHorizontal]
-            : '';
-    const ctaClasses = `${ctaAlignSelf}`;
 
     const saveDescription = useCallback(
         async (newDescription: string) => {
@@ -162,23 +133,67 @@ export const useTemplateBlockData = (appBridge: BlockProps['appBridge']) => {
         blockSettings,
         border,
         borderRadius,
-        contentClasses,
-        ctaClasses,
+        contentClasses: getContentClasses(textPositioning, textAnchoringVertical),
+        ctaClasses: getCtaClasses(hasPreview, textPositioning, textAnchoringHorizontal),
         description,
         hasBackground,
         hasPreview,
         isEditing,
         lastErrorMessage,
         preview,
-        previewClasses,
+        previewClasses: getPreviewClasses(hasPreview, textPositioning, textRatio),
         previewCustom,
         saveDescription,
         saveTitle,
         selectedTemplate,
         templateTextKey,
-        textClasses,
-        textCtaWrapperClasses,
+        textClasses: getTextClasses(hasPreview, textPositioning, textAnchoringHorizontal),
+        textCtaWrapperClasses: getTextCtaWrapperClass(hasPreview),
         title,
         updateBlockSettings,
     };
+};
+
+const getCtaClasses = (
+    hasPreview: boolean,
+    textPositioning: TextPositioningType,
+    textAnchoringHorizontal: AnchoringType,
+) => {
+    return hasPreview && [TextPositioningType.Top, TextPositioningType.Bottom].includes(textPositioning)
+        ? horizontalAlignmentToCtaSelfAlign[textAnchoringHorizontal]
+        : '';
+};
+
+const getTextClasses = (
+    hasPreview: boolean,
+    textPositioning: TextPositioningType,
+    textAnchoringHorizontal: AnchoringType,
+): string => {
+    const textAlign =
+        hasPreview && [TextPositioningType.Top, TextPositioningType.Bottom].includes(textPositioning)
+            ? horizontalAlignmentToTextAlign[textAnchoringHorizontal]
+            : 'tw-text-left';
+    return `tw-grow ${textAlign}`;
+};
+
+const getTextCtaWrapperClass = (hasPreview: boolean): string => {
+    const textCtaWrapperFlexDirection = hasPreview ? 'tw-flex-col' : '';
+    return `tw-flex tw-grow ${textCtaWrapperFlexDirection} tw-gap-y-2 tw-gap-x-8`;
+};
+
+const getPreviewClasses = (
+    hasPreview: boolean,
+    textPositioning: TextPositioningType,
+    textRatio: TextRatioType,
+): string => {
+    return hasPreview && [TextPositioningType.Right, TextPositioningType.Left].includes(textPositioning)
+        ? textRatioToPreviewFlexBasis[textRatio]
+        : '';
+};
+
+const getContentClasses = (textPositioning: TextPositioningType, textAnchoringVertical: AnchoringType): string => {
+    const alignContentItems = [TextPositioningType.Right, TextPositioningType.Left].includes(textPositioning)
+        ? verticalAlignmentToItemAlign[textAnchoringVertical]
+        : '';
+    return `tw-flex tw-gap-x-8 tw-gap-y-4 ${textPositioningToContentFlexDirection[textPositioning]} ${alignContentItems}`;
 };
