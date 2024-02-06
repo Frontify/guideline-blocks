f0e953bc1835a4929726bb60be0afad4e74c2959
Sam Creasey <sam.creasey@frontify.com>
2024-02-06 12:28:35 +0100

    remove isdragging prop

--- packages/image-block/src/ImageBlock.tsx
+++ packages/image-block/src/ImageBlock.tsx
@@ -127,7 +127,6 @@ export const ImageBlock = withAttachmentsProvider(({ appBridge }: BlockProps) =>
                             shouldHideWrapper={!isEditing}
                             shouldBeShown={showAltTextMenu}
                             showAttachments
-                            isDragging={showAltTextMenu}
                             toolbarFlyoutItems={[
                                 [
                                     {
