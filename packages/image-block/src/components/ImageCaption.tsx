597b6b2b317743c646c81de591282f7f779f230b
Daniel Fulop <daniel.fulop@frontify.com>
2024-04-08 11:42:19 +0200

    feat(ImageBlock): allow attachments when block is empty (#804)
    * feat(ImageBlock): allow attachments when block is empty
    
    * add condition
    
    * add component
    
    * fix review comments
    
    ---------
    
    Co-authored-by: Rafael Giezendanner <rafael.giezendanner@frontify.com>

--- packages/image-block/src/components/ImageCaption.tsx
+++ packages/image-block/src/components/ImageCaption.tsx
@@ -9,7 +9,7 @@ import {
 } from '@frontify/guideline-blocks-settings';
 import { getCaptionPlugins, titlePlugins } from './helpers';
 import { AppBridgeBlock } from '@frontify/app-bridge';
-import { CaptionPosition, type Settings } from '../types';
+import { CaptionPosition, type Ratio, type Settings, textRatioValues } from '../types';
 
 type ImageCaptionProps = {
     blockId: string;
@@ -20,6 +20,7 @@ type ImageCaptionProps = {
     name?: string;
     description?: string;
     setBlockSettings: (newBlockSettings: Partial<Settings>) => void;
+    ratio: Ratio;
 };
 
 export const ImageCaption = ({
@@ -30,36 +31,52 @@ export const ImageCaption = ({
     description,
     name,
     positioning,
+    ratio,
     setBlockSettings,
 }: ImageCaptionProps) => {
+    const hasTitle = hasRichTextValue(name);
+    const hasDescription = hasRichTextValue(description);
+
+    if (!isEditing && !hasTitle && !hasDescription) {
+        return null;
+    }
+
     return (
         <div
-            className={joinClassNames([
-                positioning === CaptionPosition.Below && 'tw-mt-3',
-                'tw-gap-1 tw-flex-1 tw-w-full',
-            ])}
-            style={{ wordBreak: 'break-word' }}
-            data-test-id="image-caption"
+            className={
+                positioning === CaptionPosition.Above || positioning === CaptionPosition.Below
+                    ? 'tw-w-full'
+                    : textRatioValues[ratio]
+            }
         >
-            <RichTextEditor
-                key={titleKey}
-                id={`${blockId}_title`}
-                isEditing={isEditing}
-                plugins={titlePlugins}
-                placeholder="Asset name"
-                showSerializedText={hasRichTextValue(name)}
-                value={name ?? convertToRteValue(TextStyles.imageTitle, '', 'center')}
-                onTextChange={(name) => setBlockSettings({ name })}
-            />
-            <RichTextEditor
-                id={`${blockId}_description`}
-                isEditing={isEditing}
-                plugins={getCaptionPlugins(appBridge)}
-                onTextChange={(description) => setBlockSettings({ description })}
-                placeholder="Add a description here"
-                showSerializedText={hasRichTextValue(description)}
-                value={description ?? convertToRteValue(TextStyles.imageCaption, '', 'center')}
-            />
+            <div
+                className={joinClassNames([
+                    positioning === CaptionPosition.Below && 'tw-mt-3',
+                    'tw-gap-1 tw-flex-1 tw-w-full',
+                ])}
+                style={{ wordBreak: 'break-word' }}
+                data-test-id="image-caption"
+            >
+                <RichTextEditor
+                    key={titleKey}
+                    id={`${blockId}_title`}
+                    isEditing={isEditing}
+                    plugins={titlePlugins}
+                    placeholder="Asset name"
+                    showSerializedText={hasTitle}
+                    value={name ?? convertToRteValue(TextStyles.imageTitle, '', 'center')}
+                    onTextChange={(name) => setBlockSettings({ name })}
+                />
+                <RichTextEditor
+                    id={`${blockId}_description`}
+                    isEditing={isEditing}
+                    plugins={getCaptionPlugins(appBridge)}
+                    onTextChange={(description) => setBlockSettings({ description })}
+                    placeholder="Add a description here"
+                    showSerializedText={hasDescription}
+                    value={description ?? convertToRteValue(TextStyles.imageCaption, '', 'center')}
+                />
+            </div>
         </div>
     );
 };
