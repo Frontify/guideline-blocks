8f85edc1eb77e2fa9cc26e29b1bad5bfa1621fff
Eliza Sohn <eliza.sohn@gmail.com>
2024-10-17 01:53:00 -0700

    remove unused function

--- packages/image-block/src/components/helpers.ts
+++ packages/image-block/src/components/helpers.ts
@@ -122,12 +122,9 @@ export const getFrameInterpolationSetting = (blockSettings: Settings): boolean =
     return frameInterpolation;
 };
 
+// for use with checklist
 export const getPlaybackSettings = (blockSettings: Settings): { playOnHover: boolean; autoplay: boolean } => {
     const playOnHover = blockSettings.playback?.includes('hover') ? true : false;
     const autoplay = blockSettings.playback?.includes('autoplay') ? true : false;
     return { playOnHover, autoplay };
 };
-
-// const toCssRgbaString = (color: Color): string => {
-//     return `rgba(${color.red}, ${color.green}, ${color.blue}, ${color.alpha})`;
-// };
