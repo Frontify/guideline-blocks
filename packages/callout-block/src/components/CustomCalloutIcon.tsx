0a8b30b8b30a287728f5f0d5a868c24b44be7cec
fulopdaniel <daniel.fulop@frontify.com>
2024-12-05 13:14:37 +0100

    fix(CalloutBlock): unregister assets on switch off

--- packages/callout-block/src/components/CustomCalloutIcon.tsx
+++ packages/callout-block/src/components/CustomCalloutIcon.tsx
@@ -1,17 +1,11 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { AppBridgeBlock, useBlockAssets } from '@frontify/app-bridge';
-
-import { ICON_ASSET_ID } from '../settings';
+import { Asset } from '@frontify/app-bridge';
 import { Type } from '../types';
 
-export const CustomCalloutIcon = ({ appBridge, type }: { appBridge: AppBridgeBlock; type: Type }) => {
-    const { blockAssets } = useBlockAssets(appBridge);
-
-    const icon = blockAssets?.[ICON_ASSET_ID]?.[0];
-
+export const CustomCalloutIcon = ({ customIcon, type }: { customIcon: Asset | undefined; type: Type }) => {
     const devicePixelRatio = Math.max(window.devicePixelRatio, 1);
-    const iconUrl = icon?.genericUrl.replace('{width}', (20 * devicePixelRatio).toString());
+    const iconUrl = customIcon?.genericUrl.replace('{width}', (20 * devicePixelRatio).toString());
 
     return iconUrl ? (
         <img data-test-id="callout-icon-custom" src={iconUrl} alt={type} className="tw-w-5 tw-h-5 tw-object-contain" />
