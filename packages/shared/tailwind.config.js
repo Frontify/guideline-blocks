b260da13362fb7c2c7a696a7880d3892776a32f1
ragi96 <rafael.giezendanner@frontify.com>
2025-01-07 09:54:24 +0100

    fix tailwind config

--- packages/shared/tailwind.config.js
+++ packages/shared/tailwind.config.js
@@ -1,7 +1,9 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
+import guidelineBlockSettings from '@frontify/guideline-blocks-settings/tailwind';
+
 module.exports = {
-    presets: [require('@frontify/guideline-blocks-settings/tailwind')],
+    presets: [guidelineBlockSettings],
     content: ['src/**/*.{ts,tsx}'],
     corePlugins: {
         preflight: false,
