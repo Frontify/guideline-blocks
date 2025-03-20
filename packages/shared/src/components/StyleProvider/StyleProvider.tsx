6a5bc9ec78bbd2018633b9353bfa42b30140d19b
fulopdaniel <daniel.fulop@frontify.com>
2025-03-20 14:01:54 +0100

    fix(*): fondue styles missing for legacy components

--- packages/shared/src/components/StyleProvider/StyleProvider.tsx
+++ packages/shared/src/components/StyleProvider/StyleProvider.tsx
@@ -5,6 +5,7 @@ import React, { ReactNode } from 'react';
 import { ThemeProvider } from '@frontify/fondue/components';
 import '@frontify/guideline-blocks-settings/styles';
 import 'tailwindcss/tailwind.css';
+import '@frontify/fondue/style';
 
 export const StyleProvider = ({ children }: { children: ReactNode }) => {
     return <ThemeProvider theme="light">{children}</ThemeProvider>;
