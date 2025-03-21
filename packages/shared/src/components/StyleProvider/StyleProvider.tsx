77f1d15114d7134cca6c7a8b5cac3cb0916ba485
Peter Razvan Tudosa <4538108+peter-tudosa@users.noreply.github.com>
2025-03-21 14:11:36 +0100

    fix: style import order was causing some clashes (#1038)
    Signed-off-by: Peter Tudosa <razvan-petru.tudosa@frontify.com>

--- packages/shared/src/components/StyleProvider/StyleProvider.tsx
+++ packages/shared/src/components/StyleProvider/StyleProvider.tsx
@@ -4,8 +4,8 @@ import React, { ReactNode } from 'react';
 
 import { ThemeProvider } from '@frontify/fondue/components';
 import '@frontify/guideline-blocks-settings/styles';
-import 'tailwindcss/tailwind.css';
 import '@frontify/fondue/style';
+import 'tailwindcss/tailwind.css';
 
 export const StyleProvider = ({ children }: { children: ReactNode }) => {
     return <ThemeProvider theme="light">{children}</ThemeProvider>;
