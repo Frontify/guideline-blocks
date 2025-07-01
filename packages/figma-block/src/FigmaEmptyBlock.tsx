3744618344a76b32f9b56efb5711ac2d5d7a4dd0
Ben Laemmlin <ben.laemmlin+1@frontify.com>
2025-07-01 14:12:07 +0200

    fix: made alpha level edjustments

--- packages/figma-block/src/FigmaEmptyBlock.tsx
+++ packages/figma-block/src/FigmaEmptyBlock.tsx
@@ -12,10 +12,10 @@ export const FigmaEmptyBlock = ({ onOpenAssetChooser }: Props): ReactElement => 
     return (
         <button
             data-test-id="figma-empty-block"
-            className="tw-group tw-w-full tw-py-16 tw-px-4 tw-border-dashed tw-border tw-cursor-pointer tw-text-center tw-border-line-x-strong hover:tw-border-[var(--f-theme-settings-body-color)]"
+            className="tw-group tw-w-full tw-py-16 tw-px-4 tw-border-dashed tw-border tw-cursor-pointer tw-text-center tw-border-line-x-strong hover:tw-border-[color-mix(in_srgb,_var(--f-theme-settings-body-color)_70%,_transparent)]"
             onClick={onOpenAssetChooser}
         >
-            <div className="tw-text-xl tw-mb-4 tw-flex tw-justify-center tw-text-[color-mix(in_srgb,_var(--f-theme-settings-body-color)_70%,_transparent)] group-hover:tw-text-[var(--f-theme-settings-body-color)]">
+            <div className="tw-text-xl tw-mb-4 tw-flex tw-justify-center tw-text-[var(--f-theme-settings-body-color)] group-hover:tw-text-[color-mix(in_srgb,_var(--f-theme-settings-body-color)_70%,_transparent)]">
                 <IconSuitcase size={IconSize.Size32} />
             </div>
             <span className="tw-text-text-x-weak group-hover:tw-text-[var(--f-theme-settings-body-color)]">
