f11b6f65340b0dfb45dc52a015c28550c6cf4930
fulopdaniel <daniel.fulop@frontify.com>
2025-08-18 10:01:10 +0200

    fix(ImageBlock): remove unnecessary space

--- packages/image-block/src/settings.ts
+++ packages/image-block/src/settings.ts
@@ -52,32 +52,24 @@ export const DEFAULT_BORDER_COLOR = {
 export const settings = defineSettings({
     basics: [
         {
-            id: 'imageSection',
-            type: 'sectionHeading',
-            label: '',
-            blocks: [
+            id: IMAGE_ID,
+            type: 'assetInput',
+            label: 'Image',
+            size: 'small',
+            extensions: ALLOWED_EXTENSIONS,
+        },
+        {
+            id: 'hasLink',
+            type: 'switch',
+            label: 'Link',
+            defaultValue: false,
+            on: [
                 {
-                    id: IMAGE_ID,
-                    type: 'assetInput',
-                    label: 'Image',
-                    size: 'small',
-                    extensions: ALLOWED_EXTENSIONS,
-                },
-                {
-                    id: 'hasLink',
-                    type: 'switch',
-                    label: 'Link',
-                    defaultValue: false,
-                    on: [
-                        {
-                            id: 'linkObject',
-                            type: 'link',
-                            clearable: true,
-                        },
-                    ],
+                    id: 'linkObject',
+                    type: 'link',
+                    clearable: true,
                 },
             ],
-            showForTranslations: true,
         },
     ],
     layout: [
