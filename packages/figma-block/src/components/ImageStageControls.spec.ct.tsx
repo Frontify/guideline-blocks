9c29fa94722854a4246aebee84d4ff5c552b1c32
Peter Tudosa <razvan-petru.tudosa@frontify.com>
2025-11-03 15:37:10 +0100

    refactor(ImageStageControls): update test ids
    Signed-off-by: Peter Tudosa <razvan-petru.tudosa@frontify.com>

--- packages/figma-block/src/components/ImageStageControls.spec.ct.tsx
+++ packages/figma-block/src/components/ImageStageControls.spec.ct.tsx
@@ -3,10 +3,10 @@
 import { mount } from 'cypress/react';
 import { DrawFullScreenActionButton, DrawZoomInOutButtons } from './ImageStageControls';
 
-const ICON_ZOOM_IN_SELECTOR = '[name="IconPlus16"]';
-const ICON_ZOOM_OUT_SELECTOR = '[name="IconMinus16"]';
-const ICON_REJECT_SELECTOR = '[name="IconCross20"]';
-const ICON_EXPAND_SELECTOR = '[name="IconArrowExpand20"]';
+const ICON_ZOOM_IN_SELECTOR = '[data-test-id="fondue-icons-plus"]';
+const ICON_ZOOM_OUT_SELECTOR = '[data-test-id="fondue-icons-minus"]';
+const ICON_REJECT_SELECTOR = '[data-test-id="fondue-icons-cross"]';
+const ICON_EXPAND_SELECTOR = '[data-test-id="fondue-icons-arrow-expand"]';
 
 describe('Image Control Buttons', () => {
     describe('DrawFullScreenActionButton', () => {
