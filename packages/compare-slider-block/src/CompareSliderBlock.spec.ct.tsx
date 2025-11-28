4cb9cae8a03b10de450118ffb5085a06b4006ae6
fulopdaniel <daniel.fulop@frontify.com>
2025-11-28 14:14:31 +0100

    add test

--- packages/compare-slider-block/src/CompareSliderBlock.spec.ct.tsx
+++ packages/compare-slider-block/src/CompareSliderBlock.spec.ct.tsx
@@ -4,6 +4,7 @@ import { mount } from 'cypress/react';
 import { AssetDummy, withAppBridgeBlockStubs } from '@frontify/app-bridge';
 import { CompareSliderBlock } from './CompareSliderBlock';
 import { convertToRteValue } from '@frontify/guideline-blocks-settings';
+import { Height } from './types';
 
 const CompareSliderBlockSelector = '[data-test-id="compare-slider-block"]';
 const CompareSliderSelector = '[data-test-id="compare-slider-block-slider"]';
@@ -14,21 +15,21 @@ const FirstAssetSelector = '[data-test-id="slider-item-first"]';
 const SecondAssetSelector = '[data-test-id="slider-item-second"]';
 
 describe('Compare Slider Block', () => {
-    it('renders compare slider block', () => {
+    it('should render compare slider block', () => {
         const [CompareSliderBlockWithStubs] = withAppBridgeBlockStubs(CompareSliderBlock, {});
 
         mount(<CompareSliderBlockWithStubs />);
         cy.get(CompareSliderBlockSelector).should('exist');
     });
 
-    it('renders upload buttons if there are no assets uploaded yet', () => {
+    it('should render upload buttons if there are no assets uploaded yet', () => {
         const [CompareSliderBlockWithStubs] = withAppBridgeBlockStubs(CompareSliderBlock, { editorState: true });
 
         mount(<CompareSliderBlockWithStubs />);
         cy.get(BlockInjectButtonSelector).should('have.length', 2);
     });
 
-    it('renders upload buttons if there are is only one asset uploaded', () => {
+    it('should render upload buttons if there are is only one asset uploaded', () => {
         const [CompareSliderBlockWithStubs] = withAppBridgeBlockStubs(CompareSliderBlock, {
             blockAssets: { firstAsset: [{ ...AssetDummy.with(1), previewUrl: 'https://picsum.photos/200/200' }] },
             editorState: true,
@@ -38,7 +39,7 @@ describe('Compare Slider Block', () => {
         cy.get(BlockInjectButtonSelector).should('have.length', 1);
     });
 
-    it('renders the compare slider if there are two assets uploaded', () => {
+    it('should render the compare slider if there are two assets uploaded', () => {
         const [CompareSliderBlockWithStubs] = withAppBridgeBlockStubs(CompareSliderBlock, {
             blockAssets: {
                 firstAsset: [{ ...AssetDummy.with(1), previewUrl: 'https://picsum.photos/200/200' }],
@@ -50,7 +51,7 @@ describe('Compare Slider Block', () => {
         cy.get(CompareSliderSelector).should('exist');
     });
 
-    it('renders label in view mode if content exists', () => {
+    it('should render label in view mode if content exists', () => {
         const [CompareSliderBlockWithStubs] = withAppBridgeBlockStubs(CompareSliderBlock, {
             blockAssets: {
                 firstAsset: [{ ...AssetDummy.with(1), previewUrl: 'https://picsum.photos/200/200' }],
@@ -65,7 +66,7 @@ describe('Compare Slider Block', () => {
         cy.get('[data-test-id="rte-content-html"').should('have.text', 'first Asset Label');
     });
 
-    it('renders 2 labels in view mode if content exists', () => {
+    it('should render 2 labels in view mode if content exists', () => {
         const [CompareSliderBlockWithStubs] = withAppBridgeBlockStubs(CompareSliderBlock, {
             blockAssets: {
                 firstAsset: [{ ...AssetDummy.with(1), previewUrl: 'https://picsum.photos/200/200' }],
@@ -81,7 +82,7 @@ describe('Compare Slider Block', () => {
         cy.get('[data-test-id="rte-content-html"').should('contain', 'Test', 'Test2');
     });
 
-    it('renders 2 labels in edit mode', () => {
+    it('should render 2 labels in edit mode', () => {
         const [CompareSliderBlockWithStubs] = withAppBridgeBlockStubs(CompareSliderBlock, {
             blockAssets: {
                 firstAsset: [{ ...AssetDummy.with(1), previewUrl: 'https://picsum.photos/200/200' }],
@@ -93,7 +94,7 @@ describe('Compare Slider Block', () => {
         cy.get(LabelWrapperSelector).should('have.length', 2);
     });
 
-    it('renders a strikethrough line', () => {
+    it('should render a strikethrough line', () => {
         const [CompareSliderBlockWithStubs] = withAppBridgeBlockStubs(CompareSliderBlock, {
             blockAssets: {
                 firstAsset: [{ ...AssetDummy.with(1), previewUrl: 'https://picsum.photos/200/200' }],
@@ -107,7 +108,7 @@ describe('Compare Slider Block', () => {
         cy.get(StrikethroughWrapperSelector).should('exist');
     });
 
-    it('renders the alt texts', () => {
+    it('should render the alt texts', () => {
         const [CompareSliderBlockWithStubs] = withAppBridgeBlockStubs(CompareSliderBlock, {
             blockAssets: {
                 firstAsset: [{ ...AssetDummy.with(1), previewUrl: 'https://picsum.photos/200/200' }],
@@ -123,5 +124,33 @@ describe('Compare Slider Block', () => {
         cy.get(FirstAssetSelector).should('have.attr', 'alt', 'First alt text');
         cy.get(SecondAssetSelector).should('exist');
         cy.get(SecondAssetSelector).should('have.attr', 'alt', 'Second alt text');
+    });
+
+    it('should use the correct aspect ratio for the images if height is auto', () => {
+        const [CompareSliderBlockWithStubs] = withAppBridgeBlockStubs(CompareSliderBlock, {
+            blockAssets: {
+                firstAsset: [{ ...AssetDummy.with(1), height: 200, width: 100 }],
+                secondAsset: [{ ...AssetDummy.with(2), width: 100, height: 200 }],
+            },
+            blockSettings: {
+                height: Height.Auto,
+            },
+        });
+        mount(<CompareSliderBlockWithStubs />);
+        cy.get(CompareSliderBlockSelector).should('have.css', 'aspect-ratio', '0.5 / 1');
+    });
+
+    it('should not  aspect ratio for the images if height is set', () => {
+        const [CompareSliderBlockWithStubs] = withAppBridgeBlockStubs(CompareSliderBlock, {
+            blockAssets: {
+                firstAsset: [{ ...AssetDummy.with(1), height: 200, width: 100 }],
+                secondAsset: [{ ...AssetDummy.with(2), width: 100, height: 200 }],
+            },
+            blockSettings: {
+                height: '200px',
+            },
+        });
+        mount(<CompareSliderBlockWithStubs />);
+        cy.get(CompareSliderBlockSelector).should('have.css', 'aspect-ratio', 'auto');
     });
 });
