90056b1d0bd90dd98e904ced83035ca9c848b3d6
ragi96 <rafael.giezendanner@frontify.com>
2026-02-11 17:52:57 +0100

    change css

--- packages/shared/src/components/StyleProvider/StyleProvider.tsx
+++ packages/shared/src/components/StyleProvider/StyleProvider.tsx
@@ -4,10 +4,8 @@ import { ThemeProvider } from '@frontify/fondue/components';
 // eslint-disable-next-line no-restricted-syntax
 import * as React from 'react';
 import { type ReactNode } from 'react';
-import '@frontify/guideline-blocks-settings/styles';
-import '@frontify/fondue/style';
-import '@frontify/fondue/components/styles';
-import 'tailwindcss/tailwind.css';
+
+import './styles.css';
 
 export const StyleProvider = ({ children }: { children: ReactNode }) => {
     return <ThemeProvider theme="light">{children}</ThemeProvider>;
