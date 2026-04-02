2692279698f8cf65167a66d18fabc08a46d2136d
Luca Hochreutener <34413733+hochreutenerl@users.noreply.github.com>
2026-04-02 15:17:19 +0200

    refactor: make columns a string (#1284)

--- packages/dos-donts-block/src/settings.ts
+++ packages/dos-donts-block/src/settings.ts
@@ -140,23 +140,23 @@ export const settings = defineSettings({
                     id: 'columns',
                     label: 'Columns',
                     type: 'segmentedControls',
-                    defaultValue: 2,
+                    defaultValue: '2',
                     info: "The number of columns for Dos and Don'ts. Some settings won't apply if the container is too narrow.",
                     choices: [
                         {
-                            value: 1,
+                            value: '1',
                             label: '1',
                         },
                         {
-                            value: 2,
+                            value: '2',
                             label: '2',
                         },
                         {
-                            value: 3,
+                            value: '3',
                             label: '3',
                         },
                         {
-                            value: 4,
+                            value: '4',
                             label: '4',
                         },
                     ],
