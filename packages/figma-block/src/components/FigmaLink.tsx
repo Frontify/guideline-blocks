c795e07ef17ab1c43fdb465b985eb50ce939d10f
benfrontify <ben.laemmlin+1@frontify.com>
2026-05-19 08:26:07 +0200

    refactor: delete hardcoded color

--- packages/figma-block/src/components/FigmaLink.tsx
+++ packages/figma-block/src/components/FigmaLink.tsx
@@ -1,5 +1,7 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
+import { THEME_PREFIX } from '@frontify/guideline-blocks-settings';
+
 type FigmaLinkProps = {
     assetExternalUrl: string;
     title: string | undefined;
@@ -7,7 +9,15 @@ type FigmaLinkProps = {
 
 export const FigmaLink = ({ assetExternalUrl, title }: FigmaLinkProps) => (
     <div className="tw-p-2 tw-text-sm">
-        <a href={assetExternalUrl} target="_blank" rel="noreferrer" className="tw-text-[#4a90e2]">
+        <a
+            href={assetExternalUrl}
+            target="_blank"
+            rel="noreferrer"
+            style={{
+                color: `var(${THEME_PREFIX}link-color)`,
+                textDecoration: `var(${THEME_PREFIX}link-text-decoration)`,
+            }}
+        >
             {title}
         </a>
     </div>
