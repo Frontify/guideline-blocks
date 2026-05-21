14858449687ca8b129a27bd0a0df01e076157093
benfrontify <ben.laemmlin+1@frontify.com>
2026-05-21 11:00:26 +0200

    fix:migrate Cypress tests to Vitest

---
+++ packages/figma-block/src/components/ContainerOperator/VectorContainerOperator.spec.tsx
@@ -0,0 +1,31 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import '@testing-library/jest-dom/vitest';
+import { fireEvent, render } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
+import { BlockContainerStub } from '../../tests/BlockContainerStub';
+
+const IMAGE_CONTAINER_ID = 'image-container';
+
+describe('VectorContainerOperator', () => {
+    it('renders the image container', () => {
+        render(<BlockContainerStub height="400px" />);
+
+        const containerElement = document.getElementById(IMAGE_CONTAINER_ID) as HTMLDivElement;
+
+        expect(containerElement).toBeInTheDocument();
+    });
+
+    it('moves image on mouse move', () => {
+        render(<BlockContainerStub height="400px" />);
+
+        const containerElement = document.getElementById(IMAGE_CONTAINER_ID) as HTMLDivElement;
+
+        fireEvent.mouseDown(containerElement);
+        fireEvent.mouseMove(containerElement, { buttons: 1, pageX: 300, pageY: 100 });
+        fireEvent.mouseUp(containerElement);
+
+        expect(containerElement).toBeInTheDocument();
+    });
+});
