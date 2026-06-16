38a3062ba59ef5703d5b939cd69ce9e3ce5f6fb2
benfrontify <ben.laemmlin+1@frontify.com>
2026-06-16 15:31:28 +0200

    feat(doAndDont): test to vitest

--- packages/dos-donts-block/src/components/DoAndDontColor.tsx
+++ packages/dos-donts-block/src/components/DoAndDontColor.tsx
@@ -6,10 +6,12 @@ import { THEME_PREFIX } from '@frontify/guideline-blocks-settings';
 export const DO_COLOR_DEFAULT_VALUE = { red: 0, green: 200, blue: 165, alpha: 1 };
 export const DONT_COLOR_DEFAULT_VALUE = { red: 255, green: 55, blue: 90, alpha: 1 };
 
-export const getDefaultDoColor = (themeStyle: CSSStyleDeclaration) =>
-    rgbStringToRgbObject(themeStyle.getPropertyValue(`${THEME_PREFIX}accent-color-tip-color`)) ||
-    DO_COLOR_DEFAULT_VALUE;
+export const getDefaultDoColor = (themeStyle: CSSStyleDeclaration) => {
+    const value = themeStyle.getPropertyValue(`${THEME_PREFIX}accent-color-tip-color`);
+    return (value && rgbStringToRgbObject(value)) || DO_COLOR_DEFAULT_VALUE;
+};
 
-export const getDefaultDontColor = (themeStyle: CSSStyleDeclaration) =>
-    rgbStringToRgbObject(themeStyle.getPropertyValue(`${THEME_PREFIX}accent-color-warning-color`)) ||
-    DONT_COLOR_DEFAULT_VALUE;
+export const getDefaultDontColor = (themeStyle: CSSStyleDeclaration) => {
+    const value = themeStyle.getPropertyValue(`${THEME_PREFIX}accent-color-warning-color`);
+    return (value && rgbStringToRgbObject(value)) || DONT_COLOR_DEFAULT_VALUE;
+};
