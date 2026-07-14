875cda35002806d89abfec53769e8494affb5431
benfrontify <ben.laemmlin+1@frontify.com>
2026-07-14 15:22:48 +0200

    feat(DoDontBlock): new style file (#1392)
    * Feat(DoDontBlock): new style file
    
    * refactor: spit in different files
    
    * Refactor: add new folder and added tests conteinerstyles
    
    * fix: imports
    
    * Update packages/dos-donts-block/src/helpers/getDoDontGridStyle.ts
    
    * fix lint
    
    ---------
    
    Co-authored-by: Rafael Giezendanner <rafael.giezendanner@frontify.com>

---
+++ packages/dos-donts-block/src/hooks/useDoDontColorStyle.ts
@@ -0,0 +1,36 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import { useMemo } from 'react';
+
+import {
+    DO_COLOR_DEFAULT_VALUE,
+    DONT_COLOR_DEFAULT_VALUE,
+    getDefaultDoColor,
+    getDefaultDontColor,
+} from '../helpers/Color';
+import { type Settings } from '../types';
+
+export const useDoDontColorStyle = (blockSettings: Settings) => {
+    const { doColor: customDoColor, dontColor: customDontColor, hasCustomDoColor, hasCustomDontColor } = blockSettings;
+
+    const themeStyle = useMemo(() => getComputedStyle(document.body), []);
+    const defaultDoColor = useMemo(() => getDefaultDoColor(themeStyle), [themeStyle]);
+    const defaultDontColor = useMemo(() => getDefaultDontColor(themeStyle), [themeStyle]);
+
+    const doColor = useMemo(
+        () => (hasCustomDoColor ? customDoColor : defaultDoColor),
+        [customDoColor, hasCustomDoColor, defaultDoColor]
+    );
+
+    const dontColor = useMemo(
+        () => (hasCustomDontColor ? customDontColor : defaultDontColor),
+        [customDontColor, hasCustomDontColor, defaultDontColor]
+    );
+
+    return {
+        doColor,
+        dontColor,
+        resolvedDoColor: doColor || DO_COLOR_DEFAULT_VALUE,
+        resolvedDontColor: dontColor || DONT_COLOR_DEFAULT_VALUE,
+    };
+};
