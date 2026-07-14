dd55c24d9df4b8485cb11ccdbda6a24a22b446c6
benfrontify <ben.laemmlin+1@frontify.com>
2026-07-14 11:25:07 +0200

    Refactor: made the suggested chances

--- packages/dos-donts-block/src/DosDontsAssets.tsx
+++ packages/dos-donts-block/src/DosDontsAssets.tsx
@@ -48,170 +48,146 @@ const getImageAltText = (alt: string | undefined, asset: Asset): string => {
     return alt ?? (typeof asset.alternativeText === 'string' ? asset.alternativeText : '');
 };
 
-export const DosDontsAssets = forwardRef<DosDontsAssetsRef, DosDontsAssetsProps>(
-    (
-        {
-            id,
-            appBridge,
-            mode,
-            editing,
-            linkedImage,
-            alt,
-            onChangeItem,
-            updateAssetIdsFromKey,
-            isCustomImageHeight,
-            customImageHeightValue,
-            imageDisplay,
-            draggableProps,
-            imageHeightChoice,
-            isDragging,
-            type,
-            hasStrikethrough,
-            backgroundColor,
-            hasBackground,
-            hasRadius,
-            radiusChoice,
-            borderColor,
-            borderStyle,
-            borderWidth,
-            hasBorder,
-            radiusValue,
-            dontColor,
+export const DosDontsAssets = forwardRef<DosDontsAssetsRef, DosDontsAssetsProps>((props, ref) => {
+    const {
+        id,
+        appBridge,
+        mode,
+        editing,
+        linkedImage,
+        alt,
+        onChangeItem,
+        updateAssetIdsFromKey,
+        isDragging,
+        type,
+        hasStrikethrough,
+        borderColor,
+        borderStyle,
+        borderWidth,
+        hasBorder,
+    } = props;
+
+    const [showAltTextMenu, setShowAltTextMenu] = useState(false);
+    const [localAltText, setLocalAltText] = useState<string | undefined>(alt);
+    const [isUploadLoading, setIsUploadLoading] = useState(false);
+
+    const { openAssetChooser, closeAssetChooser } = useAssetChooser(appBridge);
+
+    const [openFileDialog, { selectedFiles }] = useFileInput({
+        multiple: false,
+        accept: 'image/*',
+    });
+
+    const [uploadFile, { results: uploadResults, doneAll }] = useAssetUpload({
+        onUploadProgress: () => {
+            setIsUploadLoading(true);
         },
-        ref
-    ) => {
-        const [showAltTextMenu, setShowAltTextMenu] = useState(false);
-        const [localAltText, setLocalAltText] = useState<string | undefined>(alt);
-        const [isUploadLoading, setIsUploadLoading] = useState(false);
+    });
 
-        const { openAssetChooser, closeAssetChooser } = useAssetChooser(appBridge);
-
-        const [openFileDialog, { selectedFiles }] = useFileInput({
-            multiple: false,
-            accept: 'image/*',
-        });
-
-        const [uploadFile, { results: uploadResults, doneAll }] = useAssetUpload({
-            onUploadProgress: () => {
-                setIsUploadLoading(true);
-            },
-        });
-
-        const saveAsset = useCallback(
-            async (asset: Asset) => {
-                const imageAlt = getImageAltText(alt, asset);
-
-                setLocalAltText(imageAlt);
-
+    const saveAsset = useCallback(
+        async (asset: Asset) => {
+            const imageAlt = getImageAltText(alt, asset);
+            setLocalAltText(imageAlt);
+            try {
                 if (updateAssetIdsFromKey) {
                     await updateAssetIdsFromKey(id, [asset.id]);
                     onChangeItem(id, { alt: imageAlt });
                 }
-
+            } finally {
                 setIsUploadLoading(false);
-            },
-            [alt, id, onChangeItem, updateAssetIdsFromKey]
-        );
+            }
+        },
+        [alt, id, onChangeItem, updateAssetIdsFromKey]
+    );
 
-        const onOpenAssetChooser = useCallback(() => {
-            openAssetChooser(
-                (result: Asset[]) => {
-                    const asset = result[0];
+    const onOpenAssetChooser = useCallback(() => {
+        openAssetChooser(
+            (result: Asset[]) => {
+                const asset = result[0];
 
-                    if (!asset) {
-                        closeAssetChooser();
-                        return;
-                    }
-
-                    setIsUploadLoading(true);
-
-                    saveAsset(asset)
-                        .finally(() => {
-                            closeAssetChooser();
-                        })
-                        .catch(() => undefined);
-                },
-                {
-                    multiSelection: false,
-                    objectTypes: [AssetChooserObjectType.ImageVideo],
-                    extensions: FileExtensionSets.Images,
+                if (!asset) {
+                    closeAssetChooser();
+                    return;
                 }
-            );
-        }, [closeAssetChooser, openAssetChooser, saveAsset]);
 
-        const onUploadClick = useCallback(() => {
-            openFileDialog();
-        }, [openFileDialog]);
-
-        useImperativeHandle(
-            ref,
-            () => ({
-                openUpload: onUploadClick,
-                openAssetChooser: onOpenAssetChooser,
-                openAltTextMenu: () => setShowAltTextMenu(true),
-            }),
-            [onOpenAssetChooser, onUploadClick]
-        );
-
-        useEffect(() => {
-            if (selectedFiles) {
                 setIsUploadLoading(true);
-                uploadFile(selectedFiles);
-            }
-            // eslint-disable-next-line @eslint-react/exhaustive-deps
-        }, [selectedFiles]);
 
-        useEffect(() => {
-            if (!doneAll) {
-                return;
+                saveAsset(asset)
+                    .finally(() => {
+                        closeAssetChooser();
+                    })
+                    .catch(() => undefined);
+            },
+            {
+                multiSelection: false,
+                objectTypes: [AssetChooserObjectType.ImageVideo],
+                extensions: FileExtensionSets.Images,
             }
-            const asset = uploadResults?.[0];
-            if (!asset) {
-                return;
-            }
-            saveAsset(asset).catch(() => undefined);
-            // eslint-disable-next-line @eslint-react/exhaustive-deps
-        }, [doneAll, uploadResults]);
-
-        return (
-            <>
-                <EditAltTextFlyout
-                    setShowAltTextMenu={setShowAltTextMenu}
-                    showAltTextMenu={showAltTextMenu}
-                    setLocalAltText={setLocalAltText}
-                    defaultAltText={alt}
-                    onSave={() => onChangeItem(id, { alt: localAltText })}
-                    localAltText={localAltText}
-                />
-
-                {mode === BlockMode.TEXT_AND_IMAGE && (
-                    <ImageComponent
-                        isEditing={editing}
-                        id={id}
-                        alt={alt}
-                        image={linkedImage}
-                        onAssetChooseClick={onOpenAssetChooser}
-                        onUploadClick={onUploadClick}
-                        isUploadLoading={isUploadLoading}
-                        isCustomImageHeight={isCustomImageHeight}
-                        customImageHeightValue={customImageHeightValue}
-                        imageDisplay={imageDisplay}
-                        draggableProps={draggableProps}
-                        imageHeightChoice={imageHeightChoice}
-                        isDragging={Boolean(isDragging)}
-                        hasStrikethrough={type === DoDontType.Dont && hasStrikethrough}
-                        backgroundColor={backgroundColor}
-                        hasBackground={hasBackground}
-                        hasRadius={hasRadius}
-                        radiusChoice={radiusChoice}
-                        border={hasBorder ? `${borderWidth} ${borderStyle} ${toRgbaString(borderColor)}` : ''}
-                        radiusValue={radiusValue}
-                        dontColor={dontColor}
-                    />
-                )}
-            </>
         );
-    }
-);
+    }, [closeAssetChooser, openAssetChooser, saveAsset]);
+
+    const onUploadClick = useCallback(() => {
+        openFileDialog();
+    }, [openFileDialog]);
+
+    useImperativeHandle(
+        ref,
+        () => ({
+            openUpload: onUploadClick,
+            openAssetChooser: onOpenAssetChooser,
+            openAltTextMenu: () => setShowAltTextMenu(true),
+        }),
+        [onOpenAssetChooser, onUploadClick]
+    );
+
+    useEffect(() => {
+        if (selectedFiles) {
+            setIsUploadLoading(true);
+            uploadFile(selectedFiles);
+        }
+        // eslint-disable-next-line @eslint-react/exhaustive-deps
+    }, [selectedFiles]);
+
+    useEffect(() => {
+        if (!doneAll) {
+            return;
+        }
+        const asset = uploadResults?.[0];
+        if (!asset) {
+            return;
+        }
+        saveAsset(asset).catch(() => undefined);
+        // eslint-disable-next-line @eslint-react/exhaustive-deps
+    }, [doneAll, uploadResults]);
+
+    return (
+        <>
+            <EditAltTextFlyout
+                setShowAltTextMenu={setShowAltTextMenu}
+                showAltTextMenu={showAltTextMenu}
+                setLocalAltText={setLocalAltText}
+                defaultAltText={alt}
+                onSave={() => onChangeItem(id, { alt: localAltText })}
+                localAltText={localAltText}
+            />
+
+            {mode === BlockMode.TEXT_AND_IMAGE && (
+                <ImageComponent
+                    {...props}
+                    isEditing={editing}
+                    id={id}
+                    alt={alt}
+                    image={linkedImage}
+                    onAssetChooseClick={onOpenAssetChooser}
+                    onUploadClick={onUploadClick}
+                    isUploadLoading={isUploadLoading}
+                    isDragging={Boolean(isDragging)}
+                    hasStrikethrough={type === DoDontType.Dont && hasStrikethrough}
+                    border={hasBorder ? `${borderWidth} ${borderStyle} ${toRgbaString(borderColor)}` : ''}
+                />
+            )}
+        </>
+    );
+});
 
 DosDontsAssets.displayName = 'DosDontsAssets';
