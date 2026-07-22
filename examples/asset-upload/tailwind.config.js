3fbba36cbeefed24f39e99d148a3de1441cfe640
Peter Tudosa <razvan-petru.tudosa@frontify.com>
2026-07-22 12:36:33 +0200

    fix: example package tailwind config
    Signed-off-by: Peter Tudosa <razvan-petru.tudosa@frontify.com>

--- examples/asset-upload/tailwind.config.js
+++ examples/asset-upload/tailwind.config.js
@@ -1,11 +1,11 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import tailwindFondue from '@frontify/fondue/tailwind';
+import guidelineBlockSettings from '@frontify/guideline-blocks-settings/tailwind';
 
 import sharedTailwind from '../../packages/shared/tailwind.config';
 
 module.exports = {
-    presets: [tailwindFondue, sharedTailwind],
+    presets: [guidelineBlockSettings, sharedTailwind],
     content: ['src/**/*.{ts,tsx}', '../../packages/shared/src/**/*.{ts,tsx}'],
     corePlugins: {
         preflight: false,
