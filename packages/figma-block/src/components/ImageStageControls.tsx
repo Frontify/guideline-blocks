e3c00858920ef075b7fdf746b75be0b38a8601c5
benfrontify <ben.laemmlin+1@frontify.com>
2026-07-23 17:40:22 +0200

    Fix: define icon color right

--- packages/figma-block/src/components/ImageStageControls.tsx
+++ packages/figma-block/src/components/ImageStageControls.tsx
@@ -1,15 +1,30 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { Button } from '@frontify/fondue/components';
-import { IconArrowExpand, IconCross, IconMinus, IconPlus } from '@frontify/fondue/icons';
-import { joinClassNames } from '@frontify/guideline-blocks-settings';
+import { Button } from "@frontify/fondue/components";
+import {
+    IconArrowExpand,
+    IconCross,
+    IconMinus,
+    IconPlus,
+} from "@frontify/fondue/icons";
+import { joinClassNames } from "@frontify/guideline-blocks-settings";
 
-import { type DrawFullScreenActionButtonProps, type DrawZoomInOutButtonsProps } from '../types';
+import {
+    type DrawFullScreenActionButtonProps,
+    type DrawZoomInOutButtonsProps,
+} from "../types";
 
-export const DrawFullScreenActionButton = ({ isFullScreen = false, onClick }: DrawFullScreenActionButtonProps) => (
+export const DrawFullScreenActionButton = ({
+    isFullScreen = false,
+    onClick,
+}: DrawFullScreenActionButtonProps) => (
     <div className="tw-invisible group-hover:tw-visible tw-absolute tw-top-4 tw-right-4">
         <Button emphasis="default" onPress={onClick} aspect="square">
-            {isFullScreen ? <IconCross size={16} /> : <IconArrowExpand size={16} />}
+            {isFullScreen ? (
+                <IconCross size={16} />
+            ) : (
+                <IconArrowExpand size={16} />
+            )}
         </Button>
     </div>
 );
@@ -21,15 +36,15 @@ export const DrawZoomInOutButtons = ({
 }: DrawZoomInOutButtonsProps) => (
     <div
         className={joinClassNames([
-            'tw-invisible group-hover:tw-visible tw-absolute tw-right-4 tw-rounded-[4px] tw-cursor-pointer tw-border tw-border-[rgba(255,255,255,0.2)] tw-bg-[#222]',
-            isFullScreen ? ' tw-bottom-[50%]' : 'tw-bottom-4',
+            "tw-invisible group-hover:tw-visible tw-absolute tw-right-4 tw-rounded-[4px] tw-cursor-pointer tw-border tw-border-[rgba(255,255,255,0.2)] tw-bg-[#222]",
+            isFullScreen ? " tw-bottom-[50%]" : "tw-bottom-4",
         ])}
     >
         <button
             type="button"
             data-test-id="zoom-in"
             onClick={onClickZoomIn}
-            className="tw-w-[32px] tw-h-[32px] tw-text-white tw-flex tw-justify-center tw-items-center"
+            className="tw-w-[32px] tw-h-[32px] tw-text-[white] tw-flex tw-justify-center tw-items-center"
         >
             <IconMinus size={16} />
         </button>
@@ -37,7 +52,7 @@ export const DrawZoomInOutButtons = ({
             type="button"
             data-test-id="zoom-out"
             onClick={onClickZoomOut}
-            className="tw-w-[32px] tw-h-[32px] tw-text-white tw-flex tw-justify-center tw-items-center"
+            className="tw-w-[32px] tw-h-[32px] tw-text-[white] tw-flex tw-justify-center tw-items-center"
         >
             <IconPlus size={16} />
         </button>
