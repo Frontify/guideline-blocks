560444a7b7b786f5502df2cf5dfa3a2eaf425277
Daniel Fulop <daniel.fulop@frontify.com>
2026-09-07 13:12:53 +0200

    feat(AssetKitBlock): add settings schema

--- packages/asset-kit-block/src/types.ts
+++ packages/asset-kit-block/src/types.ts
@@ -26,7 +26,6 @@ export type Settings = {
     radiusValue_thumbnails?: number;
     description?: string;
     title?: string;
-    downloadUrlBlock?: string;
     showThumbnails?: boolean;
     showCount?: boolean;
     assetCountColor?: 'inherit' | 'override';
